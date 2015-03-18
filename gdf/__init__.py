7f2efe61c682ad72cb31014327da8d02b38e32c2
Alex Ip <alex@trentham.net.au>
2015-03-18 18:19:13 +1100

    Function added to read configuration from DBs

--- gdf/__init__.py
+++ gdf/__init__.py
@@ -98,6 +98,106 @@ class GDF(object):
         
         log_multiline(logger.debug, self.__dict__, 'GDF.__dict__', '\t')
         
+        
+    def get_ndarray_types(self, databases={}):
+        '''Function to return a dict with details of all dimensions managed in databases keyed as follows:
+        
+           <db_name>
+               'ndarray_types'
+                    <nd_array_type_tag>
+                        'measurement_types'
+                           <measurement_type_tag>
+                        'domains'
+                            <domain_name>
+                                'dimensions'
+                                    <dimension_tag>
+                   
+        '''
+        
+        def get_db_data(db_name, databases):
+            db_dict = {'ndarray_types': {}}
+            database = databases[db_name]
+            db_config_dict = self._configuration['configurations'][db_name]
+            
+            ndarray_types = database.submit_query('''-- Query to return all ndarray_type configuration info
+select * from ndarray_type 
+join ndarray_type_measurement_type using(ndarray_type_id)
+join measurement_type using(measurement_metatype_id, measurement_type_id)
+join measurement_metatype using(measurement_metatype_id)
+join ndarray_type_dimension using(ndarray_type_id)
+join dimension_domain using(dimension_id, domain_id)
+join domain using(domain_id)
+join dimension using(dimension_id)
+join indexing_type using(indexing_type_id)
+join reference_system using (reference_system_id)
+order by ndarray_type_tag, measurement_type_index, creation_order;
+''')
+            for record_dict in ndarray_types.record_generator():
+                ndarray_type_dict = db_dict['ndarray_types'].get(record_dict['nd_array_type_tag'])
+                if ndarray_type_dict is None:
+                    ndarray_type_dict = {record_dict['nd_array_type_tag']: {'ndarray_type_name': record_dict['ndarray_type_name'],
+                                                                            'measurement_types': {},
+                                                                            'domains': {}
+                                                                            }
+                                         }
+                    db_dict['ndarray_types'][record_dict['nd_array_type_tag']] = ndarray_type_dict
+                    
+                measurement_type_dict = ndarray_type_dict['measurement_types'].get(record_dict['measurement_type_tag'])
+                if measurement_type_dict is None:
+                    measurement_type_dict = {record_dict['measurement_type_tag']: {'measurement_metatype': record_dict['measurement_metatype_name'],
+                                                                                   'measurement_type_name': record_dict['measurement_type_name']
+                                                                                   }
+                                             }
+                    ndarray_type_dict['measurement_types'][record_dict['measurement_type_tag']] = measurement_type_dict
+                    
+                #TODO: Should have a domain_tag field
+                domain_dict = ndarray_type_dict['domains'].get(record_dict['domain_name'])
+                if domain_dict is None:
+                    domain_dict = {record_dict['domain_name']: {'reference_system_name': record_dict['reference_system_name'],
+                                                                'reference_system_definition': record_dict['reference_system_definition'],
+                                                                'reference_system_unit': record_dict['reference_system_unit'], 
+                                                                'dimensions': {}
+                                                                }
+                                   }
+                    ndarray_type_dict['domains'][record_dict['domain_name']] = domain_dict
+                    
+                dimension_dict = domain_dict['dimensions'].get(record_dict['dimension_tag'])
+                if dimension_dict is None:
+                    dimension_dict = {record_dict['dimension_tag']: {'creation_order': record_dict['creation_order'],
+                                                                     'dimension_extent': record_dict['dimension_extent'],
+                                                                     'dimension_elements': record_dict['dimension_elements'],
+                                                                     'dimension_cache': record_dict['dimension_cache'],
+                                                                     'dimension_origin': record_dict['dimension_origin'],
+                                                                     'dimension_extent_unit': record_dict['dimension_extent_unit'] or record_dict['reference_system_unit']
+                                                                     }
+                                      }
+                    domain_dict[record_dict['dimension_tag']] = dimension_dict
+                    
+            return db_dict
+        
+        databases = databases or self._databases
+        
+        result_dict = {} # Nested dict containing nd_array_type details for all databases
+
+        #TODO: Multi-thread this section
+        for db_name in sorted(databases.keys()):
+            result_dict.update(get_db_data(db_name, databases))
+            
+        return result_dict
+                    
+                
+    
+    def get_dimensions(self, databases={}):
+        '''Function to return a dict with details of all dimensions managed in databases
+        '''
+        databases = databases or self._databases
+        
+        for db_name in sorted(databases.keys()):
+            database = databases[db_name]
+            db_config_dict = self._configuration['configurations'][db_name]
+
+            
+        
     
     # Define properties for GDF class
     @property
