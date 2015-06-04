6ffae8f5b588e67250136ce59f3eff76f88e56b7
Alex Ip <alex@trentham.net.au>
2015-06-05 09:51:46 +1000

    Header fixed up Superfluous fields removed from SQL

--- gdf/__init__.py
+++ gdf/__init__.py
@@ -1,3 +1,37 @@
+#!/usr/bin/env python
+
+#===============================================================================
+# Copyright (c)  2014 Geoscience Australia
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither Geoscience Australia nor the names of its contributors may be
+#       used to endorse or promote products derived from this software
+#       without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#===============================================================================
+'''
+GDF Class
+Created on 12/03/2015
+
+@author: Alex Ip
+'''
 import os
 import sys
 import threading
@@ -8,11 +42,11 @@ import pytz
 import calendar
 import collections
 import numexpr
+import logging
 
 from _database import Database, CachedResultSet
 from _arguments import CommandLineArgs
 from _config_file import ConfigFile
-import logging
 
 from EOtools.utils import log_multiline
 
@@ -1049,9 +1083,6 @@ select distinct''' % storage_type_tag
 %s.storage_dimension_min as %s_min,
 %s.storage_dimension_max as %s_max,'''.replace('%s', dimension_tag)
                 SQL +='''
-storage_type_id,
-storage_id,
-storage_version,
 slice_index_value
 from storage
 '''                    
