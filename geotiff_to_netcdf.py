6ed0308c57351fe4a2fcbb4da7a9c754edb2e5aa
Damien Ayers <damien@omad.net>
2015-09-14 15:21:54 +1000

    Refactored NetCDF operations into a class
    Instead of the flat functions, now use classes to experiment with creating different structures of file.
     eg. Store each of the colour bands in separate variables, vs having colour band be an extra dimension
     of the 'observation' variable

--- geotiff_to_netcdf.py
+++ geotiff_to_netcdf.py
@@ -1,11 +1,10 @@
-from os.path import join
-import numpy as np
 from datetime import datetime
-import os
 import sys
+from abc import ABCMeta, abstractmethod
+
+import numpy as np
 import gdal
 import netCDF4
-import re
 import dateutil.parser
 
 chunk_x=400
@@ -14,144 +13,152 @@ chunk_time=1
 epoch = datetime(1970,1,1,0,0,0)
 
 
-def get_nbands_lats_lons_from_gdalds(dataset):
-    nbands, nlats, nlons = dataset.RasterCount, dataset.RasterYSize, dataset.RasterXSize
-    # Calculate pixel coordinates for each x,y based on the GeoTransform
-    geotransform = dataset.GetGeoTransform()
-    lons = np.arange(nlons)*geotransform[1]+geotransform[0]
-    lats = np.arange(nlats)*geotransform[5]+geotransform[3]
+class BaseNetCDF(object):
 
-    return nbands, lats, lons
+    __metaclass__ = ABCMeta
 
+    def __init__(self, netcdf_path, mode='r'):
+        print "BaseNetCDF.__init__"
+        self.nco = self.open_netcdf(netcdf_path, mode)
+        self.netcdf_path = netcdf_path
 
-def create_netcdf(filename):
-    nco = netCDF4.Dataset(filename,'w')
-    return nco
+    def close(self):
+        self.nco.close()
 
+    def _create_standard_dimensions(self, lats, lons):
+        """
+        Creates latitude, longitude and time dimension
 
-def create_netcdf_variables(nco, lats, lons):
-    nco.createDimension('longitude', len(lons))
-    nco.createDimension('latitude', len(lats))
-    nco.createDimension('time',None)
-    timeo = nco.createVariable('time','f4',('time'))
-    timeo.units = 'seconds since 1970-01-01 00:00:00'
-    timeo.standard_name = 'time'
-    timeo.long_name = 'Time, unix time-stamp'
-    timeo.calendar = 'standard'
+        Time is unlimited
+        Latitude and longitude are given the values in lats,lons
+        """
+        self.nco.createDimension('longitude', len(lons))
+        self.nco.createDimension('latitude', len(lats))
+        self.nco.createDimension('time',None)
+        timeo = self.nco.createVariable('time','f4',('time'))
+        timeo.units = 'seconds since 1970-01-01 00:00:00'
+        timeo.standard_name = 'time'
+        timeo.long_name = 'Time, unix time-stamp'
+        timeo.calendar = 'standard'
 
-    lon = nco.createVariable('longitude','f4',('longitude'))
-    lon.units = 'degrees_east'
-    lon.standard_name = 'longitude'
-    lon.long_name = 'longitude'
+        lon = self.nco.createVariable('longitude','f4',('longitude'))
+        lon.units = 'degrees_east'
+        lon.standard_name = 'longitude'
+        lon.long_name = 'longitude'
 
-    lat = nco.createVariable('latitude','f4',('latitude'))
-    lat.units = 'degrees_north'
-    lat.standard_name = 'latitude'
-    lat.long_name = 'latitude'
+        lat = self.nco.createVariable('latitude','f4',('latitude'))
+        lat.units = 'degrees_north'
+        lat.standard_name = 'latitude'
+        lat.long_name = 'latitude'
 
-    lon[:] = lons
-    lat[:] = lats
+        lon[:] = lons
+        lat[:] = lats
 
-    return timeo, lon, lat
+    def _set_wgs84_crs(self):
+        crso = self.nco.createVariable('crs','i4')
+        crso.long_name = "Lon/Lat Coords in WGS84"
+        crso.grid_mapping_name = "latitude_longitude"
+        crso.longitude_of_prime_meridian = 0.0
+        crso.semi_major_axis = 6378137.0
+        crso.inverse_flattening = 298.257223563
+        return crso
 
-def set_wgs84_crs(nco):
-    crso = nco.createVariable('crs','i4')
-    crso.long_name = "Lon/Lat Coords in WGS84"
-    crso.grid_mapping_name = "latitude_longitude"
-    crso.longitude_of_prime_meridian = 0.0
-    crso.semi_major_axis = 6378137.0
-    crso.inverse_flattening = 298.257223563
-    return crso
+    def _set_global_attributes(nco):
+        nco.Conventions = 'CF-1.6'
 
-def set_global_attributes(nco):
-    nco.Conventions = 'CF-1.6'
+    def open_netcdf(self, filename, mode):
+        return netCDF4.Dataset(filename, mode)
 
-def create_bands(nco, nbands):
-    netcdfbands = []
-    for i in range(1,nbands+1):
-        band = nco.createVariable('band' + str(i), 'i2',  ('time', 'latitude', 'longitude'),
-           zlib=True,chunksizes=[chunk_time,chunk_y,chunk_x],fill_value=-9999)
-        band.grid_mapping = 'crs'
-        band.set_auto_maskandscale(False)
-        band.units = "1"
-        netcdfbands.append(band)
-    #band1.units = 'degC'
-    #band1.scale_factor = 0.01
-    #band1.add_offset = 0.00
-    #band1.long_name = 'minimum monthly temperature'
-    #band1.standard_name = 'air_temperature'
-    return netcdfbands
+    def _add_time(self, timestamp):
+        # Parse ISO 8601 date/time string
+        start_datetime = dateutil.parser.parse(timestamp)
+        # Convert to seconds since epoch (1970-01-01)
+        start_datetime_delta = start_datetime - epoch
 
-def get_netcdf_bands(nco, nbands):
-    netcdfbands = []
-    for i in range(1,nbands+1):
-        band = nco.variables['band' + str(i)]
-        netcdfbands.append(band)
-    return netcdfbands
+        times = self.nco.variables['time']
 
+        # Save as next coordinate in file
+        times[len(times)] = start_datetime_delta.total_seconds()
 
+    def _get_nbands_lats_lons_from_gdalds(self, dataset):
+        nbands, nlats, nlons = dataset.RasterCount, dataset.RasterYSize, dataset.RasterXSize
+        # Calculate pixel coordinates for each x,y based on the GeoTransform
+        geotransform = dataset.GetGeoTransform()
+        lons = np.arange(nlons)*geotransform[1]+geotransform[0]
+        lats = np.arange(nlats)*geotransform[5]+geotransform[3]
 
+        return nbands, lats, lons
 
-def add_time_to_netcdf(timestamp, nco):
-    # Parse ISO 8601 date/time string
-    start_datetime = dateutil.parser.parse(timestamp)
-    # Convert to seconds since epoch (1970-01-01)
-    start_datetime_delta = start_datetime - epoch
+    def create_from_geotiff(self, geotiff_path):
+        dataset = gdal.Open(geotiff_path)
 
-    times = nco.variables['time']
+        nbands, lats, lons = self._get_nbands_lats_lons_from_gdalds(dataset)
+        ds_metadata = dataset.GetMetadata()
 
-    # Save as next coordinate in file
-    times[len(times)] = start_datetime_delta.total_seconds()
+        self._set_wgs84_crs()
+        self._set_global_attributes()
+        self._create_variables(lats, lons, nbands)
+        self._add_time(ds_metadata['start_datetime'])
+        self._write_data_to_netcdf(dataset)
 
-def write_data_to_netcdf(gdal_dataset, netcdfbands):
+    @abstractmethod
+    def _create_variables(self, lats, lons, nbands):
+        pass
 
-    for idx, out_band in enumerate(netcdfbands, start=1):
-        in_band = gdal_dataset.GetRasterBand(idx)
+    @abstractmethod
+    def _write_data_to_netcdf(self, dataset):
+        pass
 
-        metadata = in_band.GetMetadata() # eg. filename: 'source.tif', name: 'Photosynthetic Vegetation'
-        out_band.long_name = metadata.get('name')
-        out_band.source_filename = metadata.get('filename')
-        out_band.missing_value = in_band.GetNoDataValue()
+    def append_geotiff(self, geotiff):
+        dataset = gdal.Open(geotiff)
+        ds_metadata = dataset.GetMetadata()
+        self._add_time(ds_metadata['start_datetime'])
 
-        out_band[0,:,:] = in_band.ReadAsArray()
-        print('.',)
-
-def create_netcdf_from_geotiff(geotiff, netcdf):
-    # Open Geotiff
-    dataset = gdal.Open(geotiff)
-
-    nbands, lats, lons = get_nbands_lats_lons_from_gdalds(dataset)
-    ds_metadata = dataset.GetMetadata()
-
-    nco = create_netcdf(netcdf)
-    set_wgs84_crs(nco)
-    set_global_attributes(nco)
-    create_netcdf_variables(nco, lats, lons)
-
-    add_time_to_netcdf(ds_metadata['start_datetime'], nco)
-
-    netcdfbands = create_bands(nco, nbands)
-
-    write_data_to_netcdf(dataset, netcdfbands)
-
-    nco.close()
-
-def append_geotiff_to_netcdf(geotiff, netcdf):
-    nco = netCDF4.Dataset(netcdf, 'a')
-
-    dataset = gdal.Open(geotiff)
-    ds_metadata = dataset.GetMetadata()
-    nbands, lats, lons = get_nbands_lats_lons_from_gdalds(dataset)
+        self._write_data_to_netcdf(dataset)
 
 
+class SeparateBandsTimeSlicedNetCDF(BaseNetCDF):
+    def _create_variables(self, lats, lons, nbands):
+        self._create_standard_dimensions(lats, lons)
+        self._create_bands(nbands)
 
-    add_time_to_netcdf(ds_metadata['start_datetime'], nco)
+    def _create_bands(self, nbands):
+        netcdfbands = []
+        for i in range(1,nbands+1):
+            band = self.nco.createVariable('band' + str(i), 'i2',  ('time', 'latitude', 'longitude'),
+               zlib=True,chunksizes=[chunk_time,chunk_y,chunk_x],fill_value=-9999)
+            band.grid_mapping = 'crs'
+            band.set_auto_maskandscale(False)
+            band.units = "1"
+            netcdfbands.append(band)
+        #band1.units = 'degC'
+        #band1.scale_factor = 0.01
+        #band1.add_offset = 0.00
+        #band1.long_name = 'minimum monthly temperature'
+        #band1.standard_name = 'air_temperature'
+        return netcdfbands
 
-    netcdfbands = get_netcdf_bands(nco, nbands)
+    def _get_netcdf_bands(self, nbands):
+        netcdfbands = []
+        for i in range(1,nbands+1):
+            band = self.nco.variables['band' + str(i)]
+            netcdfbands.append(band)
+        return netcdfbands
 
-    write_data_to_netcdf(dataset, netcdfbands)
+    def _write_data_to_netcdf(self, gdal_dataset):
+        nbands, lats, lons = self._get_nbands_lats_lons_from_gdalds(gdal_dataset)
+        netcdfbands = self._get_netcdf_bands(nbands)
 
-    nco.close()
+        for idx, out_band in enumerate(netcdfbands, start=1):
+            in_band = gdal_dataset.GetRasterBand(idx)
+
+            metadata = in_band.GetMetadata() # eg. filename: 'source.tif', name: 'Photosynthetic Vegetation'
+            out_band.long_name = metadata.get('name')
+            out_band.source_filename = metadata.get('filename')
+            out_band.missing_value = in_band.GetNoDataValue()
+
+            out_band[0,:,:] = in_band.ReadAsArray()
+            print('.',)
 
 #     # check that the files exist
 #     # check that the files area (lat/longs) matches
@@ -162,6 +169,14 @@ def append_geotiff_to_netcdf(geotiff, netcdf):
 #     # add each band to the netcdf at the new time
 #     pass
 
+class BandAsDimensionNetCDF(BaseNetCDF):
+    def _create_variables(self, lats, lons, nbands):
+        self._create_standard_dimensions(lats, lons)
+        self._create_band_dimension(nbands)
+
+    def _create_band_dimension(self, nbands):
+        self.nco.createDimension('band', nbands)
+        band = self.nco.createVariable('band','f4',('band'))
 
 
 
@@ -170,10 +185,16 @@ def main(args):
         print("Usage: geotiff_to_netcdf.py input_geotiff.tiff output_netcdf.nc")
         exit()
 
+    netcdf_path = args[2]
+    geotiff_path = args[1]
     if (args[0] == 'geotiff_to_netcdf.py'):
-        create_netcdf_from_geotiff(*args[1:])
+        dcnc = SeparateBandsTimeSlicedNetCDF(netcdf_path, mode='w')
+        dcnc.create_from_geotiff(geotiff_path)
+        dcnc.close()
     elif (args[0] == 'append_to_netcdf.py'):
-        append_geotiff_to_netcdf(*args[1:])
+        dcnc = SeparateBandsTimeSlicedNetCDF(netcdf_path, mode='a')
+        dcnc.append_geotiff(geotiff_path)
+        dcnc.close()
     else:
         print("Unknown command line name")
 
