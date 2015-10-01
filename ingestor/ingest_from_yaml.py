d5d9db9dfe44ebc3279a3c97d32b2d48a73e5532
Damien Ayers <damien@omad.net>
2015-10-02 09:17:09 +1000

    Implement creation of single var NetCDFs from the command line
    Use injest_from_yaml.py as a command line tool. At the moment
    some of the file naming is hard coded, but will be easy to split out.

--- ingestor/ingest_from_yaml.py
+++ ingestor/ingest_from_yaml.py
@@ -4,7 +4,7 @@ import os
 import os.path
 import yaml
 from create_tiles import calc_target_names, create_tiles
-from geotiff_to_netcdf import create_or_replace
+from geotiff_to_netcdf import create_or_replace, MultiVariableNetCDF, SingleVariableNetCDF
 from pprint import pprint
 
 
@@ -24,9 +24,11 @@ def get_input_files(input_file, data):
 
 
 @click.command()
-@click.option('--output-dir', default='.')
+@click.option('--output-dir', '-o', default='.')
+@click.option('--multi-variable', 'netcdf_class', flag_value=MultiVariableNetCDF, default=True)
+@click.option('--single-variable', 'netcdf_class', flag_value=SingleVariableNetCDF)
 @click.argument('yaml_file', type=click.Path(exists=True))
-def main(yaml_file, output_dir):
+def main(yaml_file, output_dir, netcdf_class):
     os.chdir(output_dir)
 
     data = read_yaml(yaml_file)
@@ -34,7 +36,7 @@ def main(yaml_file, output_dir):
 
     input_files = get_input_files(yaml_file, data)
     basename = data['ga_label']
-    filename_format = 'combined_{x}_{y}.nc'
+    filename_format = 'combined_singlevar_{x}_{y}.nc'
     tile_options = {
         'output_format': 'GTiff',
         'create_options': ['COMPRESS=DEFLATE', 'ZLEVEL=1']
@@ -46,7 +48,7 @@ def main(yaml_file, output_dir):
 
     # Import into proper NetCDF files
     for geotiff, netcdf in renames:
-        create_or_replace(geotiff, netcdf, data)
+        create_or_replace(geotiff, netcdf, data, netcdf_class=netcdf_class)
 
 
 if __name__ == '__main__':
