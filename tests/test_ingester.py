3877bc3da720789ee1acf6da317bd5f7e31d9684
Damien Ayers <damien@omad.net>
2015-11-04 17:00:19 +1100

    Fix merge

--- tests/test_ingester.py
+++ tests/test_ingester.py
@@ -1,9 +1,15 @@
 from __future__ import print_function, absolute_import
 
-from datacube.ingester.ingester_cli import main
+from datacube.ingester.ingester import run_ingest
 
 
 def test_ingest(tmpdir):
     print(tmpdir)
-    main()
+
+    storage_config = 'data/storage_config.yaml'
+    ingest_config = 'data/ingest_config.yaml'
+
+    run_ingest()
+
     assert 0
+
