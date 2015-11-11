5df400d46e7ba91ba1a05f598bbce90a3ab73c77
Jeremy Hooke <jez@stulk.com>
2015-11-11 16:40:34 +1100

    Expand config-tool help text.

--- datacube/scripts/config_tool.py
+++ datacube/scripts/config_tool.py
@@ -22,24 +22,25 @@ def cli(verbose):
     logging.getLogger('datacube').setLevel(logging.WARN - 10 * verbose)
 
 
-@cli.group()
+@cli.group(help='Storage types')
 def storage():
     pass
 
 
 @storage.command('add')
-@click.argument('descriptor',
+@click.argument('yaml_file',
                 type=click.Path(exists=True, readable=True, writable=False),
                 nargs=-1)
-def add_storage(descriptor):
+def add_storage(yaml_file):
     config = index.data_management_connect()
 
-    for descriptor_path in descriptor:
+    for descriptor_path in yaml_file:
         config.ensure_storage_type(_parse_doc(descriptor_path))
 
 
-def _parse_doc(file_path):
-    return yaml.load(open(file_path, 'r'))
+@storage.command('template', help='Print an example YAML template')
+def template_storage():
+    sys.stderr.write('TODO: Print an example storage-type template\n')
 
 
 @storage.command('list')
@@ -47,25 +48,34 @@ def list_storage():
     sys.stderr.write('TODO: list storage types\n')
 
 
-@cli.group('mappings')
+@cli.group(help='Dataset-storage mappings')
 def mappings():
     pass
 
 
 @mappings.command('add')
-@click.argument('descriptor',
+@click.argument('yaml_file',
                 type=click.Path(exists=True, readable=True, writable=False),
                 nargs=-1)
-def add_mappings(descriptor):
+def add_mappings(yaml_file):
     config = index.data_management_connect()
 
-    for descriptor_path in descriptor:
+    for descriptor_path in yaml_file:
         config.ensure_storage_mapping(_parse_doc(descriptor_path))
+
+
+@mappings.command('template', help='Print an example YAML template')
+def template_mappings():
+    sys.stderr.write('TODO: Print an example dataset-storage mapping template\n')
 
 
 @mappings.command('list')
 def list_mappings():
     sys.stderr.write('TODO: list mappings\n')
+
+
+def _parse_doc(file_path):
+    return yaml.load(open(file_path, 'r'))
 
 
 if __name__ == '__main__':
