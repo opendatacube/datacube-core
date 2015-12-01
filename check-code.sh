f21703d63bf8cd11dc696087c1fb152a149d58df
Jeremy Hooke <jez@stulk.com>
2015-12-01 18:12:25 +1100

    Simplify check_code script
    Use a pragma to skip compat.py, rather than manually.

--- check-code.sh
+++ check-code.sh
@@ -5,17 +5,16 @@ set -eu
 
 # Check all files other than 'compat.py'.
 #   -> compat.py contains both Python 2 and 3 syntax, so will fail lint checks.
-export py_files="$( find datacube -iname '*.py' -not -iname 'compat.py' ) examples/*.py"
+export py_files="datacube examples/*.py"
 
 pylint --reports no ${py_files}
-# Tentatively replaced by pylint: 
-# flake8 --max-line-length 120 ${py_files}
 
 pep8 --max-line-length 120 ${py_files}
+
 # Check for basic Python 3 incompatiblities.
 pylint --py3k --reports no ${py_files}
 
 # Run tests, taking coverage.
 # Users can specify extra folders as arguments.
-py.test --cov datacube datacube tests $@
+py.test --cov datacube datacube examples tests $@
 
--- datacube/compat.py
+++ datacube/compat.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # We're using references that don't exist in python 3 (unicode, long):
-# pylint: disable=undefined-variable,invalid-name
+# pylint: skip-file
 """
 Compatibility helpers for Python 2 and 3.
 
