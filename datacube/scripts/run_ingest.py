274c91977d6b27909c3bacbc97e9adba8e495a9b
Gregory Raevski <gregory.raevski@ga.gov.au>
2015-12-11 09:46:41 +1100

    added --no-storage option to ingester to switch off storage unit generation/index

--- datacube/scripts/run_ingest.py
+++ datacube/scripts/run_ingest.py
@@ -15,12 +15,13 @@ CLICK_SETTINGS = dict(help_option_names=['-h', '--help'])
 
 
 @click.command(help="Ingest datasets into the Data Cube.", context_settings=CLICK_SETTINGS)
+@click.option('--no-storage', is_flag=True, help="Don't create storage units")
 @click.option('--verbose', '-v', count=True, help="Use multiple times for more verbosity")
 @click.option('--log-queries', is_flag=True, help="Print database queries.")
 @click.argument('datasets',
                 type=click.Path(exists=True, readable=True, writable=False),
                 nargs=-1)
-def cli(verbose, datasets, log_queries):
+def cli(datasets, no_storage, verbose, log_queries):
     config.init_logging(verbosity_level=verbose, log_queries=log_queries)
 
     indexed_datasets = []
@@ -28,7 +29,8 @@ def cli(verbose, datasets, log_queries):
     for dataset_path in datasets:
         indexed_datasets += index_datasets(Path(dataset_path), index=i)
 
-    store_datasets(indexed_datasets, index=i)
+    if not no_storage:
+        store_datasets(indexed_datasets, index=i)
 
 
 if __name__ == '__main__':
