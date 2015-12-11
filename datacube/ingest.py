d3bbf0a4be818b61e96d365241831a66d8892922
Jeremy Hooke <jez@stulk.com>
2015-12-11 12:25:07 +1100

    Allow any supported suffixes for default metadata.

--- datacube/ingest.py
+++ datacube/ingest.py
@@ -13,32 +13,6 @@ from .index import index_connect
 _LOG = logging.getLogger(__name__)
 
 
-def _expected_metadata_path(dataset_path):
-    """
-    Get the path where we expect a metadata file for this dataset.
-
-    (only supports eo metadata docs at the moment)
-
-    :type dataset_path: pathlib.Path
-    :rtype: Path
-    """
-
-    # - A dataset directory expects file 'agdc-metadata.yaml'.
-    # - A dataset file expects a sibling file with suffix '.agdc-md.yaml'.
-    # - Otherwise they gave us the metadata file directly.
-
-    if dataset_path.is_file():
-        if ui.is_supported_document_type(dataset_path):
-            return dataset_path
-
-        return dataset_path.parent.joinpath('{}.agdc-md.yaml'.format(dataset_path.name))
-
-    elif dataset_path.is_dir():
-        return dataset_path.joinpath('agdc-metadata.yaml')
-
-    raise ValueError('Unhandled path type for %r' % dataset_path)
-
-
 def index_datasets(path, index=None):
     """
     Discover datasets in path and add them to the index
@@ -49,7 +23,7 @@ def index_datasets(path, index=None):
     """
     index = index or index_connect()
 
-    metadata_path = _expected_metadata_path(path)
+    metadata_path = ui.get_metadata_path(path)
     if not metadata_path or not metadata_path.exists():
         raise ValueError('No supported metadata docs found for dataset {}'.format(path))
 
