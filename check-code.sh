354f7c656efd02e963ed77312fb8d87592f53b0d
Jeremy Hooke <jez@stulk.com>
2015-12-11 14:27:05 +1100

    Remove redundant code checkers.

--- check-code.sh
+++ check-code.sh
@@ -3,16 +3,7 @@
 
 set -eu
 
-# Check all files other than 'compat.py'.
-#   -> compat.py contains both Python 2 and 3 syntax, so will fail lint checks.
-export py_files="datacube examples/*.py"
-
-pylint --reports no ${py_files}
-
-pep8 --max-line-length 120 ${py_files}
-
-# Check for basic Python 3 incompatiblities.
-pylint --py3k --reports no ${py_files}
+pylint --reports no datacube examples/*.py
 
 # Run tests, taking coverage.
 # Users can specify extra folders as arguments.
