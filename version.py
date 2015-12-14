3dd8d272f2f7d1a59c46dee4e0b8c74d464f6734
Jeremy Hooke <jez@stulk.com>
2015-12-15 09:00:06 +1100

    Allow version script to work from any folder.

--- version.py
+++ version.py
@@ -2,8 +2,6 @@
 """
 Get the current version number.
 
-Loaded via git tags, package info or git-archive rewrite.
-
 Public releases are expected to be tagged in the repository
 with prefix 'datacube-' and a version number following PEP-440.
 
@@ -12,8 +10,9 @@ Eg. ::
     datacube-0.1.0
     datacube-0.2.0.dev1
 
-See PEP440: https://www.python.org/dev/peps/pep-0440
-Derived from https://github.com/Changaco/version.py
+Refer to PEP440: https://www.python.org/dev/peps/pep-0440
+
+This script is derived from https://github.com/Changaco/version.py
 """
 
 import re
@@ -33,10 +32,11 @@ def get_version():
     if version:
         return version.group(1)
 
-    d = dirname(__file__)
+    package_dir = dirname(__file__)
+    git_dir = join(package_dir, '.git')
 
-    if isdir(join(d, '.git')):
-        cmd = 'git describe --tags --match %s[0-9]* --dirty' % PREFIX
+    if isdir(git_dir):
+        cmd = 'git --git-dir={} describe --tags --match {}[0-9]* --dirty'.format(git_dir, PREFIX)
         try:
             git_version = check_output(cmd.split()).decode().strip()[len(PREFIX):]
         except CalledProcessError:
@@ -44,14 +44,14 @@ def get_version():
         components = git_version.split('-')
         version = components.pop(0)
 
-        # Any other suffixes imply this is not a release: Append an internal build number 
+        # Any other suffixes imply this is not a release: Append an internal build number
         if components:
             # <commit count>.<git hash>.<whether the working tree is dirty>
             version += '+' + '.'.join(components)
 
     else:
         # Extract the version from the PKG-INFO file.
-        with open(join(d, 'PKG-INFO')) as f:
+        with open(join(package_dir, 'PKG-INFO')) as f:
             version = VERSION_PATTERN.search(f.read()).group(1)
 
     return version
