cf335a9468149ba2e6e7d11989f830cdcbc548c2
Jeremy Hooke <jez@stulk.com>
2016-01-08 09:43:24 +1100

    Support version numbers of non-release git archives.

--- version.py
+++ version.py
@@ -18,7 +18,7 @@ This script is derived from https://github.com/Changaco/version.py
 from __future__ import absolute_import, print_function
 
 import re
-from os.path import dirname, isdir, join
+from os.path import dirname, isdir, join, exists
 from subprocess import CalledProcessError, check_output
 
 PREFIX = 'datacube-'
@@ -27,15 +27,14 @@ GIT_TAG_PATTERN = re.compile(r'\btag: %s([0-9][^,]*)\b' % PREFIX)
 VERSION_PATTERN = re.compile(r'^Version: (.+)$', re.M)
 NUMERIC = re.compile(r'^\d+$')
 
+GIT_ARCHIVE_REF_NAMES = '$Format:%D$'
+GIT_ARCHIVE_COMMIT_HASH = '$Format:%h$'
+
 
 def get_version():
-    # Return the version if it has been injected into the file by git-archive
-    version = GIT_TAG_PATTERN.search('$Format:%D$')
-    if version:
-        return version.group(1)
-
     package_dir = dirname(__file__)
     git_dir = join(package_dir, '.git')
+    pkg_info_file = join(package_dir, 'PKG-INFO')
 
     if isdir(git_dir):
         cmd = [
@@ -55,10 +54,21 @@ def get_version():
             # <commit count>.<git hash>.<whether the working tree is dirty>
             version += '+' + '.'.join(components)
 
-    else:
+    elif exists(pkg_info_file):
         # Extract the version from the PKG-INFO file.
-        with open(join(package_dir, 'PKG-INFO')) as f:
+        with open(pkg_info_file) as f:
             version = VERSION_PATTERN.search(f.read()).group(1)
+    elif not GIT_ARCHIVE_REF_NAMES.startswith('$'):
+        # Return the version if it has been injected into the file by git-archive
+        version = GIT_TAG_PATTERN.search(GIT_ARCHIVE_REF_NAMES)
+        if version:
+            return version.group(1)
+        else:
+            # Otherwise this is not a release: just use the commit hash.
+            # We can't get the last tagged version from git-archive, so we'll jus t use 0.0.0.
+            return '0.0.0+' + GIT_ARCHIVE_COMMIT_HASH
+    else:
+        raise RuntimeError('Unknown version: Not a git repository, a python dist tarball or a git-created archive.')
 
     return version
 
