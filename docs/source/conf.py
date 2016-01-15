f3b4dd26ddd51517f8f261d855fff7e569b8c843
Damien Ayers <damien@omad.net>
2016-01-15 11:23:40 +1100

    Use RTD theme when processing docs locally

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -113,7 +113,13 @@ pygments_style = 'sphinx'
 if on_rtd:
     html_theme = 'default'
 else:
-    html_theme = 'alabaster'
+    try:
+        import sphinx_rtd_theme
+        html_theme = 'sphinx_rtd_theme'
+        html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+    except ImportError:
+        html_theme = 'alabaster'
+
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
