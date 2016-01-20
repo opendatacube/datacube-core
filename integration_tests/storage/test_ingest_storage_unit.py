3a3e38a17b52e5469d9f39ca4561e5c424b4084b
Damien Ayers <damien@omad.net>
2016-01-21 09:42:28 +1100

    Expand ingest storage unit test

--- integration_tests/storage/test_ingest_storage_unit.py
+++ integration_tests/storage/test_ingest_storage_unit.py
@@ -3,8 +3,20 @@
 Module
 """
 from __future__ import absolute_import
+
+import pytest
+
 from datacube.storage.netcdf_writer import NetCDFWriter
 
 
+
+@pytest.fixture
+def ingestable_storage_unit(sample_dataset):
+    pass
+
+
 def test_ingest_storage_unit():
-    NetCDFWriter
+    # Create Storage Unit
+
+    # Ingest it
+    pass
