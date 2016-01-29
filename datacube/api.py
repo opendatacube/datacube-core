36093b4b2dc31d7a6ff8f8f2a519b85b8fd013a7
Andrew Hicks <andrew.hicks@ga.gov.au>
2016-01-29 14:16:23 +1100

    Print for py3

--- datacube/api.py
+++ datacube/api.py
@@ -16,7 +16,7 @@
 GDF Trial backward compatibility
 """
 
-from __future__ import absolute_import, division
+from __future__ import absolute_import, division, print_function
 
 import datetime
 import itertools
