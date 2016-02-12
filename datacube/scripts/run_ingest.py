36bd3adc159e59e2bc54e0ff9d9b3a09005cd743
Gregory Raevski <gregory.raevski@ga.gov.au>
2016-02-12 17:14:09 +1100

    scheduler param validation

--- datacube/scripts/run_ingest.py
+++ datacube/scripts/run_ingest.py
@@ -16,9 +16,20 @@ from datacube.ingest import index_datasets, store_datasets
 from datacube.executor import get_executor
 
 
+def parse_endpoint(ctx, param, value):
+    if not value:
+        return None
+
+    try:
+        ip, port = tuple(value.split(':'))
+        return ip, int(port)
+    except ValueError:
+        ctx.fail('%s is not a valid endpoint' % value)
+
+
 @click.command(help="Ingest datasets into the Data Cube.", context_settings=CLICK_SETTINGS)
 @ui.global_cli_options
-@click.option('--scheduler', default=None)
+@click.option('--scheduler', callback=parse_endpoint)
 @click.option('--workers', default=0)
 @click.option('--no-storage', is_flag=True, help="Don't create storage units")
 @click.argument('datasets',
@@ -30,7 +41,7 @@ def cli(index, datasets, workers, scheduler, no_storage):
     for dataset_path in datasets:
         indexed_datasets += index_datasets(Path(dataset_path), index=index)
 
-    executor = get_executor(str(scheduler), workers)
+    executor = get_executor(scheduler, workers)
     if not no_storage:
         store_datasets(indexed_datasets, index=index, executor=executor)
 
