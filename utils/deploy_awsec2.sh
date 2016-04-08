0f4c1bb0363c47d11eaa668e89bd3406d78fc0f1
Fei Zhang <fei.zhang@ga.gov.au>
2016-04-08 14:09:20 +1000

    check in again

---
+++ utils/deploy_awsec2.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+# Purpose: One script installation of agdc-v2 into a baseline AWS ec2 instance with ubuntu-14.04 OS 64bit.
+# Minimal hardware Spec: 2CPU, 8GB memory, 200GB diskspace.
+# Author: Fei.Zhang@ga.gov.au
+# Date: 2016-04-08
+
+function install_postgres94:  # Install postgres-9.4 etc
+{
+    sudo  apt-get install git g++
+
+    sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+    wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+
+    sudo apt-get update
+    sudo apt-get upgrade
+    sudo apt-get install postgresql-9.4 postgresql-client-9.4 postgresql-contrib-9.4
+    sudo apt-get install postgresql-doc-9.4
+
+    sudo apt-get install pgadmin3
+
+}
+
+function setup_db:
+{
+
+/etc/init.d/postgresql status
+#make sure postgres servewr up and running
+# 9.4/main (port 5432): online
+
+#become postgres user
+ su - postgres
+postgres@ip-10-0-0-164:~$ ls
+9.4
+
+postgres@ip-10-0-0-164:~$ createdb datacube
+
+# create a admin db users
+
+postgres@ip-10-0-0-164:~$ psql  -d datacube
+psql (9.4.7)
+Type "help" for help.
+
+datacube=# create user test_user login superuser  password 'test_user';
+CREATE ROLE
+
+# next step is create schema
+
+#as ubuntu user  config the database login for sqlalc
+ubuntu@ip-10-0-0-164:~$ cat  ~/.datacube.conf
+[datacube]
+db_database: datacube
+
+# A blank host will use a local socket. Specify a hostname to use TCP.
+db_hostname: localhost
+
+# Credentials are optional: you might have other Postgres authentication configured.
+# The default username otherwise is the current user id.
+db_username: test_user
+db_password: test_user
+
+[locations]
+# Where to reach storage locations from the current machine.
+#  -> Location names are arbitrary, but correspond to names used in the
+#     storage type files.
+#  -> We may eventually support remote protocols (http, S3, etc) to lazily fetch remote data.
+#eotiles: file:///short/public/democube/
+eotiles: file:///xdisk/democube/
+
+
+# create schema
+ubuntu@ip-10-0-0-164:~$  datacube-config -v database init
+Initialising database...
+Done.
+
+# import data storage type definition
+
+datacube-config storage add docs/config_samples/ga_landsat_7/ls7_albers.yaml
+
+# ingest datasets
+datacube-ingest -v ingest packages/nbar/LS8_OLITIRS_TNBAR_P54_GALPGS01-002_112_079_20140126 packages/pq/LS8_OLITIRS_PQ_P55_GAPQ01-002_112_079_20140126
+}
+
+# other stuff
+
+sudo apt-get install libhdf5-serial-dev libnetcdf-dev
+sudo apt-get install libgdal1-dev
+sudo apt-get install hdf5-tools netcdf-bin gdal-bin
+sudo apt-get installlibhdf5-doc netcdf-doc libgdal1-doc
+
+# firefox for http://localhost:8888 test jupyter notebook
+sudo apt-get install firefox
+
+
+
+# Install anaconda python as user ubuntu:
+wget https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda2-4.0.0-Linux-x86_64.sh
+
+# install anaconda
+
+bash Anaconda2-4.0.0-Linux-x86_64.sh
+
+# re-login or export PATH=/anaconda/bin:$PATH; to make sure python -V showing the anaconda's installation version
+conda install psycopg2 gdal libgdal hdf5 rasterio netcdf4 libnetcdf pandas
+
+
+# Clone agdc-v2 git repo and install it as ubuntu:
+git clone https://github.com/data-cube/agdc-v2.git
+cd adgc-v2
+git checkout develop # use the right branch
+python setup.py install  # will install into anaconda version
+
+# conda list
+
+
+
