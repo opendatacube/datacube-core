39ee51fcce3dbb32f75a4fb4408e2e68e486549b
Gregory Raevski <gregory.raevski@ga.gov.au>
2016-05-04 15:07:01 +1000

    update ingest script to use distributed

--- utils/ingest.submit.sh
+++ utils/ingest.submit.sh
@@ -1,27 +1,25 @@
 #!/usr/bin/env bash
 
 source /etc/bashrc
+module use -a /g/data/v10/public/modules/modulefiles
+module load agdc-py2-prod
 
-NTASKS=$#
-NNODES=$((PBS_NCPUS/NCPUS))
-OFFSET=1
-NODEID=0
+SCHEDULER_NODE=`sed '1q;d' $PBS_NODEFILE`
+SCHEDULER_PORT=45454
+SCHEDULER_ADDR=$SCHEDULER_NODE:$SCHEDULER_PORT
 
-while [ $NTASKS -gt 0 ]
-do
-  SLICE=$(((NTASKS+NNODES-1)/NNODES))
-  DOCS=${@:$OFFSET:$SLICE}
-
-  pbsdsh -n $NODEID -- /bin/bash -c "source /etc/bashrc;\
+pbsdsh -n 0 -- /bin/bash -c "source /etc/bashrc;\
 module use -a /g/data/v10/public/modules/modulefiles;\
 module load agdc-py2-prod;\
-datacube-ingest -v ingest --executor multiproc $NCPUS $DOCS;" &
-  
-  OFFSET=$((OFFSET+SLICE))
-  NTASKS=$((NTASKS-SLICE))
-  NNODES=$((NNODES-1))
-  NODEID=$((NODEID+NCPUS))
+dscheduler --port $SCHEDULER_PORT"&
+sleep 5s
+
+for ((i=0; i<PBS_NCPUS; i+=NCPUS)); do
+  pbsdsh -n $i -- /bin/bash -c "source /etc/bashrc;\
+module use -a /g/data/v10/public/modules/modulefiles;\
+module load agdc-py2-prod;\
+dworker $SCHEDULER_ADDR --nprocs 1 --nthreads 1"&
 done
+sleep 5s
 
-wait
-
+datacube-ingest -v ingest --executor distributed $SCHEDULER_ADDR "$@"
