40e093ca27b45f93a57a96cfc612901839554801
Damien Ayers <damien@omad.net>
2016-05-19 10:56:17 +1000

    Get tests to pass again

--- datacube/api/__init__.py
+++ datacube/api/__init__.py
@@ -4,6 +4,7 @@ Modules for the Storage and Access Query API
 """
 from __future__ import absolute_import
 
+from ._api import API
 from .masking import list_flag_names, describe_variable_flags, make_mask
 
-__all__ = ['list_flag_names', 'describe_variable_flags', 'make_mask']
+__all__ = ['API', 'list_flag_names', 'describe_variable_flags', 'make_mask']
