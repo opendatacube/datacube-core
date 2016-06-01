9d95f746e1f400b2ceb21883974da3a326685ff6
Andrew Hicks <andrew.hicks@ga.gov.au>
2016-06-01 14:33:15 +1000

    Allows access to Datacube object from top-level datacube module

--- datacube/__init__.py
+++ datacube/__init__.py
@@ -2,3 +2,5 @@ from __future__ import absolute_import
 from .version import get_version
 
 __version__ = get_version()
+
+from .api import Datacube # pylint: disable=wrong-import-position
--- datacube/api/__init__.py
+++ datacube/api/__init__.py
@@ -6,5 +6,7 @@ from __future__ import absolute_import
 
 from datacube.storage.masking import list_flag_names, describe_variable_flags, make_mask
 from ._api import API
+from .core import Datacube
+from .grid_workflow import GridWorkflow
 
-__all__ = ['API', 'list_flag_names', 'describe_variable_flags', 'make_mask']
+__all__ = ['API', 'Datacube', 'GridWorkflow', 'list_flag_names', 'describe_variable_flags', 'make_mask']
