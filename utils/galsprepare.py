56eac6b533fdffc438fe6988583bcc9e9b4bb770
Gregory Raevski <gregory.raevski@ga.gov.au>
2016-06-15 13:53:06 +1000

    fix getting CRS WKT in rasterio >= 0.36.0

--- utils/galsprepare.py
+++ utils/galsprepare.py
@@ -49,7 +49,7 @@ def get_projection(path):
     with rasterio.open(str(path)) as img:
         left, bottom, right, top = img.bounds
         return {
-            'spatial_reference': str(img.crs_wkt),
+            'spatial_reference': str(getattr(img, 'crs_wkt') or img.crs.wkt),
             'geo_ref_points': {
                 'ul': {'x': left, 'y': top},
                 'ur': {'x': right, 'y': top},
