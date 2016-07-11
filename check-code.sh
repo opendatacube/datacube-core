898d28ef12dd0ab816b1ef233b0468d4ba46a61f
Gregory Raevski <gregory.raevski@ga.gov.au>
2016-07-11 15:02:10 +1000

    add stats app to pylint an pytest

--- check-code.sh
+++ check-code.sh
@@ -6,9 +6,9 @@ set -x
 
 pep8 tests integration_tests --max-line-length 120
 
-pylint -j 2 --reports no datacube examples
+pylint -j 2 --reports no datacube examples apps/stats
 
 # Run tests, taking coverage.
 # Users can specify extra folders as arguments.
-py.test -r sx --cov datacube --durations=5 datacube tests $@
+py.test -r sx --cov datacube --durations=5 datacube tests apps $@
 
