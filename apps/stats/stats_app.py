703212c891177bee774ae13a5c4037f081f5de54
Damien Ayers <damien@omad.net>
2016-08-09 17:26:44 +1000

    More refactoring, use executor instead of threads

--- apps/stats/stats_app.py
+++ apps/stats/stats_app.py
@@ -10,6 +10,7 @@ from __future__ import absolute_import
 from __future__ import division
 import sys
 import dask
+import click
 import Queue
 import threading
 import time
@@ -21,10 +22,13 @@ import logging
 import rasterio
 import datacube.api
 import xarray as xr
+from datacube.ui.expression import parse_expressions
+
 from dateutil.relativedelta import relativedelta
 from datacube.api.geo_xarray import append_solar_day, _get_spatial_dims
 from datacube.storage.masking import make_mask
 from dateutil.rrule import rrule, YEARLY
+from datacube.ui import click as ui
 from enum import Enum
 
 dask.set_options(get=dask.async.get_sync)
@@ -42,33 +46,6 @@ MY_DATA = {}
 NDV = -999
 
 
-class MyStats(threading.Thread):
-    """
-     thread class to process result
-    """
-
-    def __init__(self, thread_id, acq_min, acq_max, season, dataset, band, stats, masks, sat, lon, lat, dc, q):
-        threading.Thread.__init__(self)
-        self.thread_id = thread_id
-        self.acq_min = acq_min
-        self.acq_max = acq_max
-        self.season = season
-        self.dataset = dataset
-        self.band = band
-        self.stats = stats
-        self.masks = masks
-        self.sat = sat
-        self.lon = lon
-        self.lat = lat
-        self.dc = dc
-        self.q = q
-        print("myStats initialised ")
-
-    def run(self):
-        process_result(self.acq_min, self.acq_max, self.season)
-        print("Exiting " + str(self.thread_id))
-
-
 class Ls57Arg25Bands(Enum):
     __order__ = "BLUE GREEN RED NEAR_INFRARED SHORT_WAVE_INFRARED_1 SHORT_WAVE_INFRARED_2"
 
@@ -80,14 +57,13 @@ class Ls57Arg25Bands(Enum):
     SHORT_WAVE_INFRARED_2 = 6
 
 
-def process_result(acq_min, acq_max, q):
+def process_result(acq_min, acq_max, work_queue):
     while not EXIT_FLAG:
         QUEUE_LOCK.acquire()
         if not WORK_QUEUE.empty():
-            data = q.get()
+            data = work_queue.get()
             _log.info("\t my data for epoch %s-%s is %s", str(acq_min), str(acq_max), data)
             print(" do your job with computed data")
-            # data.plot()
             MY_DATA.update({str(acq_min): data})
             QUEUE_LOCK.release()
             print("%s processing %s" % (acq_min, data))
@@ -97,10 +73,9 @@ def process_result(acq_min, acq_max, q):
 
 
 def initialise_odata(dtype, y, x):
-    shape = (y, x)
-    nbar = np.empty(shape, dtype=dtype)
-    nbar.fill(NDV)
-    return nbar
+    odata = np.empty(shape=(y, x), dtype=dtype)
+    odata.fill(NDV)
+    return odata
 
 
 def product_lookup(sat, dataset_type):
@@ -177,90 +152,65 @@ def do_compute(data, stats, odata):
     return odata
 
 
-def main(argv):
-    season = 'WINTER'
-    sat = 'LANDSAT_5'
-    band = 'NDVI'
-    epoch = 1
-    start_str = '2009-01-01'
-    end_str = '2011-01-01'
-    masks = 'PQ_MASK_CLEAR_ELB'
-    stats = 'PERCENTILE_25'
-    dataset = 'nbar'
-    interpolation = 'nearest'
-    opts, args = getopt.getopt(argv, "hn:l:d:b:i:m:p:s:e:q:z:o:x",
-                               ["lon=", "lat=", "dataset=", "band=", "stats=", "masks=", "epoch=", "start=", "end=",
-                                "season=", "sat=", "odir=", "interpolation="])
-    for opt, arg in opts:
-        if opt == '-h':
-            print('pass lon lat min max value like 130.378239-130.7823 -30.78276/-30.238')
-            print(' For ex. python stats_jupyter.py -n 146.002/146.202 -l -34.970/-34.999 -i PERCENTILE_10 -s '
-                  '2009-01-01 -e 2010-12-03 -z LANDSAT_5,LANDSAT_7 -q CALENDAR_YEAR -m PQ_MASK_CLEAR '
-                  '-b NDVI -p 2 -d nbar')
-            print('python stats_jupyter.py -o <output_dir> -n <lon> -l <lat> -d <dataset> -i <stats> '
-                  '-m <masks> -p <epoch> -s <start_date> -e <end_date> -z <sat> -q <season> -x <interpolation>')
-            sys.exit()
-        elif opt in ("-n", "--lon"):
-            lon = arg
-        elif opt in ("-l", "--lat"):
-            lat = arg
-        elif opt in ("-d", "--dataset"):
-            dataset = arg
-        elif opt in ("-b", "--band"):
-            band = arg
-        elif opt in ("-i", "--stats"):
-            stats = arg
-        elif opt in ("-s", "--start"):
-            start_str = arg
-        elif opt in ("-e", "--end"):
-            end_str = arg
-        elif opt in ("-p", "--epoch"):
-            epoch = arg
-        elif opt in ("-m", "--masks"):
-            masks = arg
-        elif opt in ("-x", "--interpolation"):
-            interpolation = arg
-        elif opt in ("-z", "--sat"):
-            sat = arg
-        elif opt in ("-q", "--season"):
-            season = arg
-    print("arguments accepted start end lon lat epoch season interpolation stats masks band dataset sat",
-          start_str, end_str, lon, lat, epoch, season, interpolation, stats, masks, band, dataset, sat)
-    start = datetime.strptime(start_str, "%Y-%m-%d").date()
-    end = datetime.strptime(end_str, "%Y-%m-%d").date()
-    epoch = int(epoch)
-    threads = []
-    thread_id = 1
-    dc = datacube.Datacube(app='jupyter-test')
-    for (acq_min, acq_max) in get_epochs(epoch, start, end):
-        thread = MyStats(thread_id, acq_min, acq_max, season, dataset, band, stats, masks,
-                         sat, lon, lat, dc, WORK_QUEUE)
-        thread.start()
-        threads.append(thread)
-        print("thread_id", thread_id)
-        thread_id += 1
+@click.command(name='stats')
+@click.argument('expression', nargs=-1)
+@click.option('--season')
+@click.option('--band')
+@click.option('--stats')
+@click.option('--epoch')
+@click.option('--start')
+@click.option('--end')
+@click.option('--masks', multiple=True)
+@ui.global_cli_options
+@ui.executor_cli_options
+@ui.pass_index(app_name='agdc-stats-app')
+def main(index, season, band, stats, start, end, epoch, masks, expression, executor):
+    index.datasets.search_summaries(**parse_expressions(*expression))
 
-    QUEUE_LOCK.acquire()
-    for (acq_min, acq_max) in get_epochs(epoch, start, end):
-        print("process starting for epoch period dates ", str(epoch), str(acq_min), str(acq_max))
-        odata = load_data(dc, acq_min, acq_max, season, dataset, band, stats, masks,
-                          lon, lat, sat, epoch)
-        print("process completed for epoch", str(acq_min))
-        WORK_QUEUE.put(odata)
-    print("all process finished")
-    QUEUE_LOCK.release()
-    # Wait for queue to empty
-    while not WORK_QUEUE.empty():
-        pass
-    global EXIT_FLAG
-    # Notify threads it's time to exit
-    EXIT_FLAG = 1
+    start_date = datetime.strptime(start, "%Y-%m-%d").date()
+    end_date = datetime.strptime(end, "%Y-%m-%d").date()
 
-    # Wait for all threads to complete
-    for t in threads:
-        print(" waiting thread ", t)
-        t.join()
-    print("Exiting Main Thread")
+    dc = datacube.Datacube(app='jupyter-test', index=index)
+
+    tasks = []
+    # Create tasks
+    for acq_min, acq_max in get_epochs(epoch, start_date, end_date):
+        tasks.append(dict(acq_min=acq_min, acq_max=acq_max, season=season, band=band,
+                          stats=stats, masks=masks, epoch=epoch))
+
+    # Execute tasks
+    results = []
+    for task in tasks:
+        results.append(executor.submit(load_data, dc=dc, **task))
+
+    for result in executor.as_completed(results):
+        acq_min, data = executor.result(result)
+        MY_DATA[acq_min] = data
+
+# def old_main(argv):
+#     season = 'WINTER'
+#     sat = 'LANDSAT_5'
+#     band = 'NDVI'
+#     epoch = 1
+#     start_str = '2009-01-01'
+#     end_str = '2011-01-01'
+#     masks = 'PQ_MASK_CLEAR_ELB'
+#     stats = 'PERCENTILE_25'
+#     dataset = 'nbar'
+#     interpolation = 'nearest'
+#     opts, args = getopt.getopt(argv, "hn:l:d:b:i:m:p:s:e:work_queue:z:o:x",
+#                                ["lon=", "lat=", "dataset=", "band=", "stats=", "masks=", "epoch=", "start=", "end=",
+#                                 "season=", "sat=", "odir=", "interpolation="])
+#     for opt, arg in opts:
+#         if opt == '-h':
+#             print('pass lon lat min max value like 130.378239-130.7823 -30.78276/-30.238')
+#             print(' For ex. python stats_jupyter.py -n 146.002/146.202 -l -34.970/-34.999 -i PERCENTILE_10 -s '
+#                   '2009-01-01 -e 2010-12-03 -z LANDSAT_5,LANDSAT_7 -work_queue CALENDAR_YEAR -m PQ_MASK_CLEAR '
+#                   '-b NDVI -p 2 -d nbar')
+#             print('python stats_jupyter.py -o <output_dir> -n <lon> -l <lat> -d <dataset> -i <stats> '
+#                   '-m <masks> -p <epoch> -s <start_date> -e <end_date> '
+#                   '-z <sat> -work_queue <season> -x <interpolation>')
+#             sys.exit()
 
 
 def get_epochs(epoch, start, end):
@@ -274,7 +224,6 @@ def get_epochs(epoch, start, end):
 
 
 def get_derive_data(band, dataset, ls, data):
-    ndvi = None
     blue = data.blue.where(data.blue != NDV)
     green = data.green.where(data.green != NDV)
     red = data.red.where(data.red != NDV)
@@ -282,38 +231,35 @@ def get_derive_data(band, dataset, ls, data):
     sw1 = data.swir1.where(data.swir1 != NDV)
     sw2 = data.swir2.where(data.swir2 != NDV)
     if band == "NDFI":
-        ndvi = (sw1 - nir) / (sw1 + nir)
+        return (sw1 - nir) / (sw1 + nir)
     if band == "NDVI":
-        ndvi = (nir - red) / (nir + red)
+        return (nir - red) / (nir + red)
     if band == "NDWI":
-        ndvi = (green - nir) / (green + nir)
+        return (green - nir) / (green + nir)
     if band == "MNDWI":
-        ndvi = (green - sw1) / (green + sw1)
+        return (green - sw1) / (green + sw1)
     if band == "NBR":
-        ndvi = (nir - sw2) / (nir + sw2)
+        return (nir - sw2) / (nir + sw2)
     if dataset == "TCI":
-        ndvi = calculate_tci(band, ls, blue, green, red, nir, sw1, sw2)
-    return ndvi
+        return calculate_tci(band, ls, blue, green, red, nir, sw1, sw2)
 
 
-def get_band_data(band, data):  # pylint: disable=too-many-branches
-    band_data = None
+def get_band_data(band, data):
     if band == "BLUE":
-        band_data = data.blue.where(data.blue != NDV)
+        return data.blue.where(data.blue != NDV)
     if band == "GREEN":
-        band_data = data.green.where(data.green != NDV)
+        return data.green.where(data.green != NDV)
     if band == "RED":
-        band_data = data.red.where(data.red != NDV)
+        return data.red.where(data.red != NDV)
     if band == "NEAR_INFRARED":
-        band_data = data.nir.where(data.nir != NDV)
+        return data.nir.where(data.nir != NDV)
     if band == "SHORT_WAVE_INFRARED_1":
-        band_data = data.swir1.where(data.swir1 != NDV)
+        return data.swir1.where(data.swir1 != NDV)
     if band == "SHORT_WAVE_INFRARED_2":
-        band_data = data.swir2.where(data.swir2 != NDV)
-    return band_data
+        return data.swir2.where(data.swir2 != NDV)
 
 
-def apply_mask(masks):
+def create_mask_def(masks):
     ga_pixel_bit = {name: True for name in
                     ('swir2_saturated',
                      'red_saturated',
@@ -349,46 +295,23 @@ def apply_mask(masks):
     return ga_pixel_bit
 
 
-def load_data(dc, acq_min, acq_max, season, dataset, band, stats, masks, lon, lat, sat, epoch):
-    prodname = None
+def load_data(dc, products, acq_min, acq_max, season, band, stats, masks, epoch, expression):
     data = None
-    lon_min = float(lon.split('/')[0])
-    lon_max = float(lon.split('/')[1])
-    lat_min = float(lat.split('/')[0])
-    lat_max = float(lat.split('/')[1])
     datasets = []
-    for st in sat.split(','):
-        if st.find('5') > 0:
-            prodname = 'ls5_nbar_albers'
-        elif st.find('7') > 0:
-            prodname = 'ls7_nbar_albers'
-        elif st.find('8') > 0:
-            prodname = 'ls8_nbar_albers'
-        _log.info("\t loading nbar data found for (%f %f) satellite %s the date range  %s %s", lon_min, lat_min, st,
-                  acq_min, acq_max)
-        data = dc.load(product=prodname, x=(lon_min, lon_max), y=(lat_min, lat_max), time=(acq_min, acq_max),
+    for prodname in products:
+        _log.info("\t loading data found for product %s the date range %s %s expression %s", prodname,
+                  acq_min, acq_max, expression)
+        data = dc.load(product=prodname,
                        measurements=['blue', 'green', 'red', 'nir', 'swir1', 'swir2'],
-                       dask_chunks={'time': 1, 'y': 200, 'x': 200})
+                       dask_chunks={'time': 1, 'y': 200, 'x': 200},
+                       **parse_expressions(*expression))
         if len(data) == 0:
-            _log.info("\t No data found for (%f %f) in the date range  %s %s", lon_min, lat_min,
-                      acq_min, acq_max)
+            _log.info("\t No data found")
             continue
         mask_clear = None
         if masks:
-            prodname = product_lookup(st, 'pqa')
-            pq = dc.load(product=prodname, x=(lon_min, lon_max), y=(lat_min, lat_max), time=(acq_min, acq_max),
-                         dask_chunks={'time': 1, 'y': 200, 'x': 200})
-            if len(pq) > 0:
-                for mask in masks.split(','):
-                    if mask == "PQ_MASK_CLEAR_ELB":
-                        mask_clear = pq['pixelquality'] & 15871 == 15871
-                    elif mask == "PQ_MASK_CLEAR":
-                        mask_clear = pq['pixelquality'] & 16383 == 16383
-                    else:
-                        mask_clear = make_mask(pq, **apply_mask(masks))
-                data = data.where(mask_clear)
-            else:
-                _log.info("\t No PQ data exists")
+            data = mask_data_with_pq(dc, data, prodname, acq_max, acq_min, expression, mask_clear, masks)
+
             append_solar_day(data, get_mean_longitude(data))
             data = data.groupby('solar_day').max(dim='time')
             season_dict = {'SUMMER': 'DJF', 'AUTUMN': 'MAM', 'WINTER': 'JJA', 'SPRING': 'SON',
@@ -417,14 +340,14 @@ def load_data(dc, acq_min, acq_max, season, dataset, band, stats, masks, lon, la
                     print(repr(e))
                     continue
 
-            print("Loaded data for ", st, acq_min, acq_max)
+            print("Loaded data for ", prodname, acq_min, acq_max)
             if band in [t.name for t in Ls57Arg25Bands]:
                 data = get_band_data(band, data)
             else:
-                data = get_derive_data(band, dataset, st, data)
+                data = get_derive_data(band, "dataset", prodname, data)
             datasets.append(data)
 
-    xr.concat(datasets, dim='solar_day')
+    # xr.concat(datasets, dim='solar_day')
 
     dtype = np.float32
     if stats == "COUNT_OBSERVED" or band in [t.name for t in Ls57Arg25Bands]:
@@ -437,6 +360,25 @@ def load_data(dc, acq_min, acq_max, season, dataset, band, stats, masks, lon, la
         return data
     else:
         return None
+
+
+def mask_data_with_pq(dc, data, prodname, acq_max, acq_min, expression, mask_clear, masks):
+    pq_prodname = prodname.replace('nbar', 'pqa')
+    pq = dc.load(product=pq_prodname, time=(acq_min, acq_max),
+                 dask_chunks={'time': 1, 'y': 200, 'x': 200},
+                 **parse_expressions(*expression))
+    if len(pq) > 0:
+        for mask in masks.split(','):
+            if mask == "PQ_MASK_CLEAR_ELB":
+                mask_clear = pq['pixelquality'] & 15871 == 15871
+            elif mask == "PQ_MASK_CLEAR":
+                mask_clear = pq['pixelquality'] & 16383 == 16383
+            else:
+                mask_clear = make_mask(pq, **create_mask_def(masks))
+        data = data.where(mask_clear)
+    else:
+        _log.info("\t No PQ data exists")
+    return data
 
 
 class TasselCapIndex(Enum):
@@ -503,7 +445,7 @@ TCI_COEFF = {
                 SixUniBands.RED: 0.4392,
                 SixUniBands.NEAR_INFRARED: -0.0580,
                 SixUniBands.SHORT_WAVE_INFRARED_1: 0.2012,
-                SixUniBands.SHORT_WAVE_INFRARED_2: -0.2768},576
+                SixUniBands.SHORT_WAVE_INFRARED_2: -0.2768},
 
             TasselCapIndex.FIFTH: {
                 SixUniBands.BLUE: -0.3280,
@@ -574,10 +516,11 @@ TCI_COEFF = {
 }
 
 
-def calculate_tci(band, satellite, blue, green, red, nir, sw1, sw2):
+def calculate_tci(band, sat, blue, green, red, nir, sw1, sw2):
     all_bands = dict()
     bn = None
     masked_bands = dict()
+
     for t in SixUniBands:
         if t.name == "BLUE":
             bn = blue
@@ -592,6 +535,7 @@ def calculate_tci(band, satellite, blue, green, red, nir, sw1, sw2):
         elif t.name == "SHORT_WAVE_INFRARED_2":
             bn = sw2
         all_bands.update({t: bn})
+
     for b in all_bands.keys():
         masked_bands[b] = all_bands[b].astype(np.float16)
         _log.info("mask band for %s is %s", b, masked_bands[b])
@@ -602,7 +546,6 @@ def calculate_tci(band, satellite, blue, green, red, nir, sw1, sw2):
             tci_cat = i
             break
 
-    sat = satellite
     if sat == "LANDSAT_7":
         sat = "LANDSAT_5"
     for i in SatTwo:
