460e2c550fdc3aa60da853676bf1baeebd74d503
Gregory Raevski <gregory.raevski@ga.gov.au>
2016-08-26 14:51:43 +1000

    update plot rgb recipe to mask out pixel where any band is 'saturated'

--- docs/user/recipes/plot_rgb.py
+++ docs/user/recipes/plot_rgb.py
@@ -11,10 +11,11 @@ dc = datacube.Datacube()
 data = dc.load(product='ls5_nbar_albers', measurements=['red', 'green', 'blue'], **query)
 data = mask_invalid_data(data)
 
-fake_saturation = 5000
+fake_saturation = 4000
 rgb = data.to_array(dim='color')
 rgb = rgb.transpose(*(rgb.dims[1:]+rgb.dims[:1]))  # make 'color' the last dimension
-rgb = rgb.where(rgb <= fake_saturation)/fake_saturation
+rgb = rgb.where((rgb <= fake_saturation).all(dim='color'))  # mask out pixels where any band is 'saturated'
+rgb /= fake_saturation #  scale to [0, 1] range for imshow
 
 rgb.plot.imshow(x=data.crs.dimensions[1], y=data.crs.dimensions[0],
                 col='time', col_wrap=5, add_colorbar=False)
