d68747ed0beda2be5faed89bdb427eef0e4eec7e
Damien Ayers <damien@omad.net>
2016-09-05 16:53:17 +1000

    Lots more movie maker cleaning

--- apps/sequencer/__init__.py
+++ apps/sequencer/__init__.py
@@ -3,15 +3,26 @@
 from __future__ import absolute_import, print_function
 
 import click
-import numpy as np
 
-from datacube.storage.masking import make_mask
+import xarray as xr
+import pandas as pd
+import numpy as np
+import rasterio
+
+from dateutil.rrule import YEARLY
+from datacube.api.geo_xarray import append_solar_day
+from datacube.storage.masking import make_mask, mask_valid_data as invalid_data_mask
 from datacube.ui import click as ui
 from datacube.ui.click import to_pathlib
 from datacube.utils import read_documents
 from datacube import Datacube
+from datacube.utils.dates import date_sequence
 
 REQUIRED_MEASUREMENTS = ['red', 'green', 'blue']
+
+DEFAULT_PRODUCTS = 'ls5_nbar_albers', 'ls7_nbar_albers', 'ls8_nbar_albers'
+
+VALID_BIT = 8  # GA Landsat PQ Contiguity Bit
 
 
 @click.command(name='sequencer')
@@ -20,103 +31,106 @@ REQUIRED_MEASUREMENTS = ['red', 'green', 'blue']
               help='configuration file location', callback=to_pathlib)
 @click.option('--load-bounds-from', type=click.Path(exists=True, readable=True, dir_okay=False),
               help='Shapefile to calculate boundary coordinates from.')
+@click.option('--start-date')
+@click.option('--end-date')
 @ui.global_cli_options
 @ui.executor_cli_options
-@ui.pass_index(app_name='agdc-stats')
-def sequencer(index, app_config, load_bounds_from, executor):
+@ui.parsed_search_expressions
+@ui.pass_index(app_name='agdc-moviemaker')
+def sequencer(index, app_config, load_bounds_from, start_date, end_date, products, executor, expressions):
+    products = products or DEFAULT_PRODUCTS
     _, config = next(read_documents(app_config))
 
+    jobname = 'foo'
+    job = {
+        'bottom': -1619354.555,
+        'left': 1188490.47,
+        'top': -1578182.723,
+        'right': 1213611.437
+    },
 
-    futures = [executor.submit(do_stats, task, config) for task in tasks]
+    left, right, top, bottom = job['left'], job['right'], job['top'], job['bottom']
 
-    for future in executor.as_completed(futures):
-        result = executor.result(future)
-        print(result)
+    results = []
+    for filenum, acq_range in enumerate(date_sequence(start_date, end_date, '1y', '1y')):
+        task = dict(output_name=jobname, filenum=filenum, products=products, time=acq_range, x=(left, right),
+                    y=(top, bottom))
+
+        result_future = executor.submit(write_median, **task)
+        results.append(result_future)
+
+    for result in executor.as_completed(results):
+        try:
+            msg = executor.result(result)
+            print(msg)
+        except MemoryError as e:
+            print(e)
+
+    print("Finished!")
+
+
+def write_median(filenum, output_name, **expression):
+    year, median = load_data(**expression)
+    filename = output_name + "_{:03d}_{}.png".format(filenum, year)
+    write_xarray_to_image(filename, median)
+    return 'Wrote {}.'.format(filename)
 
 
 def load_data(products, **parsed_expressions):
-    dc = Datacube()
-    #     test_no = 0
-    acq_range = parsed_expressions['time']
-    print("Processing time range {}".format(acq_range))
-    data = None
-    #     datasets_by_season = defaultdict(list)
-    datasets = []
+    with Datacube() as dc:
+        #     test_no = 0
+        acq_range = parsed_expressions['time']
+        print("Processing time range {}".format(acq_range))
+        data = None
+        #     datasets_by_season = defaultdict(list)
+        datasets = []
 
-    #     parsed_expressions['x'] = left, right
-    #     parsed_expressions['y'] = top, bottom
-    parsed_expressions['crs'] = 'EPSG:3577'
+        parsed_expressions['crs'] = 'EPSG:3577'
 
-    for prodname in products:
-        #         print("Loading data for {} during {}".format(prodname, acq_range), end="", flush=True)
-        dataset = dc.load(product=prodname,
-                          measurements=required_measurements,
-                          **parsed_expressions)
+        for prodname in products:
+            #         print("Loading data for {} during {}".format(prodname, acq_range), end="", flush=True)
+            dataset = dc.load(product=prodname,
+                              measurements=REQUIRED_MEASUREMENTS,
+                              group_by='solar_day',
+                              **parsed_expressions)
+            if len(dataset) == 0:
+                continue
+            else:
+                print("Found {} time slices of {} during {}.".format(len(dataset['time']), prodname, acq_range))
 
-        if len(dataset) == 0:
-            continue
-        else:
-            print("Found {} time slices of {} during {}.".format(len(dataset['time']), prodname, acq_range))
+            pq = dc.load(product=prodname.replace('nbar', 'pq'),
+                         group_by='solar_day',
+                         fuse_func=pq_fuser,
+                         **parsed_expressions)
 
-        crs = dataset.crs
-        dataset = dataset.where(dataset != -999)
-        dataset.attrs['product'] = prodname
-        dataset.attrs['crs'] = crs
+            crs = dataset.crs
+            dataset = dataset.where(dataset != -999)
+            dataset.attrs['product'] = prodname
+            dataset.attrs['crs'] = crs
 
-        dataset = mask_data_with_pq(dc, dataset, prodname, parsed_expressions)
+            cloud_free = make_mask(pq.pixelquality, ga_good_pixel=True)
+            dataset = dataset.where(cloud_free)
 
-        if len(dataset) == 0:
-            print("Nothing left after PQ masking")
-            continue
+            if len(dataset) == 0:
+                print("Nothing left after PQ masking")
+                continue
 
-        datasets.append(with_solar_day(dataset))
+            datasets.append(dataset)
 
-    # for season, dataset in by_season(data):  # Also merges to solar_day instead of time
-    #             print("{} days of capture during {}".format(len(dataset['solar_day']), season))
-    #             for i in range(len(dataset.solar_day)):
-    #                 write_xarray_to_file('test%s%s.png' % (test_no, dataset.solar_day[i].item()), dataset.isel(solar_day=i).squeeze())
-    #                 test_no += 1
+        dataset = xr.concat(datasets, dim='solar_day')
 
-    #             datasets_by_season[season].append(dataset)
-
-    #     datasets_by_season = {season: xr.concat(season_datasets, dim='solar_day') for season, season_datasets in datasets_by_season.items()}
-
-    dataset = xr.concat(datasets, dim='solar_day')
-
-    year = pd.Timestamp(dataset['solar_day'][0].data).year
+        year = pd.Timestamp(dataset['solar_day'][0].data).year
     return year, dataset.median(dim='solar_day')
 
 
-#     to_return = []
+def pq_fuser(dest, src):
+    valid_val = (1 << valid_bit)
 
-#     for code, season_name in SEASONS:
-#         if code not in datasets_by_season:
-#             continue
-#         ds = datasets_by_season[code].load()
+    no_data_dest_mask = ~(dest & valid_val).astype(bool)
+    np.copyto(dest, src, where=no_data_dest_mask)
 
-#         year = pd.Timestamp(ds['solar_day'][0].data).year
-#         median = ds.median(dim='solar_day')
-#         to_return.append((season_name, year, median))
-
-#     return to_return
-
-
-def mask_data_with_pq(dc, data, prodname, parsed_expressions):
-    mask_product_name = prodname.replace('nbar', 'pq')
-    pq = dc.load(product=mask_product_name,
-                 **parsed_expressions)
-    if len(pq) > 0:
-        cloud_free = make_mask(pq.pixelquality, ga_good_pixel=True)
-        masked_data = data.where(cloud_free)
-        masked_data.attrs = data.attrs
-    else:
-        print("No PQ Data found for {} {}".format(mask_product_name, parsed_expressions))
-    return masked_data
-
-
-def with_solar_day(year_data):
-    append_solar_day(year_data)
-    return year_data.groupby('solar_day').max(dim='time', keep_attrs=True)
+    both_data_mask = (valid_val & dest & src).astype(bool)
+    np.copyto(dest, src & dest, where=both_data_mask)
 
 
 def write_xarray_to_image(filename, dataset):
