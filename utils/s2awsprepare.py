ee1f875f5f3267527585f09317d8e2c63df9d994
Gregory Raevski <gregory.raevski@ga.gov.au>
2016-11-15 10:01:29 +1100

    remove unused code

--- utils/s2awsprepare.py
+++ utils/s2awsprepare.py
@@ -6,20 +6,10 @@ from __future__ import absolute_import
 
 import uuid
 import logging
-from xml.etree import ElementTree
-from pathlib import Path
 import yaml
 import json
 import click
 from osgeo import osr
-import os
-# image boundary imports
-import rasterio
-from rasterio.errors import RasterioIOError
-import rasterio.features
-import shapely.affinity
-import shapely.geometry
-import shapely.ops
 
 try:
   from urllib.request import urlopen
@@ -28,67 +18,6 @@ except ImportError:
   from urlparse import urlparse, urljoin
   from urllib2 import urlopen
 
-###IMAGE BOUNDARY CODE
-
-def safe_valid_region(images, mask_value=None):
-    try:
-        return valid_region(images, mask_value)
-    except (OSError, RasterioIOError):
-        return None
-
-
-def valid_region(images, mask_value=None):
-    mask = None
-    for fname in images:
-        ## ensure formats match
-        with rasterio.open(str(fname), 'r') as ds:
-            transform = ds.affine
-            img = ds.read(1)
-
-            if mask_value is not None:
-                new_mask = img & mask_value == mask_value
-            else:
-                #new_mask = img != ds.nodata
-                new_mask = img != 0
-            if mask is None:
-                mask = new_mask
-            else:
-                mask |= new_mask
-
-    shapes = rasterio.features.shapes(mask.astype('uint8'), mask=mask)
-    shape = shapely.ops.unary_union([shapely.geometry.shape(shape) for shape, val in shapes if val == 1])
-    type(shapes)
-    # convex hull
-    geom = shape.convex_hull
-
-    # buffer by 1 pixel
-    geom = geom.buffer(1, join_style=3, cap_style=3)
-
-    # simplify with 1 pixel radius
-    geom = geom.simplify(1)
-
-    # intersect with image bounding box
-    geom = geom.intersection(shapely.geometry.box(0, 0, mask.shape[1], mask.shape[0]))
-
-    # transform from pixel space into CRS space
-    geom = shapely.affinity.affine_transform(geom, (transform.a, transform.b, transform.d,
-                                                    transform.e, transform.xoff, transform.yoff))
-
-    output = shapely.geometry.mapping(geom)
-    
-    return geom
-    #output['coordinates'] = _to_lists(output['coordinates'])
-    #return output
-
-
-def _to_lists(x):
-    """
-    Returns lists of lists when given tuples of tuples
-    """
-    if isinstance(x, tuple):
-        return [_to_lists(el) for el in x]
-
-    return x
 
 def get_geo_ref_points(tileInfo):
     coords = tileInfo['tileGeometry']['coordinates'][0]
