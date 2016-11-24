e7db4daebe00dbd8c3c4cb95f261420d48988b36
Damien Ayers <damien@omad.net>
2016-11-24 18:24:06 +1100

    Fix formatting in SRTM DEM prepare scripts

--- utils/srtm_prepare.py
+++ utils/srtm_prepare.py
@@ -2,7 +2,8 @@
 """
 Ingest data from the command-line.
 
-python srtm_prepare.py --output Elevation_1secSRTM_DEMs_v1.0_DEM_Mosaic_dem1sv1_0.yaml /g/data/rr1/Elevation/NetCDF/1secSRTM_DEMs_v1.0/DEM/Elevation_1secSRTM_DEMs_v1.0_DEM_Mosaic_dem1sv1_0.nc
+python srtm_prepare.py --output Elevation_1secSRTM_DEMs_v1.0_DEM_Mosaic_dem1sv1_0.yaml \
+  /g/data/rr1/Elevation/NetCDF/1secSRTM_DEMs_v1.0/DEM/Elevation_1secSRTM_DEMs_v1.0_DEM_Mosaic_dem1sv1_0.nc
 """
 from __future__ import absolute_import
 
@@ -12,19 +13,21 @@ import yaml
 import click
 import netCDF4
 import os
+
+
 def prepare_layers(images):
-    layerdict={}
+    layerdict = {}
     for i in images:
         image = netCDF4.Dataset(i)
         layerpath = str(image.filepath())
         for targetlayer in image.variables.values():
             if targetlayer.name not in ['crs', 'lat', 'lon']:
                 layername = str(targetlayer.name)
-                layerdict[layername]= {'path': layerpath,'layer': layername,}
+                layerdict[layername] = {'path': layerpath, 'layer': layername, }
     return layerdict
 
-def prepare_dataset(image, datasets):
 
+def prepare_dataset(image, datasets):
     image = netCDF4.Dataset(image)
     projection = image.variables.values()[0].spatial_ref
     geotransform = ((str(image.variables.values()[0].GeoTransform)).split())
@@ -32,8 +35,8 @@ def prepare_dataset(image, datasets):
     lon_pixels = int(image.dimensions.values()[0].size)
     lat_pixels = int(image.dimensions.values()[1].size)
 
-    left, right = float(fgeotransform[0]), float(fgeotransform[0]+(lon_pixels*fgeotransform[1]))
-    bottom, top = float(fgeotransform[3]+(lat_pixels*fgeotransform[5])), float(fgeotransform[3])
+    left, right = float(fgeotransform[0]), float(fgeotransform[0] + (lon_pixels * fgeotransform[1]))
+    bottom, top = float(fgeotransform[3] + (lat_pixels * fgeotransform[5])), float(fgeotransform[3])
 
     return {
         'id': str(uuid.uuid4()),
@@ -73,11 +76,12 @@ def prepare_dataset(image, datasets):
 
 
 @click.command(help="Prepare single layer netcdf with common grid spec for ingestion to Data Cube.")
-@click.argument('datasets', type=click.Path(exists=True, readable=True),nargs=-1)
+@click.argument('datasets', type=click.Path(exists=True, readable=True), nargs=-1)
 @click.option('--output', help="Write datasets into this file", type=click.Path(exists=False, writable=True))
 def main(datasets, output):
     with open(output, 'w') as stream:
-        yaml.dump((prepare_dataset(datasets[0], datasets)),stream)
+        yaml.dump((prepare_dataset(datasets[0], datasets)), stream)
+
+
 if __name__ == "__main__":
     main()
-
