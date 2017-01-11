859634407d91c3e693363eb80a3f59b7979f3a9c
Gregory Raevski <gregory.raevski@ga.gov.au>
2017-01-11 15:17:21 +1100

    update line transect recipe

--- docs/user/recipes/line_transect.py
+++ docs/user/recipes/line_transect.py
@@ -1,10 +1,9 @@
 import fiona
 import numpy
-import rasterio
-import shapely.geometry
 import xarray
 
 import datacube
+from datacube.utils import geometry
 
 
 def transect(data, line, resolution, method='nearest', tolerance=None):
@@ -12,16 +11,18 @@ def transect(data, line, resolution, method='nearest', tolerance=None):
     Extract line transect from data along geom
 
     :param xarray.Dataset data: data loaded via `Datacube.load`
-    :param shapely.geometry.LineString line: line along which to extract the transect (CRS must match data.crs)
-    :param float resolution: interval used to extract points along the line (in CRS units)
+    :param datacube.utils.Geometry line: line along which to extract the transect
+    :param float resolution: interval used to extract points along the line (in data CRS units)
     :param str method: see xarray.Dataset.sel_points
     :param float tolerance: see xarray.Dataset.sel_points
     """
-    dist = numpy.arange(0, int(line.length), resolution)
-    points = zip(*[line.interpolate(d).coords[0] for d in dist])
+    assert line.type == 'LineString'
+    line = line.to_crs(data.crs)
+    dist = numpy.arange(0, line.length, resolution)
+    points = [line.interpolate(d).coords[0] for d in dist]
     indexers = {
-        data.crs.dimensions[0]: list(points[1]),
-        data.crs.dimensions[1]: list(points[0])
+        data.crs.dimensions[0]: [p[1] for p in points],
+        data.crs.dimensions[1]: [p[0] for p in points]
     }
     return data.sel_points(xarray.DataArray(dist, name='distance', dims=['distance']),
                            method=method,
@@ -29,28 +30,19 @@ def transect(data, line, resolution, method='nearest', tolerance=None):
                            **indexers)
 
 
-def warp_geometry(geom, src_crs, dst_crs):
-    """
-    warp geometry from src_crs to dst_crs
-    """
-    return shapely.geometry.shape(rasterio.warp.transform_geom(src_crs, dst_crs, shapely.geometry.mapping(geom)))
-
-
 def main():
     with fiona.open('line.shp') as shapes:
-        line_crs = str(shapes.crs_wkt)
-        line = shapely.geometry.shape(next(shapes)['geometry'])
+        crs = geometry.CRS(shapes.crs_wkt)
+        first_geometry = next(shapes)['geometry']
+        line = geometry.Geometry(first_geometry, crs=crs)
 
     query = {
         'time': ('1990-01-01', '1991-01-01'),
-        'x': (line.bounds[0], line.bounds[2]),
-        'y': (line.bounds[1], line.bounds[3]),
-        'crs': line_crs
+        'geopolygon': line
     }
 
     dc = datacube.Datacube(app='line-trans-recipe')
     data = dc.load(product='ls5_nbar_albers', measurements=['red'], **query)
 
-    line_albers = warp_geometry(line, query['crs'], data.crs.wkt)
-    trans = transect(data, line_albers, abs(data.affine.a))
+    trans = transect(data, line, abs(data.affine.a))
     trans.red.plot(x='distance', y='time')
