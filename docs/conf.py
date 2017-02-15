40ebe52a551b0658cae3a104e59f99ad3068e651
Gregory Raevski <gregory.raevski@ga.gov.au>
2017-02-15 11:09:49 +1100

    fix RTFM maybe

--- docs/conf.py
+++ docs/conf.py
@@ -349,6 +349,8 @@ class Mock(MagicMock):
 
 
 sys.modules.update((mod_name, Mock()) for mod_name in NATIVE_MODULES)
+sys.modules['rasterio.coords'].BoundingBox = Mock
+
 
 # Clean up generated documentation files that RTD seems to be having trouble with
 if on_rtd:
