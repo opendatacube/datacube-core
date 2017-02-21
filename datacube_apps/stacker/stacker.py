a4cea2ee91f875b2c07f8a0d74c6690ee5bb0293
andrewdhicks <andrew.hicks@ga.gov.au>
2017-02-21 15:16:01 +1100

    Update stacker to write correct file metadata

--- datacube_apps/stacker/stacker.py
+++ datacube_apps/stacker/stacker.py
@@ -1,24 +1,28 @@
 """
-Create statistical summaries command
+Create time-stacked NetCDF files
 
 """
 from __future__ import absolute_import, print_function
 
+import copy
+import datetime
+import itertools
 import logging
-import re
 import os
 from functools import partial
 
 import click
 import dask.array as da
 import dask
+from dateutil import tz
 import pandas as pd
 from pandas.tseries.offsets import YearBegin, YearEnd
 from pathlib import Path
+import xarray as xr
 
 import datacube
 from datacube.api import Tile
-from datacube.model.utils import make_dataset, xr_apply, datasets_to_doc
+from datacube.model.utils import xr_apply, datasets_to_doc
 from datacube.storage import netcdf_writer
 from datacube.storage.storage import create_netcdf_storage_unit
 from datacube.ui import task_app
@@ -32,51 +36,30 @@ APP_NAME = 'datacube-stacker'
 
 
 def get_filename(config, cell_index, year):
-    file_path_template = str(Path(config['location'], config['file_path_template']))
-    return file_path_template.format(tile_index=cell_index, start_time=year)
-
-
-def _stacked_log_entry(stacked, unstacked):
-    if unstacked:
-        if stacked:
-            msg = ('is already stacked ({stacked}, '
-                   'but has new data that needs to be stacked ({unstacked})')
-        else:
-            msg = 'needs to be stacked'
-    else:
-        if stacked:
-            msg = 'is already stacked'
-        else:
-            msg = 'is empty'
-    return msg
+    file_path_template = str(Path(config['location'], config['stacked_file_path_template']))
+    return file_path_template.format(tile_index=cell_index, start_time=year, version=config['taskfile_version'])
 
 
 def make_stacker_tasks(index, config, **kwargs):
-    filename_timestamp_pattern = r'\/(?:\w+)_-?\d+_-?\d+_(\d+).nc$'  # TODO: Get from config?
-
     product = config['product']
-
     query = {kw: arg for kw, arg in kwargs.items() if kw in ['time', 'cell_index'] and arg is not None}
 
     gw = datacube.api.GridWorkflow(index=index, product=product.name)
     cells = gw.list_cells(product=product.name, **query)
     for (cell_index, tile) in cells.items():
         for (year, year_tile) in _split_by_year(tile):
-            stacked, unstacked = _get_stacked_datasets(year_tile, year, filename_timestamp_pattern)
-            msg = _stacked_log_entry(stacked, unstacked).format(stacked=len(stacked), unstacked=len(unstacked))
-            _LOG.debug('%s, %s: (with %s) %s', cell_index, year, len(year_tile.sources), msg)
-
-            if unstacked:
+            storage_files = set(ds.local_path for ds in itertools.chain(*year_tile.sources.values))
+            if len(storage_files) > 1:
                 year_tile = gw.update_tile_lineage(year_tile)
                 output_filename = get_filename(config, cell_index, year)
+                _LOG.info('Stacking required for: year=%d, cell=%s. Output=%s', year, cell_index, output_filename)
                 yield dict(year=year,
                            tile=year_tile,
                            cell_index=cell_index,
-                           output_filename=output_filename,
-                           global_attributes=config['global_attributes'],
-                           variable_params=config['variable_params'],
-                           app_config_file=config['app_config_file']
-                          )
+                           output_filename=output_filename)
+            elif len(storage_files) == 1:
+                [only_filename] = storage_files
+                _LOG.info('Stacking not required for: year=%d, cell=%s. existing=%s', year, cell_index, only_filename)
 
 
 def make_stacker_config(index, config, export_path=None, **query):
@@ -103,6 +86,8 @@ def make_stacker_config(index, config, export_path=None, **query):
 
     config['variable_params'] = variable_params
 
+    config['taskfile_version'] = int(datacube.utils.datetime_to_seconds_since_1970(datetime.datetime.now()))
+
     return config
 
 
@@ -119,83 +104,40 @@ def _split_by_year(tile, time_dim='time'):
         yield year_str, Tile(sources=sources_slice, geobox=tile.geobox)
 
 
-def _get_stacked_datasets(tile, stacked_epoch, pattern):
-    ds_stacked = set()
-    ds_unstacked = set()
-    for timeslice in tile.sources:
-        ts_sources = timeslice.item()
-        if len(ts_sources) != 1:
-            raise ValueError('Cannot handle multiple source datasets for a single non-spatial coordinate')
-        ds = ts_sources[0]
-        if _is_pattern_match(ds.local_path, pattern) == stacked_epoch:
-            ds_stacked.add(ds)
-        else:
-            ds_unstacked.add(ds)
-    return ds_stacked, ds_unstacked
+def get_history_attribute(config, task):
+    return '{dt} {user} {app} ({ver}) {args}  # {comment}'.format(
+        dt=datetime.datetime.now(tz.tzlocal()).isoformat(),
+        user=os.environ.get('USERNAME') or os.environ.get('USER'),
+        app=APP_NAME,
+        ver=datacube.__version__,
+        args=', '.join([config['app_config_file'],
+                        str(config['version']),
+                        str(config['taskfile_version']),
+                        task['output_filename'],
+                        str(task['year']),
+                        str(task['cell_index'])
+                       ]),
+        comment='Stacking datasets for a year into a single NetCDF file'
+    )
 
 
-def _is_pattern_match(path, pattern):
-    result = re.search(pattern, str(path))
-    if len(result.groups()) != 1:
-        return None
-    return result.groups()[0]
-
-
-def make_datasets(tile, file_path, config):
-    def _make_dataset(labels, sources):
-        new_dataset = make_dataset(product=tile.product,
-                                   sources=sources,
-                                   extent=tile.geobox.extent,
-                                   center_time=labels['time'],
-                                   uri=file_path.absolute().as_uri(),
-                                   app_info=get_app_metadata(config),
-                                   valid_data=sources[0].extent)
-        return new_dataset
-
-    return xr_apply(tile.sources, _make_dataset, dtype='O')
-
-
-def get_app_metadata(config):
-    doc = {
-        'lineage': {
-            'algorithm': {
-                'name': 'datacube-stacker',
-                'version': datacube.__version__,
-                'repo_url': 'https://github.com/data-cube/agdc-v2.git',
-                'parameters': {'configuration_file': config.get('app_config_file', 'unknown')}
-            },
-        }
-    }
-    return doc
-
-
-def _single_dataset(labels, dataset_list):
+def _unwrap_dataset_list(labels, dataset_list):
     return dataset_list[0]
 
 
-def do_stack_task(task):
-    datasets_to_add = None
-    datasets_to_update = None
-    datasets_to_archive = None
+def do_stack_task(config, task):
+    global_attributes = config['global_attributes']
+    global_attributes['history'] = get_history_attribute(config, task)
 
-    global_attributes = task['global_attributes']
-    variable_params = task['variable_params']
+    variable_params = config['variable_params']
 
     output_filename = Path(task['output_filename'])
     tile = task['tile']
 
-    if task.get('make_new_datasets', False):
-        datasets_to_add = make_datasets(tile, output_filename, task)
-        datasets_to_archive = xr_apply(tile.sources, _single_dataset, dtype='O')
+    data = datacube.api.GridWorkflow.load(tile, dask_chunks=config['storage']['chunking'])
 
-        output_datasets = datasets_to_add
-    else:
-        datasets_to_update = xr_apply(tile.sources, _single_dataset, dtype='O')
-
-        output_datasets = datasets_to_update
-
-    data = datacube.api.GridWorkflow.load(tile, dask_chunks=dict(time=1))  # TODO: chunk along output NetCDF chunk?
-    data['dataset'] = datasets_to_doc(output_datasets)
+    unwrapped_datasets = xr_apply(tile.sources, _unwrap_dataset_list, dtype='O')
+    data['dataset'] = datasets_to_doc(unwrapped_datasets)
 
     nco = create_netcdf_storage_unit(output_filename,
                                      data.crs,
@@ -213,30 +155,31 @@ def do_stack_task(task):
         nco.sync()
 
     nco.close()
-    return datasets_to_add, datasets_to_update, datasets_to_archive
+
+    def update_dataset_location(labels, dataset):
+        new_dataset = copy.copy(dataset)
+        new_dataset.local_uri = output_filename.absolute().as_uri()
+        return [dataset]
+
+    updated_datasets = xr_apply(unwrapped_datasets, update_dataset_location, dtype='O')
+    new_tile = datacube.api.Tile(sources=updated_datasets, geobox=tile.geobox)
+
+    new_data = datacube.api.GridWorkflow.load(new_tile, dask_chunks=config['storage']['chunking'])
+
+    if not data.identical(new_data):
+        _LOG.error("Mismatch found for %s, not indexing", output_filename)
+        raise ValueError("Mismatch found for %s, not indexing" % output_filename)
+
+    return unwrapped_datasets, output_filename.absolute().as_uri()
 
 
 def process_result(index, result):
-    datasets_to_add, datasets_to_update, datasets_to_archive = result
-
-    for dataset in datasets_to_add:
-        index.datasets.add(dataset, sources_policy='skip')
-        _LOG.info('Dataset added')
-
-    for dataset in datasets_to_update:
-        index.datasets.update(dataset)
-        _LOG.info('Dataset updated')
-
-    files_to_archive = set()
-    for dataset in datasets_to_archive:
-        files_to_archive.add(dataset.local_path)
-        index.datasets.archive(dataset.id)
-
-    # for file_path in files_to_archive:
-    #     try:
-    #         file_path.unlink()
-    #     except OSError as e:
-    #         _LOG.warning('Could not delete file: %s', e)
+    datasets, new_uri = result
+    for dataset in datasets.values:
+        _LOG.info('Updating dataset location: %s', dataset.local_path)
+        old_uri = dataset.local_uri
+        index.datasets.add_location(dataset, new_uri)
+        index.datasets.remove_location(dataset, old_uri)  # TODO: archive_location
 
 
 def do_nothing(*args, **kwargs):
