f7be8e532e195c8fe7cd74d889274aea01e282cf
Kirill Kouzoubov <kirill888@gmail.com>
2017-05-01 19:31:50 +1000

    pep8 fixes for dummy task app

--- examples/dummy_task_app.py
+++ examples/dummy_task_app.py
@@ -10,7 +10,7 @@ from datacube.ui.task_app import task_app, task_app_options, run_tasks, wrap_tas
 
 APP_NAME = 'dummy'
 
-#pylint: disable=invalid-name
+# pylint: disable=invalid-name
 
 
 def random_sleep(amount_secs=0.1, prop=0.5):
@@ -26,6 +26,7 @@ def unused(*_, **_unused):
     "Used to silence pylint warnings"
     pass
 
+
 def make_config(db_index, config, **opts):
     """Called after parsing command line arguments and initialising database index.
 
@@ -38,7 +39,7 @@ def make_config(db_index, config, **opts):
     click.echo(opts)
     click.echo("------------------------------")
 
-    #in real program you might need these
+    # in real program you might need these
     unused(db_index)
 
     # Override config value with command line value, or set to default value of 10
@@ -46,6 +47,7 @@ def make_config(db_index, config, **opts):
                                    config.get('num_tasks', 10))
 
     return config
+
 
 def make_tasks(db_index, config, **opts):
     """ Generate a task list.
@@ -58,7 +60,7 @@ def make_tasks(db_index, config, **opts):
 
     for i in range(num_tasks):
         print('Generating task: {}'.format(i))
-        yield {'val':i}
+        yield {'val': i}
 
 
 def run_task(task, op):
@@ -69,7 +71,7 @@ def run_task(task, op):
     ops = {'sqrt': sqrt,
            'pow2': lambda x: x*x}
 
-    random_sleep(1, 0.1) #Sleep for 1 second 10% of the time
+    random_sleep(1, 0.1)  # Sleep for 1 second 10% of the time
 
     val = task['val']
 
@@ -80,8 +82,8 @@ def run_task(task, op):
     result = ops[op](val)
     click.echo('{} => {}'.format(val, result))
 
-
     return result
+
 
 @click.command(name=APP_NAME)
 @ui.pass_index(app_name=APP_NAME)
@@ -107,6 +109,7 @@ def app_main(db_index, config, tasks, executor, **opts):
     run_tasks(tasks, executor, task_runner, queue_size=10)
 
     return 0
+
 
 if __name__ == '__main__':
     # pylint: disable=no-value-for-parameter
