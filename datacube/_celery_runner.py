2a3d63aa99bab25d10ed2d72b30c9f680eec4ef2
Kirill Kouzoubov <kirill888@gmail.com>
2017-05-04 09:47:48 +1000

    Celery runner launches redis when needed
    Also shutdowns celery workers and redis when done, including redis-server as
    part of the environment.

--- datacube/_celery_runner.py
+++ datacube/_celery_runner.py
@@ -1,22 +1,28 @@
 from celery import Celery
+from time import sleep
+import redis
 import os
 
-#TODO: make this configureable
+# This can be changed via environment variable `REDIS`
 REDIS_URL = 'redis://localhost:6379/0'
+
 
 def mk_celery_app():
     url = os.environ.get('REDIS', REDIS_URL)
-    return Celery('datacube_task', broker=url, backend=url)
 
-#Celery worker launch script expects to see app object at the top level
-#pylint: disable=invalid-name
+    app = Celery('datacube_task', broker=url, backend=url)
+
+    app.conf.update(
+        task_serializer='pickle',
+        result_serializer='pickle',
+        accept_content=['pickle'])
+
+    return app
+
+
+# Celery worker launch script expects to see app object at the top level
+# pylint: disable=invalid-name
 app = mk_celery_app()
-
-app.conf.update(
-    task_serializer='pickle',
-    result_serializer='pickle',
-    accept_content=['pickle']
-)
 
 
 @app.task()
@@ -25,10 +31,12 @@ def run_cloud_pickled_function(f_data, *args, **kwargs):
     func = loads(f_data)
     return func(*args, **kwargs)
 
+
 def submit_cloud_pickled_function(f, *args, **kwargs):
     from cloudpickle import dumps
     f_data = dumps(f)
     return run_cloud_pickled_function.delay(f_data, *args, **kwargs)
+
 
 def launch_worker(argv=None):
     import sys
@@ -40,6 +48,7 @@ def launch_worker(argv=None):
 class CeleryExecutor(object):
     def __init__(self, host=None, port=None):
         # print('Celery: {}:{}'.format(host, port))
+        self._shutdown = None
 
         if port or host:
             db = '0'
@@ -49,6 +58,20 @@ class CeleryExecutor(object):
             app.conf.update(result_backend=url,
                             broker_url=url)
 
+        host = host if host else 'localhost'
+        port = port if port else 6379
+
+        if not check_redis(host, port):
+            if host in ['localhost', '127.0.0.1']:
+                self._shutdown = launch_redis(port if port else 6379)
+            else:
+                raise ConnectionError("Can't connect to redis server @ {}:{}".format(host, port))
+
+    def __del__(self):
+        if self._shutdown:
+            app.control.shutdown()
+            sleep(1)
+            self._shutdown()
 
     def __repr__(self):
         return 'CeleryRunner'
@@ -88,9 +111,9 @@ class CeleryExecutor(object):
                     pending.append(promise)
 
             if len(pending) == len(futures):
-                #If no change detected sleep for a bit
-                #TODO: this is sub-optimal, not sure what other options are
-                #      though?
+                # If no change detected sleep for a bit
+                # TODO: this is sub-optimal, not sure what other options are
+                #       though?
                 sleep(0.1)
 
             futures = pending
@@ -115,3 +138,71 @@ class CeleryExecutor(object):
     @staticmethod
     def release(future):
         future.forget()
+
+
+def check_redis(host='localhost', port=6379):
+    server = redis.Redis(host, port)
+    try:
+        server.ping()
+    except redis.exceptions.ConnectionError:
+        return False
+    return True
+
+
+def launch_redis(port=6379, params=None, **kwargs):
+    import tempfile
+    from os import path
+    import subprocess
+    import shutil
+    from time import sleep
+
+    def stringify(v):
+        if isinstance(v, str):
+            return '"'+v+'"' if v.find(' ') >= 0 else v
+
+        if isinstance(v, bool):
+            return {True: 'yes', False: 'no'}[v]
+
+        return str(v)
+
+    def write_config(params, cfgfile):
+        lines = ['{} {}\n'.format(k, stringify(v)) for k, v in params.items()]
+        with open(cfgfile, "w") as f:
+            f.writelines(lines)
+
+    workdir = tempfile.mkdtemp(prefix='redis-')
+
+    defaults = dict({'maxmemory-policy': 'noeviction'},
+                    daemonize=True,
+                    port=port,
+                    databases=4,
+                    maxmemory="100mb",
+                    hz=50,
+                    loglevel='notice',
+                    pidfile=path.join(workdir, 'redis.pid'),
+                    logfile=path.join(workdir, 'redis.log'))
+
+    if params:
+        defaults.update(params)
+
+    defaults.update(kwargs)
+
+    cfgfile = path.join(workdir, 'redis.cfg')
+    write_config(defaults, cfgfile)
+
+    def cleanup():
+        shutil.rmtree(workdir)
+
+    def shutdown():
+        server = redis.Redis('localhost', port)
+        server.shutdown()
+        sleep(1)
+        cleanup()
+
+    try:
+        subprocess.check_call(['redis-server', cfgfile])
+    except subprocess.CalledProcessError:
+        cleanup()
+        return False
+
+    return shutdown
