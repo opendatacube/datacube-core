30d9dcbf0b1e082bf808a0d4a644cbf359d73583
Kirill Kouzoubov <kirill888@gmail.com>
2017-05-17 11:32:16 +1000

    More test for celery executor
    So far testing task submission only, no worker is started in the tests.

--- tests/test_celery_runner.py
+++ tests/test_celery_runner.py
@@ -5,46 +5,46 @@ Tests for datacube._celery_runner
 from time import sleep
 from datacube import _celery_runner as cr
 
+PORT = 29374
+PASS = 'dfhksdjh23iuervao'
+WRONG_PASS = 'sdfghdfjsghjdfiueuiwei'
+REDIS_WAIT = 0.5
+
 
 def test_launch_redis_no_password():
-    PORT = 29374
     redis_stop = cr.launch_redis(PORT, password=None, loglevel='verbose')
     assert redis_stop is not None
 
-    sleep(1)
+    sleep(REDIS_WAIT)
     is_running = cr.check_redis(port=PORT)
     assert is_running is True
 
     redis_stop()
-    sleep(1)
+    sleep(REDIS_WAIT)
     is_running = cr.check_redis(port=PORT)
     assert is_running is False
 
 
 def test_launch_redis_with_config_password():
-    PORT = 29374
     redis_stop = cr.launch_redis(PORT, password='', loglevel='verbose')
     assert redis_stop is not None
 
-    sleep(1)
+    sleep(REDIS_WAIT)
     is_running = cr.check_redis(port=PORT, password='')
     assert is_running is True
 
     redis_stop()
-    sleep(1)
+    sleep(REDIS_WAIT)
     is_running = cr.check_redis(port=PORT, password='')
     assert is_running is False
 
 
 def test_launch_redis_with_custom_password():
-    PORT = 29374
-    PASS = 'dfhksdjh23iuervao'
-    WRONG_PASS = 'sdfghdfjsghjdfiueuiwei'
 
     redis_stop = cr.launch_redis(PORT, password=PASS, loglevel='verbose')
     assert redis_stop is not None
 
-    sleep(1)
+    sleep(REDIS_WAIT)
     is_running = cr.check_redis(port=PORT, password=PASS)
     assert is_running is True
 
@@ -52,6 +52,30 @@ def test_launch_redis_with_custom_password():
     assert is_running is False
 
     redis_stop()
-    sleep(1)
+    sleep(REDIS_WAIT)
     is_running = cr.check_redis(port=PORT, password=PASS)
     assert is_running is False
+
+
+def _echo(x):
+    return x
+
+
+def test_celery_noop():
+    DATA = [1, 2, 3, 4]
+    runner = cr.CeleryExecutor(host='localhost', port=PORT)
+    future = runner.submit(_echo, 0)
+
+    assert future.ready() is False
+    runner.release(future)
+
+    futures = runner.map(_echo, DATA)
+    assert len(futures) == len(DATA)
+
+    completed, failed, pending = runner.get_ready(futures)
+
+    assert len(completed) == 0
+    assert len(failed) == 0
+    assert len(pending) == len(DATA)
+
+    del runner
