f13aaf2d017f9e415812ba00f0ac17f03831b71b
Kirill Kouzoubov <kirill888@gmail.com>
2017-05-19 17:48:44 +1000

    Making redis tests optional
    If no redis-server is found on the test machine, then don't run tests that
    require redis-server

--- tests/test_celery_runner.py
+++ tests/test_celery_runner.py
@@ -14,6 +14,18 @@ WRONG_PASS = 'sdfghdfjsghjdfiueuiwei'
 REDIS_WAIT = 0.5
 
 
+def check_redis_binary():
+    try:
+        return subprocess.check_call(['redis-server', '--version']) == 0
+    except:
+        return False
+
+
+have_redis = check_redis_binary()
+skip_if_no_redis = pytest.mark.skipif(not have_redis, reason="Needs redis-server to run")
+
+
+@skip_if_no_redis
 def test_launch_redis_no_password():
     redis_stop = cr.launch_redis(PORT, password=None, loglevel='verbose')
     assert redis_stop is not None
@@ -28,6 +40,7 @@ def test_launch_redis_no_password():
     assert is_running is False
 
 
+@skip_if_no_redis
 def test_launch_redis_with_config_password():
     redis_stop = cr.launch_redis(PORT, password='', loglevel='verbose')
     assert redis_stop is not None
@@ -42,6 +55,7 @@ def test_launch_redis_with_config_password():
     assert is_running is False
 
 
+@skip_if_no_redis
 def test_launch_redis_with_custom_password():
 
     redis_stop = cr.launch_redis(PORT, password=PASS, loglevel='verbose')
@@ -60,6 +74,8 @@ def test_launch_redis_with_custom_password():
     assert is_running is False
 
 
+@pytest.mark.skip(reason="This test can hang if machine it runs on isn't setup appropriately")
+@skip_if_no_redis
 def test_celery_with_worker():
     DATA = [1, 2, 3, 4]
 
