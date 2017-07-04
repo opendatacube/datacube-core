6578fd40a65427271d35ea96f1582cdc8056f009
Damien Ayers <damien@omad.net>
2017-07-04 18:35:18 +1000

    Assert warnings for deprecated code

--- integration_tests/index/test_config_docs.py
+++ integration_tests/index/test_config_docs.py
@@ -240,7 +240,8 @@ def test_update_dataset(index, ls5_telem_doc, example_ls5_nbar_metadata_doc):
     doc = copy.deepcopy(updated.metadata_doc)
     doc['product_type'] = 'foobar'
     # Backwards compat: third argument was a single local uri.
-    update = Dataset(ls5_telem_type, doc, 'file:///test/doc4.yaml')
+    with pytest.warns(DeprecationWarning):
+        update = Dataset(ls5_telem_type, doc, 'file:///test/doc4.yaml')
     index.datasets.update(update, {('product_type',): changes.allow_any})
     updated = index.datasets.get(dataset.id)
     assert updated.metadata_doc['test1'] == {'some': 'thing'}
