f275f3b26181dfa4ae2db9cad67eb24267c80e07
Damien Ayers <damien@omad.net>
2017-07-27 13:14:23 +1000

    Fix sphinx errors from partially configured latex options

--- docs/conf.py
+++ docs/conf.py
@@ -232,20 +232,20 @@ htmlhelp_basename = 'ODCdoc'
 
 latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
-    #'papersize': 'letterpaper',
+    'papersize': 'a4paper',
 
     # The font size ('10pt', '11pt' or '12pt').
-    #'pointsize': '10pt',
+    'pointsize': '10pt',
 
     # Additional stuff for the LaTeX preamble.
-    #'preamble': '',
+    'preamble': '',
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    ('index', 'ODC.tex', u'Open Data Cube Documentation')
+    ('index', 'ODC.tex', 'Open Data Cube Documentation', 'ODC Members', 'manual')
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
