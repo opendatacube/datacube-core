72027a74a54206c3c150f37177972e70ee77d7dd
Imam Alam <imam.alam@gmail.com>
2017-09-06 15:38:54 +1000

    Explain the purpose of the test
    Add comment that mentions the interaction of `DriverManager` with
    logging and pickling.

--- integration_tests/test_gridworkflow.py
+++ integration_tests/test_gridworkflow.py
@@ -1,6 +1,10 @@
 from datacube.api.grid_workflow import GridWorkflow
 
 
+# The preliminary implementation of `datacube.drivers.manager.DriverManager`
+# had a member logger instance that prevented it from being pickled.
+# Creating a `GridWorkflow` instance also failed in that case, breaking
+# the `datacube-stats` application, among other things.
 def test_create_gridworkflow_with_logging(index):
     from logging import getLogger, StreamHandler
 
