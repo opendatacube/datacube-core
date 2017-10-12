d2b241324127d2582cb0342decdc280e630afd4e
Jeremy Hooke <jez@stulk.com>
2017-10-13 10:51:19 +1100

    Move datacube-worker inside the main datacube package
    It is called by other modules in the main datacube package
    (the celery/distributed/etc executors), so should be in the
     package, not as a separate "app".

--- setup.py
+++ setup.py
@@ -89,7 +89,7 @@ setup(
             'datacube-search = datacube.scripts.search_tool:cli',
             'datacube = datacube.scripts.cli_app:cli',
             'datacube-stacker = datacube_apps.stacker:main',
-            'datacube-worker = datacube_apps.worker:main',
+            'datacube-worker = datacube.execution.worker:main',
             'datacube-fixer = datacube_apps.stacker:fixer_main',
             'datacube-ncml = datacube_apps.ncml:ncml_app',
             'pixeldrill = datacube_apps.pixeldrill:main [interactive]',
