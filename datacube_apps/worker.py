9e683c304fc3025b0cb4be351b4b069b23a5e80f
Jeremy Hooke <jez@stulk.com>
2017-10-13 17:22:05 +1100

    Add backwards compatibility shim for datacube worker

---
+++ datacube_apps/worker.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import
+from datacube.execution import worker
+
+import warnings
+
+
+def main():
+    warnings.warn('datacube_apps.worker should now '
+                  'be imported as datacube.execution.worker',
+                  DeprecationWarning, stacklevel=2)
+    return worker.main()
+
+
+if __name__ == '__main__':
+    main()
