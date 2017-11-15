dd8717e4d87e914b35aef86da189a8238f3356ae
Jeremy Hooke <jez@stulk.com>
2017-11-15 11:45:10 +1100

    Improve native field descriptions

--- datacube/index/postgres/_api.py
+++ datacube/index/postgres/_api.py
@@ -90,7 +90,7 @@ def get_native_fields():
     fields = {
         'id': NativeField(
             'id',
-            None,
+            'Dataset UUID',
             DATASET.c.id
         ),
         'product': NativeField(
@@ -105,7 +105,7 @@ def get_native_fields():
         ),
         'metadata_type': NativeField(
             'metadata_type',
-            'Metadata type of dataset',
+            'Metadata type name of dataset',
             METADATA_TYPE.c.name
         ),
         'metadata_type_id': NativeField(
