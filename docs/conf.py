7db857547c2472659247373839e2bde531cdfa1c
Damien Ayers <damien@omad.net>
2018-01-05 16:57:52 +1100

    Use vector output format for plantuml

--- docs/conf.py
+++ docs/conf.py
@@ -308,6 +308,8 @@ texinfo_documents = [
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
 
+plantuml_output_format = 'svg'
+plantuml_latex_output_format = 'pdf'
 
 
 # Mock modules that need native libraries.
