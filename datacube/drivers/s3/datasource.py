9129094222cfaa7c7abfd9897fe6890ce197740a
Damien Ayers <damien@omad.net>
2018-02-08 16:30:06 +1100

    Use multiproc load for s3 data
    Point 5

--- datacube/drivers/s3/datasource.py
+++ datacube/drivers/s3/datasource.py
@@ -83,13 +83,13 @@ class S3Source(object):
         slices = (slice(indexes, indexes + 1),) + slices
 
         self.logger.debug('Retrieving data from s3 (%s, slices: %s)', s3_dataset.base_name, slices)
-        return self.storage.get_data_unlabeled(s3_dataset.base_name,
-                                               s3_dataset.macro_shape,
-                                               s3_dataset.chunk_size,
-                                               dtype(s3_dataset.numpy_type),
-                                               slices,
-                                               s3_dataset.bucket,
-                                               True)[0]
+        return self.storage.get_data_unlabeled_mp(s3_dataset.base_name,
+                                                  s3_dataset.macro_shape,
+                                                  s3_dataset.chunk_size,
+                                                  dtype(s3_dataset.numpy_type),
+                                                  slices,
+                                                  s3_dataset.bucket,
+                                                  True)[0]
 
 
 class S3DataSource(DataSource):
