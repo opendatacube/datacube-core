3add82d043a65ff36b9d3602dc57f411417dc8ee
Damien Ayers <damien@omad.net>
2018-02-08 16:24:09 +1100

    Disable latex/pdf docs, is broken in latest sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -232,23 +232,23 @@ htmlhelp_basename = 'ODCdoc'
 
 # -- Options for LaTeX output ---------------------------------------------
 
-latex_elements = {
-    # The paper size ('letterpaper' or 'a4paper').
-    #'papersize': 'letterpaper',
-
-    # The font size ('10pt', '11pt' or '12pt').
-    #'pointsize': '10pt',
-
-    # Additional stuff for the LaTeX preamble.
-    #'preamble': '',
-}
+#latex_elements = {
+#    # The paper size ('letterpaper' or 'a4paper').
+#    #'papersize': 'letterpaper',
+#
+#    # The font size ('10pt', '11pt' or '12pt').
+#    #'pointsize': '10pt',
+#
+#    # Additional stuff for the LaTeX preamble.
+#    #'preamble': '',
+#}
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
-latex_documents = [
-    ('index', 'ODC.tex', u'Open Data Cube Documentation')
-]
+#latex_documents = [
+#    ('index', 'ODC.tex', u'Open Data Cube Documentation')
+#]
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
