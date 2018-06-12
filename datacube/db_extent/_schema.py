720198e7cbe82c544106d1ed1dac362df43f0110
Ashoka Jayawardena <Ashoka.Jayawardena@ga.gov.au>
2018-06-12 14:08:39 +1000

    Integrated extent, extent_meta, and ranges tables to datacube schema and migration scripts.

--- datacube/db_extent/_schema.py
+++ datacube/db_extent/_schema.py
@@ -1,45 +1,18 @@
-from sqlalchemy import create_engine, SmallInteger, String, DateTime
-from sqlalchemy import ForeignKey, UniqueConstraint
-from sqlalchemy.schema import Column, Table
-from sqlalchemy.dialects import postgresql as postgres
-from datacube.drivers.postgres._schema import DATASET_TYPE
-from datacube.drivers.postgres._core import METADATA
+from sqlalchemy import create_engine
+from datacube.drivers.postgres._schema import EXTENT, EXTENT_META, RANGES
 
-
-EXTENT_META = Table('extent_meta', METADATA,
-                    Column('id', SmallInteger, primary_key=True, autoincrement=True),
-                    Column('dataset_type_ref', None, ForeignKey(DATASET_TYPE.c.id), nullable=False),
-                    Column('start', DateTime(timezone=True), nullable=False),
-                    Column('end', DateTime(timezone=True), nullable=False),
-                    Column('offset_alias', String, nullable=False),
-                    Column('crs', String, nullable=True),
-
-                    UniqueConstraint('dataset_type_ref', 'offset_alias')
-                   )
-
-EXTENT = Table('extent', METADATA,
-               Column('id', postgres.UUID(as_uuid=True), primary_key=True),
-               Column('extent_meta_ref', None, ForeignKey(EXTENT_META.c.id), nullable=False),
-               Column('start', DateTime(timezone=True), nullable=False),
-               Column('geometry', postgres.JSONB, nullable=True),
-
-               UniqueConstraint('extent_meta_ref', 'start')
-              )
-
-RANGES = Table('ranges', METADATA,
-               Column('id', SmallInteger, primary_key=True, autoincrement=True),
-               Column('dataset_type_ref', None, ForeignKey(DATASET_TYPE.c.id), nullable=False),
-               Column('start', DateTime(timezone=True), nullable=False),
-               Column('end', DateTime(timezone=True), nullable=False),
-               Column('bounds', postgres.JSONB, nullable=True),
-               Column('crs', String, nullable=True),
-              )
 
 if __name__ == '__main__':
     ENGINE = create_engine('postgresql://aj9439@agdcdev-db.nci.org.au:6432/datacube')
-    # Create the extent_meta table
-    EXTENT_META.create(bind=ENGINE)
-    # Create the extent table
-    EXTENT.create(bind=ENGINE)
-    # Create ranges table
-    RANGES.create(bind=ENGINE)
+    with ENGINE.connect() as conn:
+        TRANS = conn.begin()
+        try:
+            # Create the extent_meta table
+            EXTENT_META.create(bind=conn)
+            # Create the extent table
+            EXTENT.create(bind=conn)
+            # Create ranges table
+            RANGES.create(bind=conn)
+            TRANS.commit()
+        except:  # pylint: disable=bare-except
+            TRANS.rollback()
