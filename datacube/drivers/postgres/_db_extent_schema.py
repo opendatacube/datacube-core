06e9e9f0e62fd0b336f8f96f258f679c7d4de742
Ashoka Jayawardena <Ashoka.Jayawardena@ga.gov.au>
2018-06-12 14:08:39 +1000

    Integrated extent, extent_meta, and ranges tables to datacube schema and migration scripts.

--- datacube/drivers/postgres/_db_extent_schema.py
+++ datacube/drivers/postgres/_db_extent_schema.py
@@ -1,45 +1,51 @@
-from sqlalchemy import create_engine, SmallInteger, String, DateTime
-from sqlalchemy import ForeignKey, UniqueConstraint
-from sqlalchemy.schema import Column, Table
-from sqlalchemy.dialects import postgresql as postgres
-from ._schema import DATASET_TYPE
-from ._core import METADATA
+from sqlalchemy import MetaData, create_engine
+from sqlalchemy.schema import Table
+from sqlalchemy.pool import NullPool
+from sqlalchemy.engine import reflection
+import warnings
+
+SQL_NAMING_CONVENTIONS = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+    # Other prefixes handled outside of sqlalchemy:
+    # dix: dynamic-index, those indexes created automatically based on search field configuration.
+    # tix: test-index, created by hand for testing, particularly in dev.
+}
+SCHEMA_NAME = 'agdc'
+ENGINE = create_engine('postgresql://aj9439@agdcdev-db.nci.org.au:6432/datacube', poolclass=NullPool)
 
 
-EXTENT_META = Table('extent_meta', METADATA,
-                    Column('id', SmallInteger, primary_key=True, autoincrement=True),
-                    Column('dataset_type_ref', None, ForeignKey(DATASET_TYPE.c.id), nullable=False),
-                    Column('start', DateTime(timezone=True), nullable=False),
-                    Column('end', DateTime(timezone=True), nullable=False),
-                    Column('offset_alias', String, nullable=False),
-                    Column('crs', String, nullable=True),
+def is_table_equal(table, db_url, schema_name):
+    # We use db_url instead of engine objects which rely on unreliable connection states
+    egn = create_engine(db_url)
+    meta = MetaData()
+    meta.reflect(bind=egn, schema=schema_name, only=lambda name, _: name in [table.name])
+    existing = Table(table.name, meta, autoload=True, autoload_with=egn)
+    for column in table.c:
+        if column not in existing.c:
+            return False
+    return True
 
-                    UniqueConstraint('dataset_type_ref', 'offset_alias')
-                   )
 
-EXTENT = Table('extent', METADATA,
-               Column('id', postgres.UUID(as_uuid=True), primary_key=True),
-               Column('extent_meta_ref', None, ForeignKey(EXTENT_META.c.id), nullable=False),
-               Column('start', DateTime(timezone=True), nullable=False),
-               Column('geometry', postgres.JSONB, nullable=True),
+def check_and_create_table(table, db_url, schema_name):
+    # We use db_url instead of engine objects which rely on unreliable connection states
+    egn = create_engine(db_url)
+    inspector = reflection.Inspector.from_engine(egn)
+    if table.name not in inspector.get_table_names(schema=schema_name):
+        table.create(bind=egn)
+    else:
+        # verify whether schema is same
+        if not is_table_equal(table, db_url, schema_name):
+            # There is no previous versions, therefore
+            # Existing table unlikely to be useful for migration
+            # ToDo: should this be  a message to log file or an exception
+            warnings.warn("Incompatible {} table exists in the database".format(table.name),
+                          RuntimeWarning)
 
-               UniqueConstraint('extent_meta_ref', 'start')
-              )
-
-RANGES = Table('ranges', METADATA,
-               Column('id', SmallInteger, primary_key=True, autoincrement=True),
-               Column('dataset_type_ref', None, ForeignKey(DATASET_TYPE.c.id), nullable=False),
-               Column('start', DateTime(timezone=True), nullable=False),
-               Column('end', DateTime(timezone=True), nullable=False),
-               Column('bounds', postgres.JSONB, nullable=True),
-               Column('crs', String, nullable=True),
-              )
 
 if __name__ == '__main__':
-    ENGINE = create_engine('postgresql://aj9439@agdcdev-db.nci.org.au:6432/datacube')
-    # Create the extent_meta table
-    EXTENT_META.create(bind=ENGINE)
-    # Create the extent table
-    EXTENT.create(bind=ENGINE)
-    # Create ranges table
-    RANGES.create(bind=ENGINE)
+    META = MetaData(naming_convention=SQL_NAMING_CONVENTIONS, schema=SCHEMA_NAME)
+    META.reflect(bind=ENGINE, schema=SCHEMA_NAME, only=lambda name, _: name in ['extent', 'extent_meta', 'ranges'])
