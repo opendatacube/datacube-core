b7c07e96db9db20e5ce27cee9518d7840ad8f36e
Kirill Kouzoubov <kirill888@gmail.com>
2018-07-04 17:25:03 +1000

    Add tests for limited product matching in dataset add
    Check that restricting product matching set works

--- integration_tests/test_dataset_add.py
+++ integration_tests/test_dataset_add.py
@@ -1,5 +1,44 @@
 from datacube.utils import SimpleDocNav
-from datacube.testutils import gen_dataset_test_dag, load_dataset_definition
+from datacube.testutils import gen_dataset_test_dag, load_dataset_definition, write_files
+import yaml
+
+
+def check_skip_lineage_test(clirunner, index):
+    ds = SimpleDocNav(gen_dataset_test_dag(11, force_tree=True))
+
+    prefix = write_files({'agdc-metadata.yml': yaml.safe_dump(ds.doc)})
+
+    clirunner(['dataset', 'add', '--confirm-ignore-lineage', '--product', 'A', str(prefix)])
+
+    ds_ = index.datasets.get(ds.id, include_sources=True)
+    assert ds_ is not None
+    assert str(ds_.id) == ds.id
+    assert ds_.sources == {}
+
+    assert index.datasets.get(ds.sources['ab'].id) is None
+    assert index.datasets.get(ds.sources['ac'].id) is None
+    assert index.datasets.get(ds.sources['ae'].id) is None
+    assert index.datasets.get(ds.sources['ac'].sources['cd'].id) is None
+
+
+def check_no_product_match(clirunner, index):
+    ds = SimpleDocNav(gen_dataset_test_dag(22, force_tree=True))
+
+    prefix = write_files({'agdc-metadata.yml': yaml.safe_dump(ds.doc)})
+
+    r = clirunner(['dataset', 'add',
+                   '--product', 'A',
+                   str(prefix)])
+    assert 'ERROR Dataset metadata did not match product signature' in r.output
+
+    r = clirunner(['dataset', 'add',
+                   '--product', 'A',
+                   '--product', 'B',
+                   str(prefix)])
+    assert 'ERROR No matching Product found for dataset' in r.output
+
+    ds_ = index.datasets.get(ds.id, include_sources=True)
+    assert ds_ is None
 
 
 def test_dataset_add(dataset_add_configs, index_empty, clirunner):
@@ -29,3 +68,6 @@ def test_dataset_add(dataset_add_configs, index_empty, clirunner):
     x = index.datasets.get(ds.id, include_sources=True)
     assert str(x.sources['ab'].id) == ds.sources['ab'].id
     assert str(x.sources['ac'].sources['cd'].id) == ds.sources['ac'].sources['cd'].id
+
+    check_skip_lineage_test(clirunner, index)
+    check_no_product_match(clirunner, index)
