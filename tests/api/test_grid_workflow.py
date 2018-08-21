7b6c3697d6ebbcbc6307b0fcb78a02424671d612
Kirill Kouzoubov <kirill888@gmail.com>
2018-08-21 16:54:10 +1000

    Add test case for geopolygon query parameter

--- tests/api/test_grid_workflow.py
+++ tests/api/test_grid_workflow.py
@@ -48,6 +48,10 @@ def test_gridworkflow():
     # test backend : that it finds the expected cell/dataset
     assert list(gw.cell_observations(**query).keys()) == [(1, -2)]
 
+    # again but with geopolygon
+    assert list(gw.cell_observations(**query,
+                                     geopolygon=gridspec.tile_geobox((1, -2)).extent).keys()) == [(1, -2)]
+
     # test frontend
     assert len(gw.list_tiles(**query)) == 1
 
