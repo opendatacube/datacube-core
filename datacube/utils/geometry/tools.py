4ff0aed86007e8d342515dff6c25dec3885c0bf6
Kirill Kouzoubov <kirill888@gmail.com>
2018-11-20 12:43:25 +1100

    More methods from dea-proto
    - estimate affine transform from points
    - decompose affine transform into parts
    - figure out scale change for a locally linear transform

--- datacube/utils/geometry/tools.py
+++ datacube/utils/geometry/tools.py
@@ -95,3 +95,104 @@ def roi_shape(roi):
 
 def roi_is_empty(roi):
     return any(d <= 0 for d in roi_shape(roi))
+
+
+def decompose_rws(A):
+    """Compute decomposition Affine matrix sans translation into Rotation Shear and Scale.
+
+    Note: that there are ambiguities for negative scales.
+
+    Example: R(90)*S(1,1) == R(-90)*S(-1,-1),
+    (R*(-I))*((-I)*S) == R*S
+
+    A = R W S
+
+    Where:
+
+    R [ca -sa]  W [1, w]  S [sx,  0]
+      [sa  ca]    [0, 1]    [ 0, sy]
+
+    """
+    # pylint: disable=invalid-name, too-many-locals
+
+    from numpy.linalg import cholesky, det, inv
+
+    if isinstance(A, Affine):
+        def to_affine(m, t=(0, 0)):
+            a, b, d, e = m.ravel()
+            c, f = t
+            return Affine(a, b, c,
+                          d, e, f)
+
+        (a, b, c,
+         d, e, f,
+         *_) = A
+        R, W, S = decompose_rws(np.asarray([[a, b],
+                                            [d, e]], dtype='float64'))
+
+        return to_affine(R, (c, f)), to_affine(W), to_affine(S)
+
+    assert A.shape == (2, 2)
+
+    WS = cholesky(A.T @ A).T
+    R = A @ inv(WS)
+
+    if det(R) < 0:
+        R[:, -1] *= -1
+        WS[-1, :] *= -1
+
+    ss = np.diag(WS)
+    S = np.diag(ss)
+    W = WS @ np.diag(1.0/ss)
+
+    return R, W, S
+
+
+def affine_from_pts(X, Y):
+    """ Given points X,Y compute A, such that: Y = A*X.
+
+        Needs at least 3 points.
+    """
+    from numpy.linalg import lstsq
+
+    assert len(X) == len(Y)
+    assert len(X) >= 3
+
+    n = len(X)
+
+    XX = np.ones((n, 3), dtype='float64')
+    YY = np.vstack(Y)
+    for i, x in enumerate(X):
+        XX[i, :2] = x
+
+    mm, *_ = lstsq(XX, YY, rcond=-1)
+    a, d, b, e, c, f = mm.ravel()
+
+    return Affine(a, b, c,
+                  d, e, f)
+
+
+def get_scale_at_point(pt, tr, r=None):
+    """ Given an arbitrary locally linear transform estimate scale change around a point.
+
+    1. Approximate Y = tr(X) as Y = A*X+t in the neighbourhood of pt, for X,Y in R2
+    2. Extract scale components of A
+
+
+    pt - estimate transform around this point
+    r  - radius around the point (default 1)
+
+    tr - List((x,y)) -> List((x,y))
+         takes list of 2-d points on input and outputs same length list of 2d on output
+
+    """
+    pts0 = [(0, 0), (-1, 0), (0, -1), (1, 0), (0, 1)]
+    x0, y0 = pt
+    if r is None:
+        XX = [(float(x+x0), float(y+y0)) for x, y in pts0]
+    else:
+        XX = [(float(x*r+x0), float(y*r+y0)) for x, y in pts0]
+    YY = tr(XX)
+    A = affine_from_pts(XX, YY)
+    _, _, S = decompose_rws(A)
+    return (abs(S.a), abs(S.e))
