b433fe05f5f47cea0b6f01630c44621242c4d37a
Duncan Gray <duncan.gray@ga.gov.au>
2019-01-09 17:07:11 +1100

    reword error message

--- datacube/index/hl.py
+++ datacube/index/hl.py
@@ -98,9 +98,8 @@ def check_dataset_consistent(dataset):
     # It the type expects measurements, ensure our dataset contains them all.
     if not product_measurements.issubset(dataset.measurements.keys()):
         not_measured = str(product_measurements - set(dataset.measurements.keys()))
-        msg = "Measurement fields don't match type specification.\n"
-        msg += "These are in the type specification but not the measurement "
-        msg += "fields;\n" + not_measured
+        msg = "The dataset is not specifying all of the measurements in this product.\n"
+        msg += "Missing fields are;\n" + not_measured
         return False, msg
 
     return True, None
