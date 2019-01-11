894f8508774106667ff858a7c45f2d7bd95a3b39
Kirill Kouzoubov <kirill888@gmail.com>
2019-01-11 11:49:53 +1100

    More type info around IO driver code

--- datacube/drivers/driver_cache.py
+++ datacube/drivers/driver_cache.py
@@ -1,12 +1,12 @@
-
 import logging
+from typing import Mapping, Any, Tuple, Iterable
 
 from pkg_resources import iter_entry_points, DistributionNotFound
 
 _LOG = logging.getLogger(__name__)
 
 
-def load_drivers(group):
+def load_drivers(group: str) -> Mapping[str, Any]:
     """
     Load available drivers for a given group name.
 
@@ -18,7 +18,7 @@ def load_drivers(group):
      By having driver entry_points pointing to a function, we defer loading the driver
      module or running any code until required.
 
-    :param str group: Name of the entry point group e.g. "datacube.plugins.io.read"
+    :param group: Name of the entry point group e.g. "datacube.plugins.io.read"
 
     :returns: Dictionary String -> Driver Object
     """
@@ -38,7 +38,7 @@ def load_drivers(group):
 
         try:
             driver = driver_init()
-        except:
+        except Exception:
             _LOG.warning('Exception during driver init, driver name: %s::%s', group, ep.name)
             return None
 
@@ -47,7 +47,7 @@ def load_drivers(group):
 
         return driver
 
-    def resolve_all(group):
+    def resolve_all(group: str) -> Iterable[Tuple[str, Any]]:
         for ep in iter_entry_points(group=group, name=None):
             driver = safe_load(ep)
             if driver is not None:
