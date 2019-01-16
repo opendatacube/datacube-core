9d92edfd515b347e020ad9839c6f2c30a8cb7303
Kirill Kouzoubov <kirill888@gmail.com>
2019-01-16 16:09:06 +1100

    Add no cover pragma to abstract methods in datasource.py

--- datacube/drivers/datasource.py
+++ datacube/drivers/datasource.py
@@ -22,33 +22,33 @@ class GeoRasterReader(object, metaclass=ABCMeta):
     @property
     @abstractmethod
     def crs(self):
-        ...
+        ...  # pragma: no cover
 
     @property
     @abstractmethod
     def transform(self) -> Optional[Affine]:
-        ...
+        ...  # pragma: no cover
 
     @property
     @abstractmethod
     def dtype(self) -> Union[str, np.dtype]:
-        ...
+        ...  # pragma: no cover
 
     @property
     @abstractmethod
     def shape(self) -> RasterShape:
-        ...
+        ...  # pragma: no cover
 
     @property
     @abstractmethod
     def nodata(self) -> Optional[Union[int, float]]:
-        ...
+        ...  # pragma: no cover
 
     @abstractmethod
     def read(self,
              window: Optional[RasterWindow] = None,
              out_shape: Optional[RasterShape] = None) -> Optional[np.ndarray]:
-        ...
+        ...  # pragma: no cover
 
 
 class DataSource(object, metaclass=ABCMeta):
@@ -58,4 +58,4 @@ class DataSource(object, metaclass=ABCMeta):
     @abstractmethod
     @contextmanager
     def open(self) -> Iterator[GeoRasterReader]:
-        ...
+        ...  # pragma: no cover
