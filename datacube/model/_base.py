d4464107796ded04fe78e3eac142bb452eaf0c5b
Kirill Kouzoubov <kirill888@gmail.com>
2019-01-17 17:31:00 +1100

    Move `Variable` namedtuple into netcdf driver
    This one is only used by netcdf writer, yet has very generic name and is part of
    "model", moving it into netcdf driver.

--- datacube/model/_base.py
+++ datacube/model/_base.py
@@ -1,5 +1,4 @@
 from collections import namedtuple
 
 Range = namedtuple('Range', ('begin', 'end'))
-Variable = namedtuple('Variable', ('dtype', 'nodata', 'dims', 'units'))
 CellIndex = namedtuple('CellIndex', ('x', 'y'))
