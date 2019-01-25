9313bff8dcba1babc95392f30340e489cd24e549
Kirill Kouzoubov <kirill888@gmail.com>
2019-01-25 13:27:23 +1100

    Remove masking related imports from datacube.api.* namespace
    These things moved to `datacube.storage.masking` ages ago, but were still
    exposed via `datacube.api.` namespace as well.

--- datacube/api/__init__.py
+++ datacube/api/__init__.py
@@ -3,8 +3,7 @@
 Modules for the Storage and Access Query API
 """
 
-from datacube.storage.masking import list_flag_names, describe_variable_flags, make_mask
 from .core import Datacube
 from .grid_workflow import GridWorkflow, Tile
 
-__all__ = ['Datacube', 'GridWorkflow', 'Tile', 'list_flag_names', 'describe_variable_flags', 'make_mask']
+__all__ = ['Datacube', 'GridWorkflow', 'Tile']
