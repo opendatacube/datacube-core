82d016ff824d100b2e1f6bc42d1ee89d6459ff11
Kirill Kouzoubov <kirill888@gmail.com>
2019-01-29 16:19:10 +1100

    Fix bad CRS test
    rasterio changed the way wkt is generated from CRS, it now includes EPSG
    authority fields, from_wkt(from_epsg().wkt) no longer erases EPSG, which is
    good, updating test to use hardcoded wkt with authority code erased.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -823,7 +823,25 @@ def test_testutils_geobox():
     assert gbox.crs.epsg == 3578
     assert gbox.transform == A
 
-    crs_ = dc_crs_from_rio(CRS.from_wkt(crs.wkt))
+    wkt = '''PROJCS["unnamed",
+    GEOGCS["NAD83",
+       DATUM["North_American_Datum_1983",
+             SPHEROID["GRS 1980",6378137,298.257222101, AUTHORITY["EPSG","7019"]],
+             TOWGS84[0,0,0,0,0,0,0],AUTHORITY["EPSG","6269"]],
+       PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],
+       UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9122"]],
+       ],
+    PROJECTION["Albers_Conic_Equal_Area"],
+    PARAMETER["standard_parallel_1",61.66666666666666],
+    PARAMETER["standard_parallel_2",68],
+    PARAMETER["latitude_of_center",59],
+    PARAMETER["longitude_of_center",-132.5],
+    PARAMETER["false_easting",500000],
+    PARAMETER["false_northing",500000],
+    UNIT["Meter",1]]
+    '''
+
+    crs_ = dc_crs_from_rio(CRS.from_wkt(wkt))
     assert crs_.epsg is None
 
 
