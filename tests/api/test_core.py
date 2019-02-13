42fbe7137965ad8bf064d333853725a9c029e406
Kirill Kouzoubov <kirill888@gmail.com>
2019-02-13 11:33:14 +1100

    In group_datasets order by uuid on top of other keys
    This is to address issue #646 when all datasets have the same timestamp.

--- tests/api/test_core.py
+++ tests/api/test_core.py
@@ -1,3 +1,5 @@
+from uuid import UUID
+from types import SimpleNamespace
 from datacube.api.query import GroupBy
 
 from datacube import Datacube
@@ -6,17 +8,26 @@ import datetime
 
 def test_grouping_datasets():
     def group_func(d):
-        return d['time']
+        return d.time
     dimension = 'time'
     units = None
     datasets = [
-        {'time': datetime.datetime(2016, 1, 1), 'value': 'foo'},
-        {'time': datetime.datetime(2016, 1, 1), 'value': 'flim'},
-        {'time': datetime.datetime(2016, 2, 1), 'value': 'bar'}
+        SimpleNamespace(time=datetime.datetime(2016, 1, 1), value='foo', id=UUID(int=10)),
+        SimpleNamespace(time=datetime.datetime(2016, 2, 1), value='bar', id=UUID(int=1)),
+        SimpleNamespace(time=datetime.datetime(2016, 1, 1), value='flim', id=UUID(int=9)),
     ]
 
     group_by = GroupBy(dimension, group_func, units, sort_key=group_func)
     grouped = Datacube.group_datasets(datasets, group_by)
+    dss = grouped.isel(time=0).values[()]
+    assert isinstance(dss, tuple)
+    assert len(dss) == 2
+    assert [ds.value for ds in dss] == ['flim', 'foo']
+
+    dss = grouped.isel(time=1).values[()]
+    assert isinstance(dss, tuple)
+    assert len(dss) == 1
+    assert [ds.value for ds in dss] == ['bar']
 
     assert str(grouped.time.dtype) == 'datetime64[ns]'
     assert grouped.loc['2016-01-01':'2016-01-15']
