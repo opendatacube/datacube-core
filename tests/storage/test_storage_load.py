9f76e903abecbe1d9afd84e9224a5af36745ec9a
Kirill Kouzoubov <kirill888@gmail.com>
2019-02-27 10:30:31 +1100

    Test that new_load_ctx gets the data it was promised

--- tests/storage/test_storage_load.py
+++ tests/storage/test_storage_load.py
@@ -6,24 +6,24 @@ from datacube.storage._load import (
 )
 
 from datacube.api.core import Datacube
-from datacube.drivers.rio._reader import RDEntry
 from datacube.testutils import mk_sample_dataset
 from datacube.testutils.io import rio_slurp
-from datacube.testutils.threads import FakeThreadPoolExecutor
-
-
-def mk_reader():
-    pool = FakeThreadPoolExecutor()
-    rde = RDEntry()
-    return rde.new_instance({'pool': pool,
-                             'allow_custom_pool': True})
+from datacube.testutils.iodriver import mk_rio_driver, tee_new_load_context
 
 
 def test_new_xr_load(data_folder):
     base = "file://" + str(data_folder) + "/metadata.yml"
 
-    rdr = mk_reader()
+    rdr = mk_rio_driver()
     assert rdr is not None
+
+    _bands = []
+
+    def band_info_collector(bands, ctx):
+        for b in bands:
+            _bands.append(b)
+
+    tee_new_load_context(rdr, band_info_collector)
 
     band = dict(name='a',
                 path='test.tif')
@@ -35,6 +35,8 @@ def test_new_xr_load(data_folder):
     measurements = [ds.type.measurements['a']]
 
     xx, _ = xr_load(sources, meta.gbox, measurements, rdr)
+
+    assert len(_bands) == 1
 
     assert im[0].shape == xx.a.isel(time=0).shape
     # TODO: verify pixel values
