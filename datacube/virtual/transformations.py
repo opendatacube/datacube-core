4ed6169984c624c9dd0ee9523e482161cd1ff14a
Emma Ai <emma.ai@ga.gov.au>
2019-07-16 13:25:33 +1000

    add dilation in apply_mask virtual product

--- datacube/virtual/transformations.py
+++ datacube/virtual/transformations.py
@@ -66,11 +66,12 @@ class MakeMask(Transformation):
 
 class ApplyMask(Transformation):
     def __init__(self, mask_measurement_name, apply_to=None,
-                 preserve_dtype=True, fallback_dtype='float32'):
+                 preserve_dtype=True, fallback_dtype='float32', dilation=0):
         self.mask_measurement_name = mask_measurement_name
         self.apply_to = apply_to
         self.preserve_dtype = preserve_dtype
         self.fallback_dtype = fallback_dtype
+        self.dilation = int(dilation)
 
     def measurements(self, input_measurements):
         rest = {key: value
@@ -90,6 +91,18 @@ class ApplyMask(Transformation):
     def compute(self, data):
         mask = data[self.mask_measurement_name]
         rest = data.drop(self.mask_measurement_name)
+
+        def dilate(array):
+            """Dilation e.g. for the mask"""
+            # e.g. kernel = [[1] * 7] * 7 # blocky 3-pixel dilation
+            y, x = numpy.ogrid[-self.dilation:(self.dilation+1), -self.dilation:(self.dilation+1)]
+            kernel = ((x * x) + (y * y) <= (self.dilation + 0.5) ** 2)  # disk-like `self.dilation` radial dilation
+            return scipy.ndimage.binary_dilation(array, structure=kernel.reshape((1, )+kernel.shape))
+
+        if self.dilation > 0:
+            import scipy.ndimage
+            mask = xarray.apply_ufunc(dilate, mask, output_dtypes=[numpy.bool], dask='parallelized',
+                                      keep_attrs=True)
 
         def worker(key, value):
             if self.preserve_dtype:
