e932340e064fa2804e80ee0e78d7ab6cc67667d1
Kirill Kouzoubov <kirill888@gmail.com>
2019-09-04 14:18:44 +1000

    Cleaning up datacube.utils

--- datacube/utils/_misc.py
+++ datacube/utils/_misc.py
@@ -4,17 +4,7 @@ Utility functions
 
 import logging
 import os
-import pathlib
-
-from datacube.utils.dates import parse_time
-from .dates import datetime_to_seconds_since_1970
-from .documents import InvalidDocException, SimpleDocNav, DocReader, is_supported_document_type, \
-    read_strings_from_netcdf, read_documents, validate_document, NoDatesSafeLoader, get_doc_offset, \
-    get_doc_offset_safe, netcdf_extract_string, without_lineage_sources
-from .math import unsqueeze_data_array, iter_slices, unsqueeze_dataset, data_resolution_and_offset
-from .py import cached_property, ignore_exceptions_if, import_function
-from .serialise import jsonify_document
-from .uris import is_url, uri_to_local_path, get_part_from_uri, mk_part_uri
+from .documents import read_documents, validate_document
 
 _LOG = logging.getLogger(__name__)
 
