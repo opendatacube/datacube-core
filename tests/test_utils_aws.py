71638bcc814803c8081076bd1d8f63d14c31bff6
Kirill Kouzoubov <kirill888@gmail.com>
2019-09-09 19:11:34 +1000

    Adding tools for AWS s3 cloud access

--- tests/test_utils_aws.py
+++ tests/test_utils_aws.py
@@ -2,10 +2,13 @@ import pytest
 import mock
 import json
 
+from datacube.testutils import write_files
+
 from datacube.utils.aws import (
     _fetch_text,
     ec2_current_region,
     auto_find_region,
+    get_aws_settings,
 )
 
 
@@ -63,3 +66,61 @@ def test_fetch_text():
 
     with mock.patch('datacube.utils.aws.urlopen', fake_urlopen):
         assert _fetch_text('http://localhost:8817') is None
+
+
+def test_get_aws_settings(monkeypatch):
+
+    pp = write_files({
+        "config": """
+[default]
+region = us-west-2
+
+[profile east]
+region = us-east-1
+[profile no_region]
+""",
+        "credentials": """
+[default]
+aws_access_key_id = AKIAWYXYXYXYXYXYXYXY
+aws_secret_access_key = fake-fake-fake
+[east]
+aws_access_key_id = AKIAEYXYXYXYXYXYXYXY
+aws_secret_access_key = fake-fake-fake
+"""
+    })
+
+    assert (pp/"credentials").exists()
+    assert (pp/"config").exists()
+
+    monkeypatch.setenv("AWS_CONFIG_FILE", str(pp/"config"))
+    monkeypatch.setenv("AWS_SHARED_CREDENTIALS_FILE", str(pp/"credentials"))
+
+    aws, creds = get_aws_settings()
+    assert aws['region_name'] == 'us-west-2'
+    assert aws['aws_access_key_id'] == 'AKIAWYXYXYXYXYXYXYXY'
+    assert aws['aws_secret_access_key'] == 'fake-fake-fake'
+
+    aws, creds = get_aws_settings(profile='east')
+    assert aws['region_name'] == 'us-east-1'
+    assert aws['aws_access_key_id'] == 'AKIAEYXYXYXYXYXYXYXY'
+    assert aws['aws_secret_access_key'] == 'fake-fake-fake'
+
+    aws, creds = get_aws_settings(aws_unsigned=True)
+    assert creds is None
+    assert aws['region_name'] == 'us-west-2'
+    assert aws['aws_unsigned'] is True
+
+    aws, creds = get_aws_settings(profile="no_region",
+                                  region_name="us-west-1",
+                                  aws_unsigned=True)
+
+    assert aws['region_name'] == 'us-west-1'
+    assert aws['aws_unsigned'] is True
+
+    with mock.patch('datacube.utils.aws._fetch_text',
+                    return_value=_json(region="mordor")):
+        aws, creds = get_aws_settings(profile="no_region",
+                                      aws_unsigned=True)
+
+        assert aws['region_name'] == 'mordor'
+        assert aws['aws_unsigned'] is True
