b2db063666d434d9234c933be26e4df46994b061
Kirill Kouzoubov <kirill888@gmail.com>
2019-09-12 15:29:20 +1000

    Changed "home folder resolution" logic
    - Changed "home folder resolution" logic to use `Path.home()` rather than directly
      using HOME environment variable
    - Adding types to utils.io functions

--- datacube/utils/io.py
+++ datacube/utils/io.py
@@ -1,6 +1,14 @@
 import os
 from pathlib import Path
-from typing import Union
+from typing import Union, Optional
+
+
+def _norm_path(path: Union[str, Path], in_home_dir: bool = False) -> Path:
+    if isinstance(path, str):
+        path = Path(path)
+    if in_home_dir:
+        path = Path.home()/path
+    return path
 
 
 def check_write_path(fname: Union[Path, str], overwrite: bool) -> Path:
@@ -27,20 +35,23 @@ def check_write_path(fname: Union[Path, str], overwrite: bool) -> Path:
     return fname
 
 
-def write_user_secret_file(text, fname, in_home_dir=False, mode='w'):
+def write_user_secret_file(text: Union[str, bytes],
+                           fname: Union[str, Path],
+                           in_home_dir: bool = False,
+                           mode: str = 'w'):
     """Write file only readable/writeable by the user"""
 
-    if in_home_dir:
-        fname = os.path.join(os.environ['HOME'], fname)
-
+    fname = _norm_path(fname, in_home_dir)
     open_flags = os.O_WRONLY | os.O_CREAT | os.O_TRUNC
     access = 0o600  # Make sure file is readable by current user only
-    with os.fdopen(os.open(fname, open_flags, access), mode) as handle:
+    with os.fdopen(os.open(str(fname), open_flags, access), mode) as handle:
         handle.write(text)
         handle.close()
 
 
-def slurp(fname, in_home_dir=False, mode='r'):
+def slurp(fname: Union[str, Path],
+          in_home_dir: bool = False,
+          mode: str = 'r') -> Optional[Union[bytes, str]]:
     """
     Read an entire file into a string
 
@@ -48,10 +59,9 @@ def slurp(fname, in_home_dir=False, mode='r'):
     :param in_home_dir: if True treat fname as a path relative to $HOME folder
     :return: Content of a file or None if file doesn't exist or can not be read for any other reason
     """
-    if in_home_dir:
-        fname = os.path.join(os.environ['HOME'], fname)
+    fname = _norm_path(fname, in_home_dir)
     try:
-        with open(fname, mode) as handle:
+        with open(str(fname), mode) as handle:
             return handle.read()
     except IOError:
         return None
