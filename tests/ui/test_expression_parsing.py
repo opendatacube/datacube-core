f64c6be2ff0788f8c3bae26b44972d877d6fae32
Damien Ayers <damien@omad.net>
2019-12-11 16:34:16 +1100

    Replace search expressions in tests with the format we want to use Flip pytest asserts to be the correct way, like   assert f() == 4
    See https://docs.pytest.org/en/latest/assert.html
    
    This makes the error messages much less concerning wrt expected/actual labels

--- tests/ui/test_expression_parsing.py
+++ tests/ui/test_expression_parsing.py
@@ -25,29 +25,26 @@ def test_between_expression():
 
 
 def test_parse_simple_expression():
-    assert {'platform': 4} == parse_expressions('platform = 4')
-    assert {'platform': 'LANDSAT_8'} == parse_expressions('platform = "LANDSAT_8"')
-    assert {'platform': 'LANDSAT_8'} == parse_expressions('platform = LANDSAT_8')
-    assert {'platform': 'LAND SAT_8'} == parse_expressions('platform = "LAND SAT_8"')
+    assert parse_expressions('platform = 4') == {'platform': 4}
+    assert parse_expressions('platform = "LANDSAT_8"') == {'platform': 'LANDSAT_8'}
+    assert parse_expressions('platform = LANDSAT_8') == {'platform': 'LANDSAT_8'}
+    assert parse_expressions('platform = "LAND SAT_8"') == {'platform': 'LAND SAT_8'}
 
-    between_exp = {'lat': Range(4, 6)}
-    assert between_exp == parse_expressions('4<lat<6')
-    assert between_exp == parse_expressions('6 > lat > 4')
-    assert between_exp == parse_expressions('lat in range (4, 6)')
+    assert parse_expressions('lat in [4, 6]') == {'lat': Range(4, 6)}
 
 
 def test_parse_uri_expression():
-    assert {'uri': 'file:///f/data/test.nc'} == parse_expressions('uri = file:///f/data/test.nc')
-    assert {'uri': 'file:///f/data/test.nc'} == parse_expressions('uri = "file:///f/data/test.nc"')
-    assert {'uri': 'file:///f/data/test me.nc'} == parse_expressions('uri = "file:///f/data/test me.nc"')
-    assert {'uri': 'file:///C:/f/data/test.nc'} == parse_expressions('uri = file:///C:/f/data/test.nc')
-    assert {'uri': 'file:///C:/f/data/test.nc'} == parse_expressions('uri = "file:///C:/f/data/test.nc"')
-    assert {'uri': 'file:///C:/f/data/test me.nc'} == parse_expressions('uri = "file:///C:/f/data/test me.nc"')
+    assert parse_expressions('uri = file:///f/data/test.nc') == {'uri': 'file:///f/data/test.nc'}
+    assert parse_expressions('uri = "file:///f/data/test.nc"') == {'uri': 'file:///f/data/test.nc'}
+    assert parse_expressions('uri = "file:///f/data/test me.nc"') == {'uri': 'file:///f/data/test me.nc'}
+    assert parse_expressions('uri = file:///C:/f/data/test.nc') == {'uri': 'file:///C:/f/data/test.nc'}
+    assert parse_expressions('uri = "file:///C:/f/data/test.nc"') == {'uri': 'file:///C:/f/data/test.nc'}
+    assert parse_expressions('uri = "file:///C:/f/data/test me.nc"') == {'uri': 'file:///C:/f/data/test me.nc'}
 
 
 def test_parse_dates():
-    assert {'time': datetime(2014, 3, 2, tzinfo=tzutc())} == parse_expressions('time = 2014-03-02')
-    assert {'time': datetime(2014, 3, 2, tzinfo=tzutc())} == parse_expressions('time = 2014-3-2')
+    assert parse_expressions('time = 2014-03-02') == {'time': datetime(2014, 3, 2, tzinfo=tzutc())}
+    assert parse_expressions('time = 2014-3-2') == {'time': datetime(2014, 3, 2, tzinfo=tzutc())}
 
     # A missing day defaults to the first of the month.
     # They are probably better off using in-expessions in these cases (eg. "time in 2013-01"), but it's here
@@ -55,46 +52,45 @@ def test_parse_dates():
     march_2014 = {
         'time': datetime(2014, 3, 1, tzinfo=tzutc())
     }
-    assert march_2014 == parse_expressions('time = 2014-03')
-    assert march_2014 == parse_expressions('time = 2014-3')
+    assert parse_expressions('time = 2014-03') == march_2014
+    assert parse_expressions('time = 2014-3') == march_2014
 
     implied_feb_2014 = {
         'time': Range(datetime(2014, 2, 1, tzinfo=tzutc()), datetime(2014, 3, 1, tzinfo=tzutc()))
     }
-    assert implied_feb_2014 == parse_expressions('2014-02 < time < 2014-03')
-    assert implied_feb_2014 == parse_expressions('time in range (2014-02, 2014-03)')
+    assert parse_expressions('time in [2014-02, 2014-03]') == implied_feb_2014
 
 
 def test_parse_date_ranges():
     eighth_march_2014 = {
         'time': Range(datetime(2014, 3, 8, tzinfo=tzutc()), datetime(2014, 3, 8, 23, 59, 59, tzinfo=tzutc()))
     }
-    assert eighth_march_2014 == parse_expressions('time in 2014-03-08')
-    assert eighth_march_2014 == parse_expressions('time in 2014-03-8')
+    assert parse_expressions('time in 2014-03-08') == eighth_march_2014
+    assert parse_expressions('time in 2014-03-8') == eighth_march_2014
 
     march_2014 = {
         'time': Range(datetime(2014, 3, 1, tzinfo=tzutc()), datetime(2014, 3, 31, 23, 59, 59, tzinfo=tzutc()))
     }
-    assert march_2014 == parse_expressions('time in 2014-03')
-    assert march_2014 == parse_expressions('time in 2014-3')
+    assert parse_expressions('time in 2014-03') == march_2014
+    assert parse_expressions('time in 2014-3') == march_2014
     # Leap year, 28 days
     feb_2014 = {
         'time': Range(datetime(2014, 2, 1, tzinfo=tzutc()), datetime(2014, 2, 28, 23, 59, 59, tzinfo=tzutc()))
     }
-    assert feb_2014 == parse_expressions('time in 2014-02')
-    assert feb_2014 == parse_expressions('time in 2014-2')
+    assert parse_expressions('time in 2014-02') == feb_2014
+    assert parse_expressions('time in 2014-2') == feb_2014
 
     # Entire year
     year_2014 = {
         'time': Range(datetime(2014, 1, 1, tzinfo=tzutc()), datetime(2014, 12, 31, 23, 59, 59, tzinfo=tzutc()))
     }
-    assert year_2014 == parse_expressions('time in 2014')
+    assert parse_expressions('time in 2014') == year_2014
 
 
 def test_parse_multiple_simple_expressions():
     # Multiple expressions in one command-line statement.
     # Mixed whitespace:
-    between_exp = parse_expressions('platform=LS8 -4<lat<23.5 instrument="OTHER"')
+    between_exp = parse_expressions('platform=LS8 lat in [-4, 23.5] instrument="OTHER"')
     assert between_exp == {'platform': 'LS8', 'lat': Range(-4, 23.5), 'instrument': 'OTHER'}
     # Range(x,y) is "equal" to (x, y). Check explicitly that it's a range:
     assert between_exp['lat'].begin == -4
