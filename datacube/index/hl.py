fb6db926de2ea43e54d6b64e6e45494cb02ef8b5
Kirill Kouzoubov <kirill888@gmail.com>
2020-03-27 11:39:50 +1100

    Remove duplicated documentation lines (bad merge?)

--- datacube/index/hl.py
+++ datacube/index/hl.py
@@ -214,7 +214,7 @@ class Doc2Dataset(object):
     :param index: an open Database connection
 
     :param list products: List of product names against which to match datasets
-                          (including lineage datasets). if not supplied we will
+                          (including lineage datasets). If not supplied we will
                           consider all products.
 
     :param list exclude_products: List of products to exclude from matching
@@ -227,24 +227,6 @@ class Doc2Dataset(object):
 
     :param skip_lineage: If True ignore lineage sub-tree in the supplied
                          document and construct dataset without lineage datasets
-
-    :param index: Database
-
-    :param products: List of product names against which to match datasets
-                     (including lineage datasets), if not supplied will consider all
-                     products.
-
-    :param exclude_products: List of products to exclude from matching
-
-    :param fail_on_missing_lineage: If True fail resolve if any lineage
-                                    datasets are missing from the DB
-
-    :param verify_lineage: If True check that lineage datasets in the
-                           supplied document are identical to DB versions
-
-    :param skip_lineage: If True ignore lineage sub-tree in the supplied
-                         document and construct dataset without lineage datasets
-
     """
     def __init__(self,
                  index,
