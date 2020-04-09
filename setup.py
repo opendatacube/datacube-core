4a562c5293b5fd5988ed0a73da42d9cb16e30f56
Kirill Kouzoubov <kirill888@gmail.com>
2020-04-09 13:44:35 +1000

    Get rid of references to python 3.5
    it is no longer supported

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ extra_plugins = dict(read=[], write=[], index=[])
 
 setup(
     name='datacube',
-    python_requires='>=3.5.2',
+    python_requires='>=3.6.0',
 
     url='https://github.com/opendatacube/datacube-core',
     author='Open Data Cube',
@@ -55,8 +55,8 @@ setup(
         "Operating System :: Microsoft :: Windows",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Topic :: Scientific/Engineering :: GIS",
         "Topic :: Scientific/Engineering :: Information Analysis",
     ],
