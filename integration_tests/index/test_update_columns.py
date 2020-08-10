2ecd1d89b7913dc42a65fb9c1066e347a0f029d7
Tisham Dhar <whatnickd@gmail.com>
2020-08-10 21:09:29 +1000

    #997 Tests with info schema based column checks
    Signed-off-by: Tisham Dhar <whatnickd@gmail.com>

--- integration_tests/index/test_update_columns.py
+++ integration_tests/index/test_update_columns.py
@@ -2,13 +2,23 @@
 Test creation of added/updated columns during
 `datacube system init`
 """
-from datacube.drivers.postgres.sql import pg_column_exists
+from datacube.drivers.postgres.sql import SCHEMA_NAME
 from datacube.drivers.postgres import _schema
+
 
 COLUMN_PRESENCE = """
 SELECT EXISTS (SELECT 1 FROM information_schema.columns
 WHERE table_schema='{schema}' AND table_name='{table}' AND column_name='{column}');
 """
+
+
+def check_column(conn, table_name: str, column_name: str) -> bool:
+    column_result = conn.execute(
+        COLUMN_PRESENCE.format(
+            schema=SCHEMA_NAME, table=table_name, column=column_name
+        )
+    ).fetchone()
+    return column_result == (True,)
 
 
 def test_added_column(clirunner, uninitialised_postgres_db):
@@ -17,7 +27,7 @@ def test_added_column(clirunner, uninitialised_postgres_db):
     assert "Created." in result.output
 
     with uninitialised_postgres_db.connect() as connection:
-        assert pg_column_exists(connection, _schema.METADATA_TYPE, "updated")
-        assert pg_column_exists(connection, _schema.PRODUCT, "updated")
-        assert pg_column_exists(connection, _schema.DATASET, "updated")
-        assert pg_column_exists(connection, _schema.DATASET_LOCATION, "added")
+        assert check_column(connection, _schema.METADATA_TYPE.name, "updated")
+        assert check_column(connection, _schema.PRODUCT.name, "updated")
+        assert check_column(connection, _schema.DATASET.name, "updated")
+        assert check_column(connection, _schema.DATASET_LOCATION.name, "added")
