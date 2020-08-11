6ed2ec3dafc16a6ce50ca8be27ad7c4ead370d80
Tisham Dhar <tisham.dhar@ga.gov.au>
2020-08-11 05:58:33 +0000

    Test triggers

--- integration_tests/index/test_update_columns.py
+++ integration_tests/index/test_update_columns.py
@@ -7,12 +7,19 @@ from datacube.drivers.postgres import _schema
 
 
 COLUMN_PRESENCE = """
-SELECT EXISTS (SELECT 1 FROM information_schema.columns
-WHERE table_schema='{schema}' AND table_name='{table}' AND column_name='{column}');
+select exists (select 1 from information_schema.columns
+where table_schema='{schema}' and table_name='{table}' and column_name='{column}');
 """
 
 DROP_COLUMN = """
-ALTER TABLE {schema}.{table} DROP COLUMN {column}
+alter table {schema}.{table} drop column {column}
+"""
+
+TRIGGER_PRESENCE = """
+select tgname
+from pg_trigger
+where not tgisinternal
+and tgrelid = '{schema}.{table}'::regclass;
 """
 
 
@@ -25,7 +32,16 @@ def check_column(conn, table_name: str, column_name: str) -> bool:
     return column_result == (True,)
 
 
-def drop_column(conn, table, column):
+def check_trigger(conn, table_name: str) -> bool:
+    trigger_result = conn.execute(
+        TRIGGER_PRESENCE.format(schema=SCHEMA_NAME, table=table_name)
+    ).fetchone()
+    if trigger_result is None:
+        return False
+    return 'row_update_time' in trigger_result[0]
+
+
+def drop_column(conn, table: str, column: str):
     conn.execute(DROP_COLUMN.format(
         schema=SCHEMA_NAME, table=table, column=column))
 
@@ -44,6 +60,12 @@ def test_added_column(clirunner, uninitialised_postgres_db):
         assert not check_column(connection, _schema.DATASET.name, "fake_column")
         assert check_column(connection, _schema.DATASET_LOCATION.name, "added")
         assert not check_column(connection, _schema.DATASET_LOCATION.name, "fake_column")
+
+        # Check for triggers
+        assert check_trigger(connection, _schema.METADATA_TYPE.name)
+        assert check_trigger(connection, _schema.PRODUCT.name)
+        assert check_trigger(connection, _schema.DATASET.name)
+        assert not check_trigger(connection, _schema.DATASET_LOCATION.name)
 
 
 def test_readd_column(clirunner, uninitialised_postgres_db):
