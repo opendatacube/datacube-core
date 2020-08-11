cd703e695e394783beeb1632f68fb25362328733
Tisham Dhar <tisham.dhar@ga.gov.au>
2020-08-11 05:24:17 +0000

    Address some changes , add more tests

--- integration_tests/index/test_update_columns.py
+++ integration_tests/index/test_update_columns.py
@@ -11,6 +11,10 @@ SELECT EXISTS (SELECT 1 FROM information_schema.columns
 WHERE table_schema='{schema}' AND table_name='{table}' AND column_name='{column}');
 """
 
+DROP_COLUMN = """
+ALTER TABLE {schema}.{table} DROP COLUMN {column}
+"""
+
 
 def check_column(conn, table_name: str, column_name: str) -> bool:
     column_result = conn.execute(
@@ -21,10 +25,45 @@ def check_column(conn, table_name: str, column_name: str) -> bool:
     return column_result == (True,)
 
 
+def drop_column(conn, table, column):
+    conn.execute(DROP_COLUMN.format(
+        schema=SCHEMA_NAME, table=table, column=column))
+
+
 def test_added_column(clirunner, uninitialised_postgres_db):
     # Run on an empty database.
     result = clirunner(["system", "init"])
     assert "Created." in result.output
+
+    with uninitialised_postgres_db.connect() as connection:
+        assert check_column(connection, _schema.METADATA_TYPE.name, "updated")
+        assert not check_column(connection, _schema.METADATA_TYPE.name, "fake_column")
+        assert check_column(connection, _schema.PRODUCT.name, "updated")
+        assert not check_column(connection, _schema.PRODUCT.name, "fake_column")
+        assert check_column(connection, _schema.DATASET.name, "updated")
+        assert not check_column(connection, _schema.DATASET.name, "fake_column")
+        assert check_column(connection, _schema.DATASET_LOCATION.name, "added")
+        assert not check_column(connection, _schema.DATASET_LOCATION.name, "fake_column")
+
+
+def test_readd_column(clirunner, uninitialised_postgres_db):
+    # Run on an empty database. drop columns and readd
+    result = clirunner(["system", "init"])
+    assert "Created." in result.output
+
+    with uninitialised_postgres_db.connect() as connection:
+        # Drop all the columns for an init rerun
+        drop_column(connection, _schema.METADATA_TYPE.name, "updated")
+        drop_column(connection, _schema.PRODUCT.name, "updated")
+        drop_column(connection, _schema.DATASET.name, "updated")
+        drop_column(connection, _schema.DATASET_LOCATION.name, "added")
+
+        assert not check_column(connection, _schema.METADATA_TYPE.name, "updated")
+        assert not check_column(connection, _schema.PRODUCT.name, "updated")
+        assert not check_column(connection, _schema.DATASET.name, "updated")
+        assert not check_column(connection, _schema.DATASET_LOCATION.name, "added")
+
+    result = clirunner(["system", "init"])
 
     with uninitialised_postgres_db.connect() as connection:
         assert check_column(connection, _schema.METADATA_TYPE.name, "updated")
