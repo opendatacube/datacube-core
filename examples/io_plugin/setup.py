abbc041227db0775cdb4e0306e853a2a590f5f07
Ronnie Taib <ronnie.taib@data61.csiro.au>
2021-06-02 11:17:14 +1000

    3D datasets tests.
    Require the xarray_3d driver and gedi data, both part of this commit.

--- examples/io_plugin/setup.py
+++ examples/io_plugin/setup.py
@@ -6,19 +6,19 @@ from setuptools import setup, find_packages
 
 setup(
     name='dcio_example',
-    version="1.0",
-    description="Test IO plugins for datacube",
-    author='AGDC Collaboration',
+    version='1.1',
+    description='Test IO plugins for datacube',
+    author='Open Data Cube',
     packages=find_packages(),
 
     entry_points={
         'datacube.plugins.io.read': [
             'pickle=dcio_example.pickles:rdr_driver_init',
-            'zeros=dcio_example.zeros:init_driver'
+            'zeros=dcio_example.zeros:init_driver',
+            'xarray_3d=dcio_example.xarray_3d:reader_driver_init',
         ],
         'datacube.plugins.io.write': [
             'pickle=dcio_example.pickles:writer_driver_init',
-        ]
-
-    }
+        ],
+    },
 )
