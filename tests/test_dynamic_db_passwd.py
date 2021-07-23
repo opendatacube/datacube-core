c0eaad31f4ce3a3366994709370f90b98119860e
phaesler <paul.haesler@data61.csiro.au>
2021-07-23 12:32:44 +1000

    Cleanup coding style.

--- tests/test_dynamic_db_passwd.py
+++ tests/test_dynamic_db_passwd.py
@@ -1,15 +1,23 @@
+# This file is part of the Open Data Cube, see https://opendatacube.org for more information
+#
+# Copyright (c) 2015-2020 ODC Contributors
+# SPDX-License-Identifier: Apache-2.0
 import pytest
 from sqlalchemy.exc import OperationalError
 from sqlalchemy.engine.url import URL
 
 from datacube.drivers.postgres._connections import PostgresDb, handle_dynamic_token_authentication
 
+
 counter = [0]
 last_base = [None]
+
+
 def next_token(base):
     counter[0] = counter[0] + 1
     last_base[0] = base
     return f"{base}{counter[0]}"
+
 
 def test_dynamic_password():
     url = URL.create('postgresql',
