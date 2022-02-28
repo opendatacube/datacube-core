306f3146d07b1e6de330dc1f15718dbbcc7401dc
Paul Haesler <linkjuggler@gmail.com>
2022-02-28 12:41:48 +1100

    Copy null index template to start in-memory index driver.

---
+++ datacube/index/memory/index.py
@@ -0,0 +1,82 @@
+# This file is part of the Open Data Cube, see https://opendatacube.org for more information
+#
+# Copyright (c) 2015-2022 ODC Contributors
+# SPDX-License-Identifier: Apache-2.0
+import logging
+
+from datacube.index.memory._datasets import DatasetResource  # type: ignore
+from datacube.index.memory._metadata_types import MetadataTypeResource
+from datacube.index.memory._products import ProductResource
+from datacube.index.memory._users import UserResource
+from datacube.index.abstract import AbstractIndex, AbstractIndexDriver
+from datacube.model import MetadataType
+
+_LOG = logging.getLogger(__name__)
+
+
+class Index(AbstractIndex):
+    """
+    Lightweight in-memory index driver
+    """
+
+    def __init__(self) -> None:
+        self._users = UserResource()
+        self._metadata_types = MetadataTypeResource()
+        self._products = ProductResource(self.metadata_types)
+        self._datasets = DatasetResource(self.products)
+
+    @property
+    def users(self) -> UserResource:
+        return self._users
+
+    @property
+    def metadata_types(self) -> MetadataTypeResource:
+        return self._metadata_types
+
+    @property
+    def products(self) -> ProductResource:
+        return self._products
+
+    @property
+    def datasets(self) -> DatasetResource:
+        return self._datasets
+
+    @property
+    def url(self) -> str:
+        return "null"
+
+    @classmethod
+    def from_config(cls, config, application_name=None, validate_connection=True):
+        return cls()
+
+    @classmethod
+    def get_dataset_fields(cls, doc):
+        return {}
+
+    def init_db(self, with_default_types=True, with_permissions=True):
+        return True
+
+    def close(self):
+        pass
+
+    def __repr__(self):
+        return "Index<null>"
+
+
+class MemoryIndexDriver(AbstractIndexDriver):
+    @staticmethod
+    def connect_to_index(config, application_name=None, validate_connection=True):
+        return Index.from_config(config, application_name, validate_connection)
+
+    @staticmethod
+    def metadata_type_from_doc(definition: dict) -> MetadataType:
+        """
+        :param definition:
+        """
+        MetadataType.validate(definition)  # type: ignore
+        return MetadataType(definition,
+                            dataset_search_fields=Index.get_dataset_fields(definition))
+
+
+def index_driver_init():
+    return MemoryIndexDriver()
--- setup.py
+++ setup.py
@@ -126,6 +126,7 @@ setup(
         'datacube.plugins.index': [
             'default = datacube.index.postgres.index:index_driver_init',
             'null = datacube.index.null.index:index_driver_init',
+            'memory = datacube.index.memory.index:index_driver_init',
             *extra_plugins['index'],
         ],
     },
