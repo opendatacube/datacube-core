522db9148716ce2275046319d1931bc941e13b65
Paul Haesler <linkjuggler@gmail.com>
2022-09-20 14:28:52 +1000

    lint format

--- integration_tests/index/test_index_data.py
+++ integration_tests/index/test_index_data.py
@@ -330,10 +330,10 @@ def test_transactions_api_ctx_mgr(index,
 
 
 def test_transactions_api_manual(index,
-                          extended_eo3_metadata_type_doc,
-                          ls8_eo3_product,
-                          eo3_ls8_dataset_doc,
-                          eo3_ls8_dataset2_doc):
+                                 extended_eo3_metadata_type_doc,
+                                 ls8_eo3_product,
+                                 eo3_ls8_dataset_doc,
+                                 eo3_ls8_dataset2_doc):
     from datacube.index.hl import Doc2Dataset
     import logging
     _LOG = logging.getLogger(__name__)
