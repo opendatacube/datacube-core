95c6045352580ff7263d4df1c57b56c3b67a3c63
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2022-10-27 23:02:21 +0000

    add autosectionlabel extension

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,8 @@ extensions = [
     'click_utils',
     'autodocsumm',
     'nbsphinx',
-    'sphinx.ext.napoleon'
+    'sphinx.ext.napoleon',
+    'sphinx.ext.autosectionlabel'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
