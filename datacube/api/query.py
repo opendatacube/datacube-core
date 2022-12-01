5d93bbde613d4729ea7d3b8df7627bf63e07c206
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2022-12-01 02:38:51 +0000

    prettify doc

--- datacube/api/query.py
+++ datacube/api/query.py
@@ -29,6 +29,8 @@ _LOG = logging.getLogger(__name__)
 class GroupBy:
     def __init__(self, group_by_func, dimension, units, sort_key=None, group_key=None):
         """
+        GroupBy Object
+
         :param group_by_func: Dataset -> group identifier
         :param dimension: dimension of the group key
         :param units: units of the group key
@@ -193,7 +195,7 @@ def query_group_by(group_by='time', **kwargs):
     """
     Group by function for loading datasets
 
-    :param group_by: str group_by name, supported str are:
+    :param str group_by: group_by name, supported str are:
         - time (default)
         - solar_day, see :func:`datacube.api.query.solar_day`
     :return: :class:`datacube.api.query.GroupBy`
