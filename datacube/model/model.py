708ee0901064e0de761957052942834eecea4b1b
Ariana Barzinpour <ariana.barzinpour@ga.gov.au>
2023-01-24 01:00:32 +0000

    add conversion function

--- datacube/model/model.py
+++ datacube/model/model.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from typing import Dict, List, Optional, Tuple, Union
 from uuid import UUID
+import re
 
 import affine
 import attr
@@ -8,6 +9,7 @@ from ruamel.yaml.comments import CommentedMap
 from shapely.geometry.base import BaseGeometry
 
 from .properties import Eo3Dict, Eo3Interface
+from datacube.model import Dataset
 
 DEA_URI_PREFIX = "https://collections.dea.ga.gov.au"
 ODC_DATASET_SCHEMA_URL = "https://schemas.opendatacube.org/dataset"
@@ -119,3 +121,99 @@ class DatasetDoc(Eo3Interface):
     accessories: Dict[str, AccessoryDoc] = attr.ib(factory=CommentedMap)
     #: Links to source dataset uuids
     lineage: Dict[str, List[UUID]] = attr.ib(factory=CommentedMap)
+
+
+# Conversion functions copied from datacube-alchemist for ease of transition
+
+def dataset_to_datasetdoc(ds: Dataset) -> DatasetDoc:
+    """
+    Convert to the DatasetDoc format that eodatasets expects.
+    """
+    if ds.metadata_type.name in {"eo_plus", "eo_s2_nrt", "gqa_eo"}:
+        # Handle S2 NRT metadata identically to eo_plus files.
+        # gqa_eo is the S2 ARD with extra quality check fields.
+        return _convert_eo_plus(ds)
+
+    if ds.metadata_type.name == "eo":
+        return _convert_eo(ds)
+
+    # Else we have an already mostly eo3 style dataset
+    product = ProductDoc(name=ds.type.name)
+    # Wrap properties to avoid typos and the like
+    properties = Eo3Dict(ds.metadata_doc.get("properties", {}))
+    if properties.get("eo:gsd"):
+        del properties["eo:gsd"]
+    return DatasetDoc(
+        id=ds.id,
+        product=product,
+        crs=str(ds.crs),
+        properties=properties,
+        geometry=ds.extent,
+    )
+
+
+def _convert_eo_plus(ds) -> DatasetDoc:
+    # Definitely need: # - 'datetime' # - 'eo:instrument' # - 'eo:platform' # - 'odc:region_code'
+    region_code = _guess_region_code(ds)
+    properties = Eo3Dict(
+        {
+            "odc:region_code": region_code,
+            "datetime": ds.center_time,
+            "eo:instrument": ds.metadata.instrument,
+            "eo:platform": ds.metadata.platform,
+            "landsat:landsat_scene_id": ds.metadata_doc.get(
+                "tile_id", "??"
+            ),  # Used to find abbreviated instrument id
+            "sentinel:sentinel_tile_id": ds.metadata_doc.get("tile_id", "??"),
+        }
+    )
+    product = ProductDoc(name=ds.type.name)
+    return DatasetDoc(id=ds.id, product=product, crs=str(ds.crs), properties=properties)
+
+
+def _convert_eo(ds) -> DatasetDoc:
+    # Definitely need: # - 'datetime' # - 'eo:instrument' # - 'eo:platform' # - 'odc:region_code'
+    region_code = _guess_region_code(ds)
+    properties = Eo3Dict(
+        {
+            "odc:region_code": region_code,
+            "datetime": ds.center_time,
+            "eo:instrument": ds.metadata.instrument,
+            "eo:platform": ds.metadata.platform,
+            "landsat:landsat_scene_id": ds.metadata.instrument,  # Used to find abbreviated instrument id
+        }
+    )
+    product = ProductDoc(name=ds.type.name)
+    return DatasetDoc(id=ds.id, product=product, crs=str(ds.crs), properties=properties)
+
+
+# Regex for extracting region codes from tile IDs.
+RE_TILE_REGION_CODE = re.compile(r".*A\d{6}_T(\w{5})_N\d{2}\.\d{2}")
+
+
+def _guess_region_code(ds: Dataset) -> str:
+    """
+    Get the region code of a dataset.
+    """
+    try:
+        # EO plus
+        return ds.metadata.region_code
+    except AttributeError:
+        # Not EO plus
+        pass
+
+    try:
+        # EO
+        return ds.metadata_doc["region_code"]
+    except KeyError:
+        # No region code!
+        pass
+
+    # Region code not specified, so get it from the tile ID.
+    # An example of such a tile ID for S2A NRT is:
+    # S2A_OPER_MSI_L1C_TL_VGS1_20201114T053541_A028185_T50JPP_N02.09
+    # The region code is 50JPP.
+    tile_match = RE_TILE_REGION_CODE.match(ds.metadata_doc["tile_id"])
+    if not tile_match:
+        raise ValueError("No region code for dataset {}".format(ds.id))
+    return tile_match.group(1)
