2d3670ae14542a2b2628197e9d5bf18410804906
Paul Haesler <linkjuggler@gmail.com>
2023-02-08 10:56:36 +1100

    Docstrings and tests.

--- datacube/model/lineage.py
+++ datacube/model/lineage.py
@@ -21,6 +21,12 @@ class LineageDirection(Enum):
     SOURCES = 1
     DERIVED = 2
 
+    def opposite(self):
+        if self == self.SOURCES:
+            return self.DERIVED
+        else:
+            return self.SOURCES
+
 
 @dataclass
 class LineageTree:
@@ -68,9 +74,12 @@ class LineageTree:
 
     def child_datasets(self) -> Set[UUID]:
         child_dsids = set()
+        if self.children is None:
+            return child_dsids
         for classifier, children in self.children.items():
             for child in children:
                 subchildren = child.child_datasets()
+                subchildren.add(child.dataset_id)
                 if self.dataset_id in subchildren:
                     raise InconsistentLineageException("LineageTrees must be acyclic")
                 child_dsids.update(subchildren)
@@ -78,7 +87,9 @@ class LineageTree:
 
 
 class InconsistentLineageException(Exception):
-    pass
+    """
+    Raised when a method would result in an inconsistent/invalid LineageTree or LineageRelations collection.
+    """
 
 
 @dataclass
@@ -92,22 +103,64 @@ class LineageRelation:
 
 
 class LineageRelations:
+    """
+    An indexed collection of LineageRelations.
+
+    For converting between iterables of LineageRelations and LineageTrees.
+    Enforces all lineage chains are acyclic.
+    """
     def __init__(self,
                  tree: Optional[LineageTree] = None,
                  max_depth: int = 0,
-                 merge_with: Optional["LineageRelations"] = None) -> None:
+                 clone: Optional["LineageRelations"] = None) -> None:
+        """
+        All arguments are optional.  Default gives an empty LineageRelations, and:
+
+             rels = LineageRelations(tree, max_depth=max_depth, clone=clone)
+
+        is equivalent to:
+
+             rels = LineageRelations()
+             rels.merge(clone)
+             rels.merge_tree(tree, max_depth=max_depth)
+
+        :param tree: Initially merge a LineageTree
+        :param max_depth: The maximum depth to read the LineageTree.
+                          Default/0: no limit.  Not used if tree is None.
+
+        :param clone: Initially clone this other LineageRelations object
+        """
+        # Internal index of id/home relations
         self._homes: MutableMapping[UUID, str] = {}
+        # Tuple[UUID, UUID]'s are always (derived, source)
+        # Mapping  (derived, source): classifier - Allow search by source, derived pair.
         self._relations_idx: MutableMapping[Tuple[UUID, UUID], str] = {}
+        # Mapping  (derived, source): list of LineageTree objects merged so far - prevent infinite loops in merging
         self._trees_idx: MutableMapping[Tuple[UUID, UUID], Sequence[LineageTree]] = {}
+        # Sequence of the distinct LineageRelation objects this object represents.
         self.relations: Sequence[LineageRelation] = []
+        # Mapping source to mapping derived to classifier.  Allow search by source
         self.by_source: MutableMapping[UUID, Mapping[UUID, str]] = {}
+        # Mapping source to mapping derived to classifier.  Allow search by derived
         self.by_derived: MutableMapping[UUID] = {}
-        if merge_with is not None:
-            self.merge(merge_with)
+        # Dataset ids known to this object
+        self.dataset_ids: Set[UUID] = set()
+
+        # Merge initial arguments
+        if clone is not None:
+            self.merge(clone)
         if tree is not None:
             self.merge_tree(tree, max_depth=max_depth)
 
-    def _merge_new_home(self, id_: UUID, home: str) -> None:
+    def merge_new_home(self, id_: UUID, home: str) -> None:
+        """
+        Merge a new home relation
+
+        Raises InconsistentLineageException if we already have this id with a different home
+
+        :param id_: The dataet id
+        :param home: The home string
+        """
         if id_ in self._homes:
             if self._homes[id_] and self._homes[id_] != home:
                 raise InconsistentLineageException(f"Tree contains inconsistent homes for dataset {id_}")
@@ -115,9 +168,18 @@ class LineageRelations:
             self._homes[id_] = home
 
     def _merge_new_relation(self, ids: Tuple[UUID, UUID], classifier: str) -> None:
+        """
+        Internal convenience wrapper to merge_new_lineage_relation
+        """
         self.merge_new_lineage_relation(LineageRelation(classifier=classifier, source_id=ids[0], derived_id=ids[1]))
 
     def merge_new_lineage_relation(self, rel: LineageRelation) -> None:
+        """
+        Merge a new LineageRelation object
+
+        Raises InconsistentLineageException if we already have this relation with a different classifier, or
+        this relation would result in a cyclic relation.
+        """
         ids = (rel.source_id, rel.derived_id)
         if ids in self._relations_idx:
             if self._relations_idx[ids] != rel.classifier:
@@ -133,56 +195,98 @@ class LineageRelations:
                 self.by_derived[rel.derived_id] = {}
             self.by_source[rel.source_id][rel.derived_id] = rel.classifier
             self.by_derived[rel.derived_id][rel.source_id] = rel.classifier
+            # Check for cyclic dependencies:
+            new_ids = set([rel.source_id, rel.derived_id])
+            if new_ids & self.dataset_ids:
+                # We already know about these ids so need to confirm we are still acyclic
+                # Extract sourcewards from derived and vice versa for full tree coverage
+                self.extract_tree(rel.derived_id, direction=LineageDirection.SOURCES)
+                self.extract_tree(rel.source_id, direction=LineageDirection.DERIVED)
+            self.dataset_ids.update(new_ids)
 
-    def _merge_new_node(self, ids: Tuple[UUID, UUID], node):
-        if ids in self._trees_idx:
-            got_grandchildren = bool(node.children)
-            for prev_tree in self._trees_idx[ids]:
-                if prev_tree.children and got_grandchildren:
-                    raise InconsistentLineageException(
-                        f"Self-reference or duplicate subtrees detected: risk of infinite recursion."
-                    )
-            self._trees_idx[ids].append(node)
-        else:
-            self._trees_idx[ids] = [node]
+    def _merge_new_node(self, ids: Tuple[UUID, UUID], node: LineageTree):
+        """
+        Check for self-reference/infinite recursion.
+
+        If a dataset is repeated in a LineageTree e.g. because of "diamond" relation:
+
+            A -> B     B -> D
+            A -> C     C -> D
+
+        then only one can explicitly have grandchildren - the rest must be "placeholder" LineageTrees
+        with children=None.
+
+        :param ids:
+        :param node:
+        :return:
+        """
+        got_grandchildren = bool(node.children)
+        if got_grandchildren:
+            if ids in self._trees_idx:
+                raise InconsistentLineageException(
+                    f"Self-reference or duplicate subtrees detected: risk of infinite recursion."
+                )
+            else:
+                self._trees_idx[ids] = node
 
     def merge(self, pool: "LineageRelations") -> None:
+        """
+        Merge in another LineageRelations collection, ensuring it is consistent with this one.
+
+        :param pool: The other LineageRelations object
+        """
         for id_, home in pool._homes.items():
-            self._merge_new_home(id_, home)
+            self.merge_new_home(id_, home)
         for ids, classifier in pool._relations_idx.items():
             self._merge_new_relation(ids, classifier)
         for ids, node in pool._trees_idx.items():
             self._merge_new_node(ids, node)
 
     def merge_tree(self, tree: LineageTree,
-                                     parent_id: Optional[UUID] = None,
+                                     parent_node: Optional[LineageTree] = None,
                                      max_depth: int = 0) -> None:
-        # Check acyclic
+        """
+        Merge in a LineageTree, ensuring it is consistent with the collection so far.
+
+        Raises InconsistentLineageException if tree contains cyclic depenedencies or inconsistent direction
+
+        :param tree: The LineageTree to merge
+        :param parent_node: The parent node (used to mark recursive traversal - should be None on first call)
+        :param max_depth: The depth to traverse the tree to.  default/zero = unlimited
+        """
+        # Check new tree is acyclic within itself
         tree.child_datasets()
-        self._merge_new_home(tree.dataset_id, tree.home)
+        self.merge_new_home(tree.dataset_id, tree.home)
+        # Determine recursion behaviour
         recurse = True
         next_max_depth = max_depth - 1
-        if not parent_id:
+        if not parent_node:
             next_max_depth = max_depth
         elif max_depth == 0:
             next_max_depth = 0
         elif max_depth == 1:
             recurse = False
+        if not tree.children:
+            # tree.children is {} or None (i.e. leaf node of original input tree).
+            # Try to extract a reverse-direction tree to check for cyclic dependencies
+            self.extract_tree(tree.dataset_id, direction=tree.direction.opposite())
+        if tree.children is None:
+            return
+        # Perform recursion, as determined above
         for classifier, children in tree.children.items():
             for child in children:
                 if child.direction != tree.direction:
                     raise InconsistentLineageException("Tree contains both derived and source nodes")
-                if parent_id:
-                    if tree.direction == LineageDirection.SOURCES:
-                        ids = (parent_id, child.dataset_id)
-                    else:
-                        ids = (child.dataset_id, parent_id)
-                    self._merge_new_relation(ids, classifier)
-                    self._merge_new_node(ids, child)
+                if tree.direction == LineageDirection.SOURCES:
+                    ids = (tree.dataset_id, child.dataset_id)
+                else:
+                    ids = (child.dataset_id, tree.dataset_id)
+                self._merge_new_relation(ids, classifier)
+                self._merge_new_node(ids, child)
                 if recurse:
                     self.merge_tree(
                         child,
-                        parent_id = tree.dataset_id,
+                        parent_node=tree,
                         max_depth=next_max_depth
                     )
         return
@@ -196,6 +300,23 @@ class LineageRelations:
                       Mapping[UUID, str],
                       Mapping[UUID, str]
                       ]:
+        """
+        Compare to another LineageRelations object, returning records to be added to or updated in
+        the other LinearRelations collection to consistently merge this collection into it.
+
+        Intended to be used by index drivers when adding lineage data to an index.
+
+        Raises InconsistentLineageException if updates are required and allow_updates is False, or if
+        merging the two LineageRelations would result in cyclic depenedencies.
+
+        :param existing_relations: The relations currently in an index.
+        :param allow_updates: Whether updates to existing records are allowed.
+        :return: Tuple containing:
+            Relations that need to be added to existing_relations to merge with this collection.
+            Relations that need to be updated in existing_relations to merge with this collection.
+            Homes that need to be added to existing_relations to merge with this collection.
+            Homes that need to up updated in existing_relations to merge with this collection.
+        """
         if not existing_relations:
             return (
                 self.relations, {},
@@ -208,8 +329,9 @@ class LineageRelations:
 
         if not allow_updates:
             # Ensure no inconsistencies
-            merged = LineageRelations(merge_with=self)
+            merged = LineageRelations(clone=self)
             merged.merge(existing_relations)
+            # Determine homes and relations to add
             for id_, home in self._homes:
                 if id_ not in existing_relations._homes:
                     homes_to_add[id_] = home
@@ -217,11 +339,13 @@ class LineageRelations:
                 if ids not in existing_relations._relations_idx:
                     relations_to_add[ids] = classifier
         else:
+            # Determine homes to add and update
             for id_, home in self._homes:
                 if id_ not in existing_relations._homes:
                     homes_to_add[id_] = home
                 elif home != existing_relations._homes[id_]:
                     homes_to_update[id_] = home
+            # Determine relations to add and update
             for ids, classifier in self._relations_idx:
                 if ids not in existing_relations._relations_idx:
                     relations_to_add[ids] = classifier
@@ -234,14 +358,33 @@ class LineageRelations:
 
     def extract_tree(self,
                      root: UUID,
-                     direction: LineageDirection = LineageDirection.SOURCES) -> LineageTree:
+                     direction: LineageDirection = LineageDirection.SOURCES,
+                     so_far: Optional[Set[UUID]] = None
+                     ) -> LineageTree:
+        """
+        Extract a LineageTree from this LineageRelations collection.
+
+        Used to detect cyclic dependencies.
+
+        :param root: The dataset id at the root of the extracted LineageTree
+        :param direction: The direction of the extracted tree
+        :param so_far: Used to detect cyclic dependencies in recursive mode - should be None on initial call.
+        :return: the extracted LineageTree.
+        """
+        # Trees are extracted from the root down, so the leaf-up cycle-detection of tree.child_datasets
+        # is insufficient here
+        if so_far is None:
+            so_far = set()
+        if root in so_far:
+            raise InconsistentLineageException(f"LineageTrees must be acyclic: {root}")
+        so_far.add(root)
         children = {}
         if direction == LineageDirection.SOURCES:
             subtrees = self.by_source.get(root, {})
         else:
             subtrees = self.by_derived.get(root, {})
         for dsid, classifier in subtrees.items():
-            subtree = self.extract_tree(dsid, direction)
+            subtree = self.extract_tree(dsid, direction, set(so_far))
             if classifier in children:
                 children[classifier].append(subtree)
             else:
@@ -250,7 +393,6 @@ class LineageRelations:
             dataset_id=root,
             direction=direction,
             children=children,
-            home=self._homes[root]
+            home=self._homes.get(root)
         )
-        # Check acyclic
-        tree.child_datasets()
+        return tree
--- tests/test_ext_lineage.py
+++ tests/test_ext_lineage.py
@@ -10,17 +10,19 @@ from datacube.model.lineage import InconsistentLineageException, LineageRelation
 
 def test_ltree_clsmethods():
     root = random_uuid()
+    # Minimal tree - root node only
     minimal = LineageTree.from_eo3_doc(dsid=root)
     assert minimal.dataset_id == root
     assert minimal.direction == LineageDirection.SOURCES
     assert minimal.children is None
+    # Check optional args to from_eo3_doc are set correctly.
     optional_args = LineageTree.from_eo3_doc(dsid=root, sources={},
                                           direction=LineageDirection.DERIVED,
                                           home="notused")
     assert optional_args == LineageTree(direction=LineageDirection.DERIVED,
                                         dataset_id=root,
                                         children={},
-                                        home=None
+                                        home=None   # Note home is not written to the root node
                                         )
 
 @pytest.fixture
@@ -94,8 +96,25 @@ def big_src_lineage_tree(big_src_tree_ids):
     )
 
 
-def test_lin_rels_minimal(big_src_lineage_tree):
+def test_child_datasets(big_src_lineage_tree, big_src_tree_ids):
+    cds = big_src_lineage_tree.child_datasets()
+    for dsid in big_src_tree_ids.values():
+        assert dsid == big_src_lineage_tree.dataset_id or dsid in cds
+
+
+def test_lin_rels_lin_tree_conversions(big_src_lineage_tree):
+    # Create LRS from LT
     rels1 = LineageRelations(tree=big_src_lineage_tree)
+    # Extract LT from LRS
+    extracted_tree = rels1.extract_tree(big_src_lineage_tree.dataset_id, big_src_lineage_tree.direction)
+    # Confirm extract LT produces same LRS as original LT
+    rels2 = LineageRelations(tree=extracted_tree)
+    for rel in rels1.relations:
+        assert rel in rels2.relations
+
+
+def test_detect_cyclic_deps(big_src_lineage_tree, big_src_tree_ids):
+    # Confirm trivial cyclic dependencies are detected.
     repeated_uuid = random_uuid()
     looped_tree = LineageTree(
         dataset_id=repeated_uuid, direction=LineageDirection.SOURCES, children={
@@ -107,5 +126,20 @@ def test_lin_rels_minimal(big_src_lineage_tree):
         }
     )
     with pytest.raises(InconsistentLineageException) as e:
-        rels2 = LineageRelations(looped_tree)
-    assert "foobar" in str(e.value)
+        rels2 = LineageRelations(tree=looped_tree)
+    assert "LineageTrees must be acyclic" in str(e.value)
+    # Confirm more complex cyclic dependencies can be detected.
+    rels = LineageRelations(tree=big_src_lineage_tree)
+    breaking_tree = LineageTree(
+        dataset_id= big_src_tree_ids["l1_1"],
+        direction=LineageDirection.SOURCES,
+        children={
+            "cyclic_dep": [LineageTree(
+                dataset_id=big_src_tree_ids["root"],
+                direction=LineageDirection.SOURCES
+            )]
+        }
+    )
+    with pytest.raises(InconsistentLineageException) as e:
+        rels.merge_tree(breaking_tree)
+    assert "LineageTrees must be acyclic" in str(e.value)
