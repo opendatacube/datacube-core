e68d1a4fed9e1fa69abe544796b49ceb5dba31c1
Ariana Barzinpour <ariana.barzinpour@ga.gov.au>
2023-09-08 15:59:12 +1000

    give better error msg if dataset is not compatible with archive-less-mature

--- datacube/index/abstract.py
+++ datacube/index/abstract.py
@@ -1164,21 +1164,41 @@ class AbstractDatasetResource(ABC):
         """
         less_mature = []
         assert delta >= 0
+
+        def check_maturity_information(dataset, props):
+            # check that the dataset metadata includes all maturity-related properties
+            # passing in the required props to enable greater extensibility should it be needed
+            for prop in props:
+                if hasattr(dataset.metadata, prop) and (getattr(dataset.metadata, prop) is not None):
+                    return
+                raise ValueError(
+                    f"Dataset {dataset.id} is missing property {prop} required for maturity check"
+                )
+
+        check_maturity_information(ds, ["region_code", "time", "dataset_maturity"])
+
         # 'expand' the date range by `delta` milliseconds to give a bit more leniency in datetime comparison
         expanded_time_range = Range(ds.metadata.time.begin - timedelta(milliseconds=delta),
                                     ds.metadata.time.end + timedelta(milliseconds=delta))
         dupes = self.search(product=ds.product.name,
                             region_code=ds.metadata.region_code,
                             time=expanded_time_range)
+
         for dupe in dupes:
             if dupe.id == ds.id:
                 continue
+
+            # only need to check that dupe has dataset maturity, missing/null region_code and time
+            # would already have been filtered out during the search query
+            check_maturity_information(dupe, ["dataset_maturity"])
+
             if dupe.metadata.dataset_maturity == ds.metadata.dataset_maturity:
                 # Duplicate has the same maturity, which one should be archived is unclear
                 raise ValueError(
                     f"A dataset with the same maturity as dataset {ds.id} already exists, "
                     f"with id: {dupe.id}"
                 )
+
             if dupe.metadata.dataset_maturity < ds.metadata.dataset_maturity:
                 # Duplicate is more mature than dataset
                 # Note that "final" < "nrt"
@@ -1186,6 +1206,7 @@ class AbstractDatasetResource(ABC):
                     f"A more mature version of dataset {ds.id} already exists, with id: "
                     f"{dupe.id} and maturity: {dupe.metadata.dataset_maturity}"
                 )
+
             less_mature.append(dupe)
         return less_mature
 
