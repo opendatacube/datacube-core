ee49ccdb456b41942aa3e00680bf01f86e5dbec8
Paul Haesler <linkjuggler@gmail.com>
2023-11-25 14:39:22 +1100

    Initial version.

---
+++ datacube/drivers/postgis/alembic/versions/48fb71ece268_initial_revision.py
@@ -0,0 +1,32 @@
+# This file is part of the Open Data Cube, see https://opendatacube.org for more information
+#
+# Copyright (c) 2015-2023 ODC Contributors
+# SPDX-License-Identifier: Apache-2.0
+"""Initial revision
+
+Revision ID: 48fb71ece268
+Revises: 
+Create Date: 2023-11-21 16:28:45.209473
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '48fb71ece268'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+# It is helpful for schema management to have an empty initial revision
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
