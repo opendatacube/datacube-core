c8366ec996a38d228196be7cf69e0563ef2d8a49
Paul Haesler <linkjuggler@gmail.com>
2024-01-13 16:07:00 +1100

    Fix memory driver.

--- datacube/index/memory/_datasets.py
+++ datacube/index/memory/_datasets.py
@@ -17,7 +17,7 @@ from uuid import UUID
 from datacube.index import fields
 
 from datacube.index.abstract import (AbstractDatasetResource, DSID, dsid_to_uuid, BatchStatus,
-                                     QueryField, DatasetSpatialMixin, NoLineageResource)
+                                     QueryField, DatasetSpatialMixin, NoLineageResource, AbstractIndex)
 from datacube.index.fields import Field
 from datacube.index.memory._fields import build_custom_fields, get_dataset_fields
 from datacube.index.memory._products import ProductResource
@@ -32,9 +32,8 @@ _LOG = logging.getLogger(__name__)
 
 
 class DatasetResource(AbstractDatasetResource):
-    def __init__(self, product_resource: ProductResource) -> None:
-        self.product_resource = product_resource
-        self.metadata_type_resource = product_resource.metadata_type_resource
+    def __init__(self, index: AbstractIndex) -> None:
+        super().__init__(index)
         # Main dataset index
         self.by_id: MutableMapping[UUID, Dataset] = {}
         # Indexes for active and archived datasets
@@ -300,9 +299,9 @@ class DatasetResource(AbstractDatasetResource):
 
     def get_field_names(self, product_name=None) -> Iterable[str]:
         if product_name is None:
-            prods = self.product_resource.get_all()
+            prods = self._index.products.get_all()
         else:
-            prod = self.product_resource.get_by_name(product_name)
+            prod = self._index.products.get_by_name(product_name)
             if prod:
                 prods = [prod]
             else:
@@ -497,7 +496,7 @@ class DatasetResource(AbstractDatasetResource):
         )
 
     def _get_prod_queries(self, **query: QueryField) -> Iterable[Tuple[Mapping[str, QueryField], Product]]:
-        return ((q, product) for product, q in self.product_resource.search_robust(**query))
+        return ((q, product) for product, q in self._index.products.search_robust(**query))
 
     def search(self,
                limit: Optional[int] = None,
@@ -664,7 +663,7 @@ class DatasetResource(AbstractDatasetResource):
     def get_product_time_bounds(self, product: str) -> Tuple[datetime.datetime, datetime.datetime]:
         min_time: Optional[datetime.datetime] = None
         max_time: Optional[datetime.datetime] = None
-        prod = self.product_resource.get_by_name(product)
+        prod = self._index.products.get_by_name(product)
         if prod is None:
             raise ValueError(f"Product {product} not in index")
         time_fld = prod.metadata_type.dataset_fields["time"]
@@ -728,7 +727,7 @@ class DatasetResource(AbstractDatasetResource):
         else:
             uris = []
         return Dataset(
-            product=self.product_resource.clone(orig.product),
+            product=self._index.products.clone(orig.product),
             metadata_doc=jsonify_document(orig.metadata_doc_without_lineage()),
             uris=uris,
             indexed_by="user" if for_save and orig.indexed_by is None else orig.indexed_by,
