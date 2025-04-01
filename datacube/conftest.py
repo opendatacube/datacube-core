9ca2b7881086d548022341374605b1cf45ecf9da
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2025-04-02 01:36:19 +0200

    pytest: only doctest relevant files (#1751)
    * config: fix permissions
    
    * pytest: only doctest relevant files
    
    Avoid collecting files under
    datacube/ unless they need
    to be tested (=contain lines
    that start with whitespace
    followed by ">>>").
    
    This avoids triggering a bunch
    of deprecation warnings.
    
    Refs #1716

---
+++ datacube/conftest.py
@@ -0,0 +1,16 @@
+# This file is part of the Open Data Cube, see https://opendatacube.org for more information
+#
+# Copyright (c) 2015-2025 ODC Contributors
+# SPDX-License-Identifier: Apache-2.0
+from pathlib import Path
+
+
+# The only thing tested under datacube/ is doctests, so only collect files
+# that need testing (=have lines starting with whitespace + ">>>").
+def pytest_ignore_collect(collection_path: Path) -> bool:
+    if collection_path.is_dir():
+        return False
+    if collection_path.suffix != ".py":
+        return True
+    with collection_path.open() as f:
+        return not any(line.strip().startswith(">>>") for line in f)
