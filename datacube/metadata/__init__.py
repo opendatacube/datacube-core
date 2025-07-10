df3cb6e984b1cdb4b36366f22f3cdfbb37dce5c6
Paul Haesler <linkjuggler@gmail.com>
2025-07-10 10:33:11 +1000

    Add eo3converter.py from odc-stac without history.

---
+++ datacube/metadata/__init__.py
@@ -0,0 +1,16 @@
+# This file is part of the Open Data Cube, see https://opendatacube.org for more information
+#
+# Copyright (c) 2015-2025 ODC Contributors
+# SPDX-License-Identifier: Apache-2.0
+"""
+Datacube metadata generation from STAC.
+
+This only imports if datacube is installed.
+"""
+
+from ._eo3converter import infer_dc_product, stac2ds
+
+__all__ = (
+    "infer_dc_product",
+    "stac2ds",
+)
