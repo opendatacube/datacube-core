3602d4d99092e94bba846467489edaaab8bdc245
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2025-07-28 20:32:20 +0200

    index: adjust search_returning types
    Align the types and documentation for
    the implementations of search_returning.

--- datacube/index/postgis/_datasets.py
+++ datacube/index/postgis/_datasets.py
@@ -765,21 +765,33 @@ class DatasetResource(AbstractDatasetResource, IndexResourceAddIn):
         archived: bool | None = False,
         order_by: Iterable[Any] | None = None,
         **query: QueryField,
-    ) -> Generator[tuple]:
+    ) -> Generator[NamedTuple]:
         """
         Perform a search, returning only the specified fields.
 
-        This method can be faster than normal search() if you don't need all fields of each dataset.
+        This method can be faster than normal search() if you don't need all
+        fields of each dataset.
 
-        It also allows for returning rows other than datasets, such as a row per uri when requesting field 'uri'.
+        It also allows for returning rows other than datasets, such as a row
+        per uri when requesting field 'uri'.
 
-        :param field_names:
-        :param query:
-        :param limit: Limit number of datasets
-        :param archived: include archived datasets
-        :param order_by: sql text, dataset field, or sqlalchemy expression
-        by which to order results
-        :return: sequence of results, each result is a namedtuple of your requested fields
+        :param field_names: Names of desired fields (default = all known search
+                            fields, unless custom_offsets is set, see below)
+        :param custom_offsets: A dictionary of offsets in the metadata doc for custom
+                               fields custom offsets are returned in addition to fields
+                               named in field_names. Default is None, field_names only.
+                               If field_names is None, and custom_offsets are provided,
+                               only the custom offsets are included, over-riding
+                               the normal field_names default.
+        :param limit: Limit number of dataset (None/default = unlimited)
+        :param archived: False (default): Return active datasets only.
+                         None: Include archived and active datasets.
+                         True: Return archived datasets only.
+        :param order_by: a field name, field, function or clause by which to sort
+                         output. None is unsorted and may allow faster return of first
+                          result depending on the index driver's implementation.
+        :param query: search query parameters
+        :return: Namedtuple of requested fields, for each matching dataset.
         """
         field_name_d: dict[str, None] = {}
         if field_names is None and custom_offsets is None:
