852c33979fe7fbe175bfb75129ce166a268028d8
Paul Haesler <linkjuggler@gmail.com>
2025-11-03 15:34:08 +1100

    Convert bool option to int for internal api to prevent warnings.

--- datacube/scripts/dataset.py
+++ datacube/scripts/dataset.py
@@ -252,7 +252,7 @@ def index_cmd(
             index,
             auto_add_lineage=auto_add_lineage and not ignore_lineage,
             dry_run=dry_run,
-            archive_less_mature=archive_less_mature,
+            archive_less_mature=500 if archive_less_mature else False
         )
 
     # If outputting directly to terminal, show a progress bar.
