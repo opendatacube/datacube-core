26d9482c35b3c28ce8ab1402534fde76d70a4921
Paul Haesler <linkjuggler@gmail.com>
2026-07-25 16:34:21 +1000

    Restore test of metadata creation type per driver.

--- integration_tests/index/test_pluggable_indexes.py
+++ integration_tests/index/test_pluggable_indexes.py
@@ -2,28 +2,53 @@
 #
 # Copyright (c) 2015-2026 ODC Contributors
 # SPDX-License-Identifier: Apache-2.0
-from typing import Type
 
 import pytest
 import yaml
 
-from datacube.index.postgres.index import Index as PgIndex
 from datacube.index.postgis.index import Index as PGISIndex
+from datacube.index.postgres.index import Index as PgIndex
 from datacube.model import MetadataType
 
-from datacube.index.postgres.index import Index
+TYPE_CHECKING = False
+
 
 @pytest.fixture
-def index_cls(datacube_env_name) -> Type[PgIndex | PGISIndex]:
+def index_cls(datacube_env_name) -> type[PgIndex | PGISIndex]:
     if datacube_env_name.startswith("postgis"):
         return PGISIndex
     return PgIndex
 
 
-@pytest.mark.parametrize("datacube_env_name", ("datacube", "datacube3", "postgis", "postgis3"))
+@pytest.mark.parametrize(
+    "datacube_env_name", ("datacube", "datacube3", "postgis", "postgis3")
+)
 def test_with_standard_index(uninitialised_postgres_db, cfg_env, index_cls) -> None:
     index = index_cls(uninitialised_postgres_db, cfg_env)
     index.init_db()
+
+
+@pytest.mark.parametrize("datacube_env_name", ("datacube3", "postgis3"))
+def test_metadata_type_with_standard_index(index) -> None:
+    metadata_doc = yaml.safe_load("""
+    name: minimal
+    description: minimal metadata definition
+    dataset:
+        id: [id]
+        sources: [lineage, source_datasets]
+        label: [label]
+        creation_dt: ["properties", "odc:processing_datetime"]
+        search_fields:
+            some_custom_field:
+                description: some custom field
+                offset: [properties, custom]
+        """)
+
+    metadata = index.metadata_types.from_doc(metadata_doc)
+    assert isinstance(metadata, MetadataType)
+    assert metadata.id is None
+    assert metadata.name == "minimal"
+    assert "some_custom_field" in metadata.dataset_fields
 
 
 def test_system_init(uninitialised_postgres_db, clirunner) -> None:
